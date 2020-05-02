4bbdfd1aa53ab53af83b86c71b24a35fbd4172fe
yahiro <yahiro07@users.noreply.github.com>
2020-05-03 03:09:51 +0900

    refactor logic modules

--- software/electron_keyboard_utility_kermite/src/shell/services/InputLogicSimulatorC/index.ts
+++ software/electron_keyboard_utility_kermite/src/shell/services/InputLogicSimulatorC/index.ts
@@ -11,37 +11,51 @@ import { appGlobal } from '../appGlobal';
 import { IInputLogicSimulator } from '../InputLogicSimulator.interface';
 import { IntervalTimerWrapper } from '../InputLogicSimulator/IntervalTimerWrapper';
 import { KeyIndexKeyEvent, TKeyTrigger } from '../InputLogicSimulatorB/common';
+import { Arrays } from '~funcs/Arrays';
 
 interface IKeyBindingInfo {
   assign: IKeyAssignEntry;
   timeStamp: number;
 }
 
-type IKeyBindingInfoDict = { [keyId: string]: IKeyBindingInfo };
-
 const logicSimulatorStateC = new (class {
   editModel: IEditModel = fallbackProfileData;
-  keyBindingInfoDict: IKeyBindingInfoDict = {};
+  keyBindingInfoDict: { [keyId: string]: IKeyBindingInfo } = {};
 })();
 
-namespace ModuleW {
+namespace ModuleW_HidReportOutputBuffer {
   const local = new (class {
     hidReportQueue: number[][] = [];
+    prevInputHidReport: number[] = [];
   })();
 
-  export function commitHidReport(report: number[]) {
-    local.hidReportQueue.push(report);
+  export function commitHidReport(hidReport: number[]) {
+    // fix modifier transition delay problem
+    //[ 2, 0, 0, ...] (shift hold)
+    //[ 0, 0, 0, ...] (nothing hold) <--- will be added here
+    //[ 0, 0, X, ...] (key X hold)
+    //* insert nothing hold report state when modifier key removed and some key added
+    if (
+      local.prevInputHidReport[0] &&
+      !hidReport[0] &&
+      hidReport.slice(2).some((val) => val > 0)
+    ) {
+      local.hidReportQueue.push([0, 0, 0, 0, 0, 0, 0, 0]);
+    }
+
+    local.hidReportQueue.push(hidReport);
+    local.prevInputHidReport = hidReport;
   }
 
   export function processTicker() {
-    const report = local.hidReportQueue.shift();
-    if (report) {
-      appGlobal.deviceService.writeSideBrainHidReport(report);
+    const hidReport = local.hidReportQueue.shift();
+    if (hidReport) {
+      appGlobal.deviceService.writeSideBrainHidReport(hidReport);
     }
   }
 }
 
-namespace ModuleP {
+namespace ModuleP_OutputKeyStateCombiner {
   function getModifierBits(modFlags: { [vk in ModifierVirtualKey]: boolean }) {
     let modifierBits = 0;
     modFlags.K_Ctrl && (modifierBits |= 0x01);
@@ -51,8 +65,8 @@ namespace ModuleP {
     return modifierBits;
   }
 
-  function reduceBindingInfosToHidReport(
-    bindingInfos: IKeyBindingInfo[]
+  function reduceHoldKeyBindsToHidReport(
+    holdKeyBinds: HoldKeyBind[]
   ): number[] {
     const modFlags: {
       [vk in ModifierVirtualKey]: boolean;
@@ -64,40 +78,28 @@ namespace ModuleP {
     };
     const hidKeyCodes: number[] = [];
 
-    for (const info of bindingInfos) {
-      const assign = info.assign;
-      if (assign.type === 'layerCall') {
-        const layer = logicSimulatorStateC.editModel.layers.find(
-          (la) => la.layerId === assign.targetLayerId
-        );
-        if (layer?.layerName.includes('shift')) {
+    for (const hk of holdKeyBinds) {
+      const { virtualKey: vk, attachedModifiers: mods } = hk;
+      if (vk in modFlags) {
+        modFlags[vk as ModifierVirtualKey] = true;
+      } else {
+        if (mods) {
+          mods.forEach((modVk) => {
+            modFlags[modVk] = true;
+          });
+        }
+        const hk = HidKeyCodes[vk];
+        const withShift = (hk & 0x100) > 0;
+        const cancelShift = (hk & 0x200) > 0;
+        if (withShift) {
           modFlags.K_Shift = true;
         }
-      }
-      if (assign.type === 'keyInput') {
-        const vk = assign.virtualKey;
-        if (vk in modFlags) {
-          modFlags[vk as ModifierVirtualKey] = true;
-        } else {
-          const mods = assign.attachedModifiers;
-          if (mods) {
-            mods.forEach((modVk) => {
-              modFlags[modVk] = true;
-            });
-          }
-          const hk = HidKeyCodes[vk];
-          const withShift = (hk & 0x100) > 0;
-          const cancelShift = (hk & 0x200) > 0;
-          if (withShift) {
-            modFlags.K_Shift = true;
-          }
-          const hasExModifiers =
-            modFlags.K_Ctrl || modFlags.K_Alt || modFlags.K_OS;
-          if (!hasExModifiers && cancelShift) {
-            modFlags.K_Shift = false;
-          }
-          hidKeyCodes.push(hk & 0xff);
+        const hasExModifiers =
+          modFlags.K_Ctrl || modFlags.K_Alt || modFlags.K_OS;
+        if (!hasExModifiers && cancelShift) {
+          modFlags.K_Shift = false;
         }
+        hidKeyCodes.push(hk & 0xff);
       }
     }
 
@@ -105,28 +107,35 @@ namespace ModuleP {
     return [modifierBits, 0, ...[...hidKeyCodes, 0, 0, 0, 0, 0, 0].slice(0, 6)];
   }
 
-  let prevReport: number[] = [];
+  interface HoldKeyBind {
+    keyId: string;
+    virtualKey: VirtualKey;
+    attachedModifiers: ModifierVirtualKey[];
+  }
+
+  const local = new (class {
+    holdKeyBinds: HoldKeyBind[] = [];
+  })();
+
+  export function pushHoldKeyBind(
+    keyId: string,
+    virtualKey: VirtualKey,
+    attachedModifiers: ModifierVirtualKey[]
+  ) {
+    local.holdKeyBinds.push({ keyId, virtualKey, attachedModifiers });
+  }
+
+  export function removeHoldKeyBind(keyId: string) {
+    Arrays.removeIf(local.holdKeyBinds, (hk) => hk.keyId === keyId);
+  }
 
   export function updateOutputReport() {
-    const bindingInfos = Object.values(logicSimulatorStateC.keyBindingInfoDict);
-    bindingInfos.sort((a, b) => a.timeStamp - b.timeStamp);
-    const hidReport = reduceBindingInfosToHidReport(bindingInfos);
-
-    // dirty fix for modifier transition delay problem
-    if (
-      prevReport[0] &&
-      !hidReport[0] &&
-      hidReport.slice(2).some((val) => val > 0)
-    ) {
-      ModuleW.commitHidReport([0, 0, 0, 0, 0, 0, 0, 0]);
-    }
-    ModuleW.commitHidReport(hidReport);
-
-    prevReport = hidReport;
+    const hidReport = reduceHoldKeyBindsToHidReport(local.holdKeyBinds);
+    ModuleW_HidReportOutputBuffer.commitHidReport(hidReport);
   }
 }
 
-namespace ModuleF {
+namespace ModuleF_KeyEventPrioritySorter {
   type CommitEventType =
     | {
         type: 'down';
@@ -192,16 +201,34 @@ namespace ModuleF {
     commitEventQueue: CommitEventType[] = [];
   })();
 
+  function isShiftLayer(targetLayerId: string) {
+    const layer = logicSimulatorStateC.editModel.layers.find(
+      (la) => la.layerId === targetLayerId
+    );
+    return layer?.layerName.includes('shift');
+  }
+
   function handleCommitEvent(ev: CommitEventType) {
     const { keyBindingInfoDict } = logicSimulatorStateC;
     if (ev.type === 'down') {
       const { keyId, assign } = ev;
       console.log('down', ev.keyId, assign);
       keyBindingInfoDict[keyId] = { assign, timeStamp: Date.now() };
-    }
-    if (ev.type === 'up') {
+      if (assign.type === 'keyInput') {
+        ModuleP_OutputKeyStateCombiner.pushHoldKeyBind(
+          keyId,
+          assign.virtualKey,
+          assign.attachedModifiers || []
+        );
+      } else if (assign.type === 'layerCall') {
+        if (isShiftLayer(assign.targetLayerId)) {
+          ModuleP_OutputKeyStateCombiner.pushHoldKeyBind(keyId, 'K_Shift', []);
+        }
+      }
+    } else {
       const { keyId } = ev;
       // console.log('up', ev.keyId);
+      ModuleP_OutputKeyStateCombiner.removeHoldKeyBind(keyId);
       delete keyBindingInfoDict[keyId];
     }
   }
@@ -239,7 +266,7 @@ namespace ModuleF {
   }
 }
 
-namespace ModuleA {
+namespace Module_KeyInputAssignBinder {
   function getKeyId(keyIndex: number): string | undefined {
     const kp = logicSimulatorStateC.editModel.keyboardShape.keyUnits.find(
       (key) => key.keyIndex === keyIndex
@@ -298,7 +325,7 @@ namespace ModuleA {
         if (assign) {
           const sortOrderVirtualKey =
             (assign.type === 'keyInput' && assign.virtualKey) || 'K_NONE';
-          ModuleF.pushCommitEvent({
+          ModuleF_KeyEventPrioritySorter.pushCommitEvent({
             type: 'down',
             keyId,
             assign,
@@ -307,7 +334,7 @@ namespace ModuleA {
           });
         }
       } else {
-        ModuleF.pushCommitEvent({
+        ModuleF_KeyEventPrioritySorter.pushCommitEvent({
           type: 'up',
           keyId,
           priorityVirtualKey: 'K_NONE',
@@ -318,9 +345,9 @@ namespace ModuleA {
   }
 
   export function processTicker() {
-    const needUpdate = ModuleF.readQueuedEvents();
+    const needUpdate = ModuleF_KeyEventPrioritySorter.readQueuedEvents();
     if (needUpdate) {
-      ModuleP.updateOutputReport();
+      ModuleP_OutputKeyStateCombiner.updateOutputReport();
     }
   }
 }
@@ -328,7 +355,6 @@ namespace ModuleA {
 export class InputLogicSimulatorC implements IInputLogicSimulator {
   private tikerTimer = new IntervalTimerWrapper();
   private tikerTimer2 = new IntervalTimerWrapper();
-  // private sorterIntervalTimer = new IntervalTimerWrapper();
 
   private onProfileStatusChanged = (
     changedStatus: Partial<IProfileManagerStatus>
@@ -342,16 +368,16 @@ export class InputLogicSimulatorC implements IInputLogicSimulator {
     if (event.type === 'keyStateChanged') {
       const { keyIndex, isDown } = event;
       const ev0 = { keyIndex, isDown };
-      ModuleA.processEvents(ev0);
+      Module_KeyInputAssignBinder.processEvents(ev0);
     }
   };
 
   private processTicker = () => {
-    ModuleA.processTicker();
+    Module_KeyInputAssignBinder.processTicker();
   };
 
   private processFastTicker = () => {
-    ModuleW.processTicker();
+    ModuleW_HidReportOutputBuffer.processTicker();
   };
 
   async initialize() {
@@ -359,12 +385,13 @@ export class InputLogicSimulatorC implements IInputLogicSimulator {
     appGlobal.deviceService.setSideBrainMode(true);
     appGlobal.deviceService.subscribe(this.onRealtimeKeyboardEvent);
     this.tikerTimer.start(this.processTicker, 10);
-    this.tikerTimer.start(this.processFastTicker, 2);
+    this.tikerTimer2.start(this.processFastTicker, 2);
   }
 
   async terminate() {
     appGlobal.deviceService.unsubscribe(this.onRealtimeKeyboardEvent);
     appGlobal.deviceService.setSideBrainMode(false);
     this.tikerTimer.stop();
+    this.tikerTimer2.stop();
   }
 }
