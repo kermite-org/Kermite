f319ff18675b98a0727bdf9763b3c0890cfd8c48
yahiro <yahiro07@users.noreply.github.com>
2020-05-31 21:53:41 +0900

    レポートデスクリプタを調整

--- firmware/kermite_firmware_atmega32u4/src/modules/usbiocore.c
+++ firmware/kermite_firmware_atmega32u4/src/modules/usbiocore.c
@@ -233,38 +233,38 @@ static uint8_t const PROGMEM rawhid_hid_report_desc[] = {
 
 // Keyboard Protocol 1, HID 1.11 spec, Appendix B, page 59-60
 static uint8_t const PROGMEM keyboard_hid_report_desc[] = {
-  0x05, 0x01, // Usage Page (Generic Desktop),
-  0x09, 0x06, // Usage (Keyboard),
-  0xA1, 0x01, // Collection (Application),
-  0x75, 0x01, //   Report Size (1),
-  0x95, 0x08, //   Report Count (8),
-  0x05, 0x07, //   Usage Page (Key Codes),
-  0x19, 0xE0, //   Usage Minimum (224),
-  0x29, 0xE7, //   Usage Maximum (231),
-  0x15, 0x00, //   Logical Minimum (0),
-  0x25, 0x01, //   Logical Maximum (1),
-  0x81, 0x02, //   Input (Data, Variable, Absolute), ;Modifier byte
-  0x95, 0x01, //   Report Count (1),
-  0x75, 0x08, //   Report Size (8),
-  0x81, 0x03, //   Input (Constant),                 ;Reserved byte
-  0x95, 0x05, //   Report Count (5),
-  0x75, 0x01, //   Report Size (1),
-  0x05, 0x08, //   Usage Page (LEDs),
-  0x19, 0x01, //   Usage Minimum (1),
-  0x29, 0x05, //   Usage Maximum (5),
-  0x91, 0x02, //   Output (Data, Variable, Absolute), ;LED report
-  0x95, 0x01, //   Report Count (1),
-  0x75, 0x03, //   Report Size (3),
-  0x91, 0x03, //   Output (Constant),                 ;LED report padding
-  0x95, 0x06, //   Report Count (6),
-  0x75, 0x08, //   Report Size (8),
-  0x15, 0x00, //   Logical Minimum (0),
-  0x25, 0xFF, //   Logical Maximum(104),
-  0x05, 0x07, //   Usage Page (Key Codes),
-  0x19, 0x00, //   Usage Minimum (0),
-  0x29, 0xFF, //   Usage Maximum (104),
-  0x81, 0x00, //   Input (Data, Array),
-  0xc0        // End Collection
+  0x05, 0x01,       // Usage Page (Generic Desktop),
+  0x09, 0x06,       // Usage (Keyboard),
+  0xA1, 0x01,       // Collection (Application),
+  0x75, 0x01,       //   Report Size (1),
+  0x95, 0x08,       //   Report Count (8),
+  0x05, 0x07,       //   Usage Page (Key Codes),
+  0x19, 0xE0,       //   Usage Minimum (224),
+  0x29, 0xE7,       //   Usage Maximum (231),
+  0x15, 0x00,       //   Logical Minimum (0),
+  0x25, 0x01,       //   Logical Maximum (1),
+  0x81, 0x02,       //   Input (Data, Variable, Absolute), ;Modifier byte
+  0x95, 0x01,       //   Report Count (1),
+  0x75, 0x08,       //   Report Size (8),
+  0x81, 0x03,       //   Input (Constant),                 ;Reserved byte
+  0x95, 0x05,       //   Report Count (5),
+  0x75, 0x01,       //   Report Size (1),
+  0x05, 0x08,       //   Usage Page (LEDs),
+  0x19, 0x01,       //   Usage Minimum (1),
+  0x29, 0x05,       //   Usage Maximum (5),
+  0x91, 0x02,       //   Output (Data, Variable, Absolute), ;LED report
+  0x95, 0x01,       //   Report Count (1),
+  0x75, 0x03,       //   Report Size (3),
+  0x91, 0x03,       //   Output (Constant),                 ;LED report padding
+  0x95, 0x06,       //   Report Count (6),
+  0x75, 0x08,       //   Report Size (8),
+  0x15, 0x00,       //   Logical Minimum (0),
+  0x26, 0xFF, 0x00, // Logical maximum(255)
+  0x05, 0x07,       //   Usage Page (Key Codes),
+  0x19, 0x00,       //   Usage Minimum (0),
+  0x29, 0xFF,       //   Usage Maximum (255),
+  0x81, 0x00,       //   Input (Data, Array),
+  0xc0              // End Collection
 };
 
 const uint8_t PROGMEM mouse_hid_report_desc[] = {
