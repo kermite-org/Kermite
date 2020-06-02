b21138e950b6ddb024829056ee31982e53473417
yahiro <yahiro07@users.noreply.github.com>
2020-06-03 08:33:07 +0900

    ビットフラグ配列コピーヘルパを作成

--- firmware/kermite_firmware_atmega32u4/src/modules/generalUtils.c
+++ firmware/kermite_firmware_atmega32u4/src/modules/generalUtils.c
@@ -1,5 +1,5 @@
 #include "generalUtils.h"
-
+#include "bit_operations.h"
 #include <stdio.h>
 
 void generalUtils_debugShowBytes(uint8_t *buf, uint16_t len) {
@@ -12,6 +12,19 @@ void generalUtils_debugShowBytes(uint8_t *buf, uint16_t len) {
 void generalUtils_copyBytes(uint8_t *dst, uint8_t *src, uint16_t len) {
   for (uint16_t i = 0; i < len; i++) {
     dst[i] = src[i];
+  }
+}
+
+void generalUtils_copyBitFlagsBuf(uint8_t *dstBuf, uint8_t dstOffset, uint8_t *srcBuf, uint8_t srcOffset, uint8_t count) {
+  for (uint8_t i = 0; i < count; i++) {
+    uint8_t srcPos = srcOffset + i;
+    uint8_t bi0 = srcPos >> 3;
+    uint8_t fi0 = srcPos & 0b111;
+    uint8_t dstPos = dstOffset + i;
+    uint8_t bi1 = dstPos >> 3;
+    uint8_t fi1 = dstPos & 0b111;
+    uint8_t val = bit_read(srcBuf[bi0], fi0);
+    bit_spec(dstBuf[bi1], fi1, val);
   }
 }
 
--- firmware/kermite_firmware_atmega32u4/src/modules/generalUtils.h
+++ firmware/kermite_firmware_atmega32u4/src/modules/generalUtils.h
@@ -7,6 +7,8 @@ void generalUtils_debugShowBytes(uint8_t *buf, uint16_t len);
 
 void generalUtils_copyBytes(uint8_t *dst, uint8_t *src, uint16_t len);
 
+void generalUtils_copyBitFlagsBuf(uint8_t *dstBuf, uint8_t dstOffset, uint8_t *srcBuf, uint8_t srcOffset, uint8_t count);
+
 bool generalUtils_compareBytes(uint8_t *arr1, uint8_t *arr2, uint16_t len);
 
 #endif
