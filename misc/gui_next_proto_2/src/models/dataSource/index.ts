5eb67361eae06080b92903fa21669bb42c251d8c
yahiro <yahiro07@users.noreply.github.com>
2020-06-14 22:11:33 +0900

    使っていないコードを削除

--- misc/gui_next_proto_2/src/models/dataSource/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './interfaces/IProfileProvider';
-export * from './interfaces/IRealtimeKeyboardEventProvider';
-export * from './prototypeImpl/ProfileProvider_LocalStorage';
-export * from './prototypeImpl/RealtimeKeyboardEventProvider_DomKeyboardSimulator';
