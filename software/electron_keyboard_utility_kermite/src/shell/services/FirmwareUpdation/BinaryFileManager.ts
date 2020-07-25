8f2d60155b3a0281626fc4a443bae41f3f4feb55
yahiro <yahiro07@users.noreply.github.com>
2020-07-25 15:15:35 +0900

    バイナリファイルを列挙

---
+++ software/electron_keyboard_utility_kermite/src/shell/services/FirmwareUpdation/BinaryFileManager.ts
@@ -0,0 +1,22 @@
+import { resolveAssetsPath } from '~shell/AppEnvironment';
+import {
+  fsIsFileExists,
+  fsCreateDirectory,
+  fsListFilesInDirectory
+} from '~funcs/Files';
+
+export class BinaryFileManager {
+  static async ensureBinariesDirectoryExists() {
+    const binariesDirPath = resolveAssetsPath('dist/binaries');
+    if (!fsIsFileExists(binariesDirPath)) {
+      await fsCreateDirectory(binariesDirPath);
+    }
+  }
+
+  static async listAllBinaryFileNames(): Promise<string[]> {
+    const fileNames = await fsListFilesInDirectory(
+      resolveAssetsPath('dist/binaries')
+    );
+    return fileNames.map((fname) => fname.replace('.hex', ''));
+  }
+}
--- software/electron_keyboard_utility_kermite/src/shell/services/FirmwareUpdation/FirmwareUpdationService.ts
+++ software/electron_keyboard_utility_kermite/src/shell/services/FirmwareUpdation/FirmwareUpdationService.ts
@@ -1,6 +1,20 @@
+import { BinaryFileManager } from './BinaryFileManager';
+
 export class FirmwareUpdationService {
+  private binaryFileNames: string[] = [];
+
+  getFirmwareNamesAvailable(): string[] {
+    return this.binaryFileNames;
+  }
+
+  private async listBinaryFileNames() {
+    await BinaryFileManager.ensureBinariesDirectoryExists();
+    this.binaryFileNames = await BinaryFileManager.listAllBinaryFileNames();
+    console.log({ fnames: this.binaryFileNames });
+  }
+
   async initialize(): Promise<void> {
-    console.log(`fus initialize`);
+    this.listBinaryFileNames();
   }
 
   async terminate(): Promise<void> {}
