e19dba862f73b7d73c091aef2dcb572c599f1817
yahiro <yahiro07@users.noreply.github.com>
2020-09-19 17:44:15 +0900

    ファームウェア側のフォルダ階層からレイアウト定義を列挙

--- software/electron_keyboard_utility_kermite/src/defs/keyboardShapes/index.ts
+++ software/electron_keyboard_utility_kermite/src/defs/keyboardShapes/index.ts
@@ -1,10 +1,48 @@
-import { keyboardShape_astalia } from './astelia';
-import { keyboardShape_4x3pad } from './4x3pad';
-import { IKeyboardShape } from '~defs/ProfileData';
+import {
+  IKeyboardShape,
+  IKeyboardShapeDisplayArea,
+  IKeyUnitEntry
+} from '~defs/ProfileData';
+import * as path from 'path';
+import * as fs from 'fs';
+import glob = require('glob');
 
-export const keyboardShapes = [keyboardShape_astalia, keyboardShape_4x3pad];
+//todo: メインプロセス内のみで使用するサービスとして記述
 
-//glue code
+const keyboardShapes: IKeyboardShape[] = [];
+
+interface IKeyboardShapeSourceJson {
+  keyUnits: IKeyUnitEntry[];
+  displayArea: IKeyboardShapeDisplayArea;
+  bodyPathMarkups: string[];
+}
+
+export function initKeyboardShapeProvider() {
+  const baseDir = path.resolve(
+    '../../firmware/kermite_firmware_atmega32u4/src/projects'
+  );
+  const pattern = `${baseDir}/**/*/layout.json`;
+  glob(pattern, (err, files) => {
+    if (!err && files) {
+      files.forEach((filePath) => {
+        const relPath = filePath.replace(baseDir + '/', '');
+        const breedName = relPath.replace('/layout.json', '');
+        const content = fs.readFileSync(filePath, 'utf-8');
+        if (content) {
+          const source = JSON.parse(content) as IKeyboardShapeSourceJson;
+          const shape: IKeyboardShape = {
+            breedName,
+            keyUnits: source.keyUnits,
+            displayArea: source.displayArea,
+            bodyPathMarkupText: source.bodyPathMarkups.join(' ')
+          };
+          keyboardShapes.push(shape);
+        }
+      });
+    }
+  });
+}
+
 export function getAvailableBreedNames(): string[] {
   return keyboardShapes.map((shape) => shape.breedName);
 }
