d00dab8303c6ce00815e1fbffea63655375e3593
yahiro <yahiro07@users.noreply.github.com>
2020-09-19 18:39:22 +0900

    ユーティリティの動作確認のためasteliaのコードをサブ階層に複製

---
+++ firmware/kermite_firmware_atmega32u4/src/projects/csp/astelia_dev/rules.mk
@@ -0,0 +1,12 @@
+MODULE_SRCS += pio.c
+MODULE_SRCS += debug_uart.c
+MODULE_SRCS += KeyMatrixScanner2.c
+MODULE_SRCS += usbiocore.c
+MODULE_SRCS += configuratorServant.c
+MODULE_SRCS += xf_eeprom.c
+MODULE_SRCS += generalUtils.c
+MODULE_SRCS += ConfigurationMemoryReader.c
+#MODULE_SRCS += keyboardCoreLogic.c
+MODULE_SRCS += keyboardCoreLogic2_Dual.c
+
+PROJECT_SRCS += main.c
