ba01ec20b7e301605653e6da1181e29666a096cf
yahiro <yahiro07@users.noreply.github.com>
2020-09-22 06:35:37 +0900

    ページの振り分けを実装

--- software/electron_keyboard_utility_kermite/src/ui2/views/root/ConfiguratorSite/ConfiguratorSiteRoot.tsx
+++ software/electron_keyboard_utility_kermite/src/ui2/views/root/ConfiguratorSite/ConfiguratorSiteRoot.tsx
@@ -1,55 +1,29 @@
 import { css } from 'goober';
-import { KeyAssignEditPage } from './KeyAssignEditPage/KeyAssignEditPage';
 import { h } from '~ui2/views/basis/qx';
-import { ProfileManagementPart } from './KeyAssignEditPage/ProfilesSection/ProfileManagementPart';
 import { CustomWindowFrame } from './WindowFrame/CustomWindowFrame';
-import { DeviceControlSection } from './KeyAssignEditPage/DeviceControlSection';
 import { GlobalMenuPart } from './GlobalMenu';
 import { appDomain } from '~ui2/models/zAppDomain';
 import { KeyboardShapePreviewPage } from './ShapePreviewPage/ShapePreviewPage';
-import { ProfileConfigratuionModalLayer } from './KeyAssignEditPage/ProfileConfigurationPart';
 import { uiTheme } from '~ui2/models/UiTheme';
+import { PageSignature } from '~ui2/models/UiStatusModel';
+import { FirmwareUpdationPage } from './FirmwareUpdationPage';
+import { EditorPage } from './KeyAssignEditPage/EditorPage';
+import { NavigationButtonsArea } from './NavigationButtonsArea';
 
-export function ConfiguratorSiteRootContent() {
-  const cssPageRoot = css`
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-
-    > .topRow {
-      flex-shrink: 0;
-      display: flex;
-      justify-content: space-between;
-      background: ${uiTheme.colors.clPanelBox};
-      height: 40px;
-      align-items: center;
-    }
-
-    > .mainRow {
-      flex-grow: 1;
-      display: flex;
-      flex-direction: column;
-      > * {
-        flex-grow: 1;
-      }
-    }
-  `;
-
-  return (
-    <div css={cssPageRoot}>
-      <div className="topRow">
-        <ProfileManagementPart />
-        <DeviceControlSection />
-      </div>
-      <div className="mainRow">
-        <KeyAssignEditPage />
-      </div>
-      <ProfileConfigratuionModalLayer />
-    </div>
-  );
+function getPageComponent(pageSig: PageSignature): () => JSX.Element {
+  const pageComponentMap: {
+    [key in PageSignature]: () => JSX.Element;
+  } = {
+    editor: EditorPage,
+    shapePreview: KeyboardShapePreviewPage,
+    firmwareUpdation: FirmwareUpdationPage
+  };
+  return pageComponentMap[pageSig];
 }
 
 export const ConfiguratorSiteRoot = () => {
+  const PageComponent = getPageComponent(appDomain.uiStatusModel.settings.page);
+
   const cssContentRow = css`
     background: ${uiTheme.colors.clBackground};
     color: ${uiTheme.colors.clMainText};
@@ -67,18 +41,15 @@ export const ConfiguratorSiteRoot = () => {
     flex-grow: 1;
   `;
 
-  const showEditor = appDomain.uiStatusModel.settings.page === 'editor';
-  const showShapePreview =
-    appDomain.uiStatusModel.settings.page === 'shapePreview';
   return (
     <CustomWindowFrame>
       <div css={cssContentRow}>
         <div css={cssNavigationColumn}>
           <GlobalMenuPart />
+          <NavigationButtonsArea />
         </div>
         <div css={cssMainColumn}>
-          {showEditor && <ConfiguratorSiteRootContent />}
-          {showShapePreview && <KeyboardShapePreviewPage />}
+          <PageComponent />
         </div>
       </div>
     </CustomWindowFrame>
