4c235434fe208f68cf6986e3ac7f693260ff45a0
yahiro <yahiro07@users.noreply.github.com>
2020-09-22 13:39:13 +0900

    KeyboardShapesProviderをリファクタリング

--- software/electron_keyboard_utility_kermite/src/shell/services/KeyboardShapesProvider.ts
+++ software/electron_keyboard_utility_kermite/src/shell/services/KeyboardShapesProvider.ts
@@ -4,8 +4,7 @@ import {
   IKeyUnitEntry
 } from '~defs/ProfileData';
 import * as path from 'path';
-import * as fs from 'fs';
-import glob = require('glob');
+import { fsxReadJsonFile, globAsync } from '~funcs/Files';
 
 interface IKeyboardShapeSourceJson {
   keyUnits: IKeyUnitEntry[];
@@ -13,38 +12,30 @@ interface IKeyboardShapeSourceJson {
   bodyPathMarkups: string[];
 }
 
+const baseDir = path.resolve(
+  '../../firmware/kermite_firmware_atmega32u4/src/projects'
+);
+
+async function loadShapeFromFile(filePath: string): Promise<IKeyboardShape> {
+  const relPath = filePath.replace(baseDir + '/', '');
+  const breedName = relPath.replace('/layout.json', '');
+  const source = (await fsxReadJsonFile(filePath)) as IKeyboardShapeSourceJson;
+  return {
+    breedName,
+    keyUnits: source.keyUnits,
+    displayArea: source.displayArea,
+    bodyPathMarkupText: source.bodyPathMarkups.join(' ')
+  };
+}
+
+async function loadKeyboardShapes(): Promise<IKeyboardShape[]> {
+  const pattern = `${baseDir}/**/*/layout.json`;
+  const files = await globAsync(pattern);
+  return await Promise.all(files.map(loadShapeFromFile));
+}
+
 export class KeyboardShapesProvider {
   keyboardShapes: IKeyboardShape[] = [];
-  breedNames: string[] = [];
-
-  loadShapes() {
-    const baseDir = path.resolve(
-      '../../firmware/kermite_firmware_atmega32u4/src/projects'
-    );
-    const pattern = `${baseDir}/**/*/layout.json`;
-
-    this.keyboardShapes = [];
-
-    glob(pattern, (err, files) => {
-      if (!err && files) {
-        files.forEach((filePath) => {
-          const relPath = filePath.replace(baseDir + '/', '');
-          const breedName = relPath.replace('/layout.json', '');
-          const content = fs.readFileSync(filePath, 'utf-8');
-          if (content) {
-            const source = JSON.parse(content) as IKeyboardShapeSourceJson;
-            const shape: IKeyboardShape = {
-              breedName,
-              keyUnits: source.keyUnits,
-              displayArea: source.displayArea,
-              bodyPathMarkupText: source.bodyPathMarkups.join(' ')
-            };
-            this.keyboardShapes.push(shape);
-          }
-        });
-      }
-    });
-  }
 
   getAvailableBreedNames(): string[] {
     return this.keyboardShapes.map((shape) => shape.breedName);
@@ -55,7 +46,7 @@ export class KeyboardShapesProvider {
   }
 
   async initialize() {
-    this.loadShapes();
+    this.keyboardShapes = await loadKeyboardShapes();
   }
 
   async terminate() {}
