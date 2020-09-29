3c505fe1f2139edaf724e6266504e42ae44008b6
yahiro <yahiro07@users.noreply.github.com>
2020-09-29 17:44:06 +0900

    AppEntryをindex.tsに統合

--- software/electron_keyboard_utility_kermite/src/shell/index.ts
+++ software/electron_keyboard_utility_kermite/src/shell/index.ts
@@ -1,3 +1,29 @@
-import { AppEntry } from './AppEntry';
+import { app } from 'electron';
+import { appWindowManager } from './base/AppWindowManager';
+import { eventBus } from './base/AppEnvironment';
+import { services } from './services';
 
-new AppEntry().start();
+function startApplication() {
+  console.log('debug v0126a');
+
+  app.allowRendererProcessReuse = true;
+
+  app.on('ready', async () => {
+    await services.initialize();
+    appWindowManager.openMainWindow();
+  });
+
+  app.on('window-all-closed', async () => {
+    await services.terminate();
+    app.quit();
+  });
+
+  app.on('browser-window-focus', () => {
+    eventBus.emit('appWindowEvent', { activeChanged: true });
+  });
+  app.on('browser-window-blur', () => {
+    eventBus.emit('appWindowEvent', { activeChanged: false });
+  });
+}
+
+startApplication();
