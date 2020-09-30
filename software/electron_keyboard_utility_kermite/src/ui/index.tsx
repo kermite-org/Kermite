ca933cdaff026f317d1a4fc127b6fe706521fc74
yahiro <yahiro07@users.noreply.github.com>
2020-09-30 21:09:27 +0900

    appEntryをindexに統合

--- software/electron_keyboard_utility_kermite/src/ui/index.tsx
+++ software/electron_keyboard_utility_kermite/src/ui/index.tsx
@@ -1,2 +1,18 @@
-import './appEntry';
-// import './views/dev0';
+import { models } from './models';
+import { initialzeView, finalizeView } from './views';
+import { dumpXpcSubscriptionsRemained } from './models/dataSource/ipc';
+
+async function start() {
+  console.log('start');
+
+  await models.initialize();
+  initialzeView();
+
+  window.addEventListener('beforeunload', async () => {
+    finalizeView();
+    await models.terminate();
+    dumpXpcSubscriptionsRemained();
+  });
+}
+
+window.addEventListener('load', start);
