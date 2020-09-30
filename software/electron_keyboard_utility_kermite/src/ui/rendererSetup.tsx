938b5717bf5eb23cf77fe111c56d11138ccbd025
yahiro <yahiro07@users.noreply.github.com>
2020-09-30 22:25:13 +0900

    フォルダ名を変更

--- software/electron_keyboard_utility_kermite/src/ui/rendererSetup.tsx
+++ software/electron_keyboard_utility_kermite/src/ui/rendererSetup.tsx
@@ -1,6 +1,6 @@
 import { appUi } from '~ui/models/appUi';
 import { h, render, rerender as qxRerender } from '~lib/qx';
-import { ViewRoot } from './pages/ViewRoot';
+import { ViewRoot } from './views/ViewRoot';
 import { thinningListenerCall } from '~funcs/Utils';
 
 export function initialzeRenderer() {
