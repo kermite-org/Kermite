2abb22ee55abffaf95d566dbcda17e85612731e0
yahiro <yahiro07@users.noreply.github.com>
2020-09-30 22:41:01 +0900

    stub dataSourceを削除

--- software/electron_keyboard_utility_kermite/src/ui/models/dataSource/interfaces/IProfileProvider.ts
+++
@@ -1,6 +0,0 @@
-import { IProfileData } from '~defs/ProfileData';
-
-export interface IProfileProvider {
-  loadProfile(): IProfileData | undefined;
-  saveProfile(profile: IProfileData): void;
-}
--- software/electron_keyboard_utility_kermite/src/ui/models/dataSource/interfaces/IRealtimeKeyboardEventProvider.ts
+++
@@ -1,7 +0,0 @@
-import { IRealtimeKeyboardEvent } from '~defs/IpcContract';
-
-export interface IUiRealtimeKeyboardEventProvider {
-  setListener(listener: (event: IRealtimeKeyboardEvent) => void): void;
-  start(): void;
-  stop(): void;
-}
