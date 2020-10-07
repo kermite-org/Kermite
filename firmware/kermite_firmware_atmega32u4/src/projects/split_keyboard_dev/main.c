363b0fca291aff009d643fd19a34bbe2ff372155
yahiro <yahiro07@users.noreply.github.com>
2020-10-07 22:11:12 +0900

    keyIndexテーブルを変更

--- firmware/kermite_firmware_atmega32u4/src/projects/split_keyboard_dev/main.c
+++ firmware/kermite_firmware_atmega32u4/src/projects/split_keyboard_dev/main.c
@@ -140,7 +140,7 @@ static const uint8_t columnPins[NumColumns] = { P_F4, P_F5, P_F6, P_F7, P_B1, P_
 
 #define SingleWireMaxPacketSize 6 //NumKeySlotBytesHalf + 1
 
-#define KeyIndexRange 78
+#define KeyIndexRange 80
 
 // clang-format off
 static const int8_t keySlotIndexToKeyIndexMap[NumKeySlots] PROGMEM = {
@@ -151,11 +151,11 @@ static const int8_t keySlotIndexToKeyIndexMap[NumKeySlots] PROGMEM = {
   24, 25, 26, 27, 28, 29, 30, 31,
   32, 33, 34, 35, -1, 36, 37, 38,
   //right
-  39, 40, 41, 42, 43, 44, 45, 46, 
-  47, 48, 49, 50, 51, 52, 53, 54, 
-  55, 56, 57, 58, 59, 60, 61, 62, 
-  63, 64, 65, 66, 67, 68, 69, 70,
-  71, 72, 73, 74, -1, 75, 76, 77
+  40, 41, 42, 43, 44, 45, 46, 47, 
+  48, 49, 50, 51, 52, 53, 54, 55, 
+  56, 57, 58, 59, 60, 61, 62, 63, 
+  64, 65, 66, 67, 68, 69, 70, 71, 
+  72, 73, 74, 75, -1, 76, 77, 78
 };
 // clang-format on
 
