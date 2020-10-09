f1c4101d1fe3af49cb6f5cdbec50802083b85420
yahiro <yahiro07@users.noreply.github.com>
2020-10-10 06:39:44 +0900

    フォームの値を読み取るヘルパを追加

--- software/electron_keyboard_utility_kermite/src/ui/views/base/FormHelpers.ts
+++ software/electron_keyboard_utility_kermite/src/ui/views/base/FormHelpers.ts
@@ -4,6 +4,12 @@ export function reflectValue<T extends string>(proc: (value: T) => void) {
   };
 }
 
+export function reflectChecked(proc: (checked: boolean) => void) {
+  return (e: Event) => {
+    proc((e.currentTarget as HTMLInputElement).checked as any);
+  };
+}
+
 export function reflectFieldValue<T>(target: T, fieldName: keyof T) {
   return (e: Event) => {
     target[fieldName] = (e.currentTarget as HTMLInputElement).value as any;
