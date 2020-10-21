6b78902b4d2378b8ddb0427efd775c3be9307d80
yahiro <yahiro07@users.noreply.github.com>
2020-10-22 00:10:42 +0900

    値の定義をconfig.hに移動

--- kermite_firmware/src/projects/proto/minivers_gsk/config.h
+++ kermite_firmware/src/projects/proto/minivers_gsk/config.h
@@ -3,4 +3,7 @@
 #define GSK_NUM_ROWS 5
 #define GSK_NUM_COLUMNS 8
 
+#define SINGLEWIRE_SIGNAL_PIN_PD2
+#define CORELOGIC_NUM_KEYSLOTS 80
+
 #endif
--- kermite_firmware/src/projects/proto/minivers_gsk/rules.mk
+++ kermite_firmware/src/projects/proto/minivers_gsk/rules.mk
@@ -1,17 +1,13 @@
-PROJECT_SRCS += main.c
-
 MODULE_SRCS += pio.c
-MODULE_SRCS += debug_uart.c
-MODULE_SRCS += KeyMatrixScanner2.c
 MODULE_SRCS += usbiocore.c
-MODULE_SRCS += configuratorServant.c
 MODULE_SRCS += xf_eeprom.c
+MODULE_SRCS += debug_uart.c
 MODULE_SRCS += generalUtils.c
+MODULE_SRCS += KeyMatrixScanner2.c
+MODULE_SRCS += configuratorServant.c
 MODULE_SRCS += ConfigurationMemoryReader.c
-
 MODULE_SRCS += singlewire3.c
 MODULE_SRCS += keyboardCoreLogic2_Dual.c
 MODULE_SRCS += GeneralSplitKeyboard.c
 
-CFLAGS += -DSINGLEWIRE_SIGNAL_PIN_PD2
-CFLAGS += -DCORELOGIC_KEYSLOTS_NUM=80
+PROJECT_SRCS += main.c
