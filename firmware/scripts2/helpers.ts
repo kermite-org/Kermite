d413e774568afb88845e0ee65206c1fd9b93b555
yahiro <yahiro07@users.noreply.github.com>
2020-11-23 13:52:56 +0900

    layout.jsonのスキーマバリデーションを実装

--- firmware/scripts2/helpers.ts
+++ firmware/scripts2/helpers.ts
@@ -4,6 +4,11 @@ export function fsxReadTextFile(fpath: string) {
   return fs.readFileSync(fpath, { encoding: "utf-8" });
 }
 
+export function fsxReadJsonFile(fpath: string) {
+  const content = fs.readFileSync(fpath, { encoding: "utf-8" });
+  return JSON.parse(content);
+}
+
 export function getMatched(text: string, pattern: RegExp): string | undefined {
   const m = text.match(pattern);
   return (m && m[1]) || undefined;
@@ -15,4 +20,16 @@ export function checkHasFields<T>(obj: T, keys: (keyof T)[]) {
 
 export function compareObjectByJsonStringifyParse(a: any, b: any) {
   return JSON.stringify(a) == JSON.stringify(b);
+}
+
+export function createObjectFromKeyValues<K extends string | number, V>(
+  arr: [K, V][]
+): { [key in K]: V } {
+  const obj: { [key in K]: V } = {} as any;
+  arr.forEach((el) => {
+    const key = el[0];
+    const value = el[1];
+    obj[key] = value;
+  });
+  return obj;
 }
