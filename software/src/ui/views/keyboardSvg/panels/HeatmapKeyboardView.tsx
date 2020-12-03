f4e9722edea8cd74c879ca58d29e9c08967a01b8
yahiro <yahiro07@users.noreply.github.com>
2020-12-03 21:07:18 +0900

    ビューを実装

---
+++ software/src/ui/views/keyboardSvg/panels/HeatmapKeyboardView.tsx
@@ -0,0 +1,50 @@
+import { css } from 'goober';
+import { h } from '~lib/qx';
+import { ViewModelProps } from '~ui/base/helper/mvvmHelpers';
+import { IRealtimeHeatmapKeyboardViewModel } from '~ui/viewModels/RealtimeHeatmapViewModel';
+import { KeyboardBodyShape } from '~ui/views/keyboardSvg/atoms/KeyboardBodyShape';
+import { HeatmapKeyUnitCard } from '~ui/views/keyboardSvg/molecules/HeatmapKeyUnitCard';
+import { KeyboardSvgFrameWithAutoScaler } from '~ui/views/keyboardSvg/outlines/KeyboardSvgFrameWithAutoScaler';
+
+export const HeatmapKeyboardView = ({
+  vm
+}: ViewModelProps<IRealtimeHeatmapKeyboardViewModel>) => {
+  const cssKeyboardShapeView = css`
+    height: 300px;
+    overflow: hidden;
+    background: #ccc;
+  `;
+
+  const dpiScale = 2;
+  const marginRatio = 0.06;
+  const baseStrokeWidth = 0.3;
+
+  const fillColor = '#FFF';
+  const strokeColor = '#888';
+
+  return (
+    <div css={cssKeyboardShapeView}>
+      <KeyboardSvgFrameWithAutoScaler
+        displayArea={vm.displayArea}
+        dpiScale={dpiScale}
+        marginRatio={marginRatio}
+        baseStrokeWidth={baseStrokeWidth}
+      >
+        <KeyboardBodyShape
+          outerPaths={vm.bodyPathMarkupText}
+          fillColor={fillColor}
+          strokeColor={strokeColor}
+        />
+        <g>
+          {vm.cardsVM.map((keyUnit) => (
+            <HeatmapKeyUnitCard
+              model={keyUnit}
+              key={keyUnit.keyUnitId}
+              qxOptimizer="deepEqual"
+            />
+          ))}
+        </g>
+      </KeyboardSvgFrameWithAutoScaler>
+    </div>
+  );
+};
--- software/src/ui/views/pages/HeatmapPage.tsx
+++ software/src/ui/views/pages/HeatmapPage.tsx
@@ -1,6 +1,9 @@
 import { css } from 'goober';
 import { h } from '~lib/qx';
 import { uiTheme } from '~ui/core';
+import { makeRealtimeHeatmapViewModel } from '~ui/viewModels/RealtimeHeatmapViewModel';
+import { GeneralButton } from '~ui/views/controls/GeneralButton';
+import { HeatmapKeyboardView } from '~ui/views/keyboardSvg/panels/HeatmapKeyboardView';
 
 const cssHeatmapPage = css`
   background: ${uiTheme.colors.clBackground};
@@ -11,8 +14,46 @@ const cssHeatmapPage = css`
   > * + * {
     margin-top: 10px;
   }
+
+  .headRow {
+    display: flex;
+    align-items: center;
+
+    .text {
+      font-size: 18px;
+    }
+
+    > * + * {
+      margin-left: 20px;
+    }
+  }
 `;
 
 export const HeatmapPage = () => {
-  return <div css={cssHeatmapPage}>Realtime Heatmap</div>;
+  const vm = makeRealtimeHeatmapViewModel();
+
+  if (!vm.keyboardVM.displayArea) {
+    return <div>incompatible keyboardShape, displayArea is not defined</div>;
+  }
+
+  return (
+    <div css={cssHeatmapPage}>
+      <div>Realtime Heatmap</div>
+      <div class="headRow">
+        <GeneralButton
+          handler={vm.startRecording}
+          text="start"
+          disabled={vm.isRecording}
+        />
+        <GeneralButton
+          handler={vm.stopRecording}
+          text="stop"
+          disabled={!vm.isRecording}
+        />
+        <div class="text">{vm.timeText}</div>
+        <div class="text">{vm.numTotalTypes}</div>
+      </div>
+      <HeatmapKeyboardView vm={vm.keyboardVM} />
+    </div>
+  );
 };
