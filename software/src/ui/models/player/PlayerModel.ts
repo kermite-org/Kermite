7cae458cca2e8c36aa5c83d2fc0a537a11c51441
yahiro <yahiro07@users.noreply.github.com>
2020-12-03 21:33:55 +0900

    RealtimeKeyboardEventProviderを削除

--- software/src/ui/models/player/PlayerModel.ts
+++ software/src/ui/models/player/PlayerModel.ts
@@ -1,9 +1,8 @@
 import { IRealtimeKeyboardEvent } from '~defs/IpcContract';
+import { backendAgent } from '~ui/core';
 import { EditorModel } from '../editor/EditorModel';
-import { RealtimeKeyboardEventProvider } from './RealtimeKeyboardEventProvider';
 
 export class PlayerModel {
-  private keyEventProvider = new RealtimeKeyboardEventProvider();
   private _keyStates: { [keyId: string]: boolean } = {};
   private _layerActiveFlags: number = 1;
   private _holdKeyIndices: Set<number> = new Set();
@@ -82,11 +81,10 @@ export class PlayerModel {
   };
 
   initialize() {
-    this.keyEventProvider.setListener(this.handlekeyEvents);
-    this.keyEventProvider.start();
+    backendAgent.keyEvents.subscribe(this.handlekeyEvents);
   }
 
   finalize() {
-    this.keyEventProvider.stop();
+    backendAgent.keyEvents.unsubscribe(this.handlekeyEvents);
   }
 }
