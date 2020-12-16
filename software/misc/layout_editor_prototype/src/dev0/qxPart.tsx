bc21c4c9929b5a5d11044192c67a3ba5a4e3be37
yahiro <yahiro07@users.noreply.github.com>
2020-12-17 01:02:19 +0900

    Hooksによるテキスト編集状態管理の実験コードを追加

---
+++ software/misc/layout_editor_prototype/src/dev0/qxPart.tsx
@@ -0,0 +1,78 @@
+import { reflectValue } from '~/FormHelpers';
+import { h, Hook, render } from '~/qx';
+
+const Counter = () => {
+  const [count, setCount] = Hook.useState(0);
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>inc</button>
+      <span>{count}</span>
+    </div>
+  );
+};
+
+const editorState = {
+  userName: 'yamada',
+};
+
+const TextInput = (props: {
+  value: string;
+  handleChange: (value: string) => void;
+}) => {
+  const sourceValue = props.value;
+  const [editText, setEditText] = Hook.useState(sourceValue);
+
+  Hook.useEffect(() => {
+    setEditText(sourceValue);
+  }, [sourceValue]);
+
+  const onChange = () => {
+    props.handleChange(editText);
+    setEditText(sourceValue);
+  };
+
+  return (
+    <input
+      type="text"
+      value={editText}
+      onInput={reflectValue(setEditText)}
+      onChange={onChange}
+    />
+  );
+};
+
+const NameEditor = () => {
+  const onChange = (text: string) => {
+    if (text.length < 8) {
+      editorState.userName = text;
+      console.log({ text });
+    }
+  };
+
+  const setRandom = () => {
+    editorState.userName = `user-${(Math.random() * 1000) >> 0}`;
+  };
+
+  return (
+    <div>
+      <TextInput value={editorState.userName} handleChange={onChange} />
+      <button onClick={setRandom}>random</button>
+    </div>
+  );
+};
+
+// const renderCount = 0;
+const PageRoot = () => {
+  // console.log(`render ${renderCount++}`);
+  return (
+    <div style={{ border: 'solid 1px #888' }}>
+      <div>qx part</div>
+      <div>hello2</div>
+      <Counter />
+      <NameEditor />
+    </div>
+  );
+};
+export function startQxPart() {
+  render(() => <PageRoot />, document.getElementById('app2'));
+}
--- software/misc/layout_editor_prototype/src/index.tsx
+++ software/misc/layout_editor_prototype/src/index.tsx
@@ -1,21 +1,2 @@
-import { css } from 'goober';
-import { initializeCss } from '~/cssInitializer';
-import { h, render } from '~/qx';
-
-const cssPageRoot = css`
-  border: solid 2px #f08;
-  padding: 10px;
-  height: 100%;
-`;
-
-const PageRoot = () => {
-  return <div css={cssPageRoot}>layout editor proto</div>;
-};
-
-window.addEventListener('load', () => {
-  console.log('start');
-
-  initializeCss();
-
-  render(() => <PageRoot />, document.getElementById('app'));
-});
+import './uiAppEntry';
+// import './dev0/uiDev0Entry';
