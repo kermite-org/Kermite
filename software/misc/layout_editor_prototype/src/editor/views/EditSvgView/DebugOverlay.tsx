bc3ff4cb1aec51d44386be7b60044046e6b7902a
yahiro <yahiro07@users.noreply.github.com>
2020-12-31 22:17:47 +0900

    デバッグデータを一旦非表示化

--- software/misc/layout_editor_prototype/src/editor/views/EditSvgView/DebugOverlay.tsx
+++ software/misc/layout_editor_prototype/src/editor/views/EditSvgView/DebugOverlay.tsx
@@ -13,8 +13,9 @@ export const DebugOverlay = () => {
     word-break: break-all;
     font-size: 12px;
   `;
+  const show = false;
   return (
-    <div css={cssDebugOverlay}>
+    <div css={cssDebugOverlay} qxIf={show}>
       <div>{JSON.stringify(appState.editor.design)}</div>
       <div>{JSON.stringify(appState.env)}</div>
     </div>
