635b58c62bd9f5f7f84a9f0f64736937086499e2
yahiro <yahiro07@users.noreply.github.com>
2021-01-01 23:36:05 +0900

    キーボード操作によるundo/redoに対応

--- software/misc/layout_editor_prototype/src/editor/store/KeyboardOperationHandler.ts
+++ software/misc/layout_editor_prototype/src/editor/store/KeyboardOperationHandler.ts
@@ -1,3 +1,4 @@
+import { editManager } from '~/editor/store/EditManager';
 import { editMutations } from '~/editor/store/EditMutations';
 import { editReader } from '~/editor/store/EditReader';
 import { rerender } from '~/qx';
@@ -12,6 +13,30 @@ export function setupKeyboardOperationHander() {
       } else if (currentOutlinePoint) {
         editMutations.deleteCurrentOutlinePoint();
         rerender();
+      }
+    }
+
+    const isMacOS = navigator.userAgent.toLowerCase().includes('mac os x');
+    if (isMacOS) {
+      if (e.key === 'z' && e.metaKey) {
+        if (!e.shiftKey) {
+          editManager.undo();
+        } else {
+          editManager.redo();
+        }
+        rerender();
+        e.preventDefault();
+      }
+    } else {
+      if (e.key === 'z' && e.ctrlKey) {
+        editManager.undo();
+        rerender();
+        e.preventDefault();
+      }
+      if (e.key === 'y' && e.ctrlKey) {
+        editManager.redo();
+        rerender();
+        e.preventDefault();
       }
     }
   });
