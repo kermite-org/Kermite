07dbed7512cd3cfda8fe1e119eaa0ef4def902e9
yahiro <yahiro07@users.noreply.github.com>
2021-01-03 00:08:34 +0900

    iframeによるページの入れ子構成を実験

--- software/packages/ui-editor/src/index.tsx
+++ software/packages/ui-editor/src/index.tsx
@@ -1,7 +1,38 @@
-console.log('editor');
+import { initializeCss } from '@kermite/ui';
+import { css } from 'goober';
+import { h, render } from 'qx';
 
-const el = document.getElementById('app');
-if (el) {
-  el.innerHTML = 'editor';
-}
-document.body.style.background = '#DEF';
+const cssPageRoot = css`
+  background: #ddd;
+  border: solid 2px #f08;
+  height: 100%;
+`;
+
+const cssIframe = css`
+  width: 400px;
+  height: 400px;
+`;
+const PageRoot = () => {
+  initializeCss();
+
+  return (
+    <div css={cssPageRoot}>
+      <div>UI APP ROOT</div>
+      <iframe src="./page1/index.html" css={cssIframe}></iframe>
+      <div>
+        <div>
+          <a href="./page1/index.html">move to page1</a>
+        </div>
+        <div>
+          <a href="./page1/index.html" target="_blank">
+            open page1 in new window
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+window.addEventListener('load', () => {
+  render(() => <PageRoot />, document.getElementById('app'));
+});
