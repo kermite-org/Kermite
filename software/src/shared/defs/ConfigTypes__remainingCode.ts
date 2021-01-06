cd3193c8d93d36af818cfdff651bce12975091c6
yahiro <yahiro07@users.noreply.github.com>
2021-01-07 03:04:53 +0900

    同等機能を移植/実装済みのコードを削除

--- software/src/shared/defs/ConfigTypes__remainingCode.ts
+++ software/src/shared/defs/ConfigTypes__remainingCode.ts
@@ -12,18 +12,5 @@ export interface IApplicationSettings {
 }
 
 export const fallabackApplicationSettings: IApplicationSettings = {
-  showTestInputArea: false
-};
-
-export type IKeyboardBehaviorMode = 'Standalone' | 'SideBrain';
-export type IKeyboardLayoutStandard = 'US' | 'JIS';
-
-export interface IKeyboardConfig {
-  behaviorMode: IKeyboardBehaviorMode;
-  layoutStandard: IKeyboardLayoutStandard;
-}
-
-export const fallbackKeyboardConfig: IKeyboardConfig = {
-  behaviorMode: 'Standalone',
-  layoutStandard: 'US'
+  showTestInputArea: false,
 };
