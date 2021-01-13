124f00a850894953b06b10f016311ef37d510908
yahiro <yahiro07@users.noreply.github.com>
2021-01-13 14:10:26 +0900

    qx内蔵の非同期再描画を使用

--- software/src/ui-common/base/appUi.ts
+++ software/src/ui-common/base/appUi.ts
@@ -1,4 +1,4 @@
-import { rerender as qxRerender } from 'qx';
+import { asyncRerender } from 'qx';
 
 class AppUi {
   isDevelopment = (window as any).debugConfig?.isDevelopment;
@@ -17,22 +17,9 @@ class AppUi {
     return this._debugObject;
   }
 
-  private _reqRenderAsync: boolean = false;
-
   rerender = () => {
-    this._reqRenderAsync = true;
+    asyncRerender();
   };
-
-  startAsyncRenderLoop() {
-    const renderLoop = () => {
-      if (this._reqRenderAsync) {
-        qxRerender();
-        this._reqRenderAsync = false;
-      }
-      requestAnimationFrame(renderLoop);
-    };
-    renderLoop();
-  }
 
   navigateTo(path: string) {
     location.href = path;
