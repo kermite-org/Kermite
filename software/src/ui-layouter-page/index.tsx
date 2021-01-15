119a2b07d98e7c23d2cbec6716e47bfeeeef78b2
yahiro <yahiro07@users.noreply.github.com>
2021-01-16 00:56:49 +0900

    レイアウトファイルの読み込みと保存を実装

--- software/src/ui-layouter-page/index.tsx
+++ software/src/ui-layouter-page/index.tsx
@@ -1,6 +1,7 @@
+import { ipcAgent } from '@ui-common';
 import { UiLayouterCore } from '@ui-layouter';
 import { css } from 'goober';
-import { h } from 'qx';
+import { h, rerender } from 'qx';
 
 const cssBase = css`
   height: 100%;
@@ -28,11 +29,25 @@ const cssBase = css`
   }
 `;
 export const UiLayouterPageComponent = () => {
+  const onLoadButton = async () => {
+    const data = await ipcAgent.async.file_loadObjectFromJsonWithFileDialog();
+    if (data) {
+      UiLayouterCore.loadEditDesign(data as any);
+      rerender();
+    }
+  };
+  const onSaveButton = async () => {
+    const data = UiLayouterCore.emitEditDesign();
+    const done = await ipcAgent.async.file_saveObjectToJsonWithFileDialog(data);
+    if (done) {
+      console.log('file saved');
+    }
+  };
   return (
     <div css={cssBase}>
       <div className="topRow">
-        <button>load</button>
-        <button>save</button>
+        <button onClick={onLoadButton}>load</button>
+        <button onClick={onSaveButton}>save</button>
       </div>
       <div className="mainRow">
         <UiLayouterCore.Component />
