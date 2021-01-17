474c090d640d7628c1d401b89c6e47740a0bb70a
yahiro <yahiro07@users.noreply.github.com>
2021-01-18 05:23:07 +0900

    データの永続化処理をファイルに分離

--- software/src/ui-mock-view/UiLayouterDevelopmentComponent.tsx
+++ software/src/ui-mock-view/UiLayouterDevelopmentComponent.tsx
@@ -1,40 +1,9 @@
 import { h, Hook } from 'qx';
-import { IPersistKeyboardDesign } from '~/shared';
 import { UiLayouterCore } from '~/ui-layouter';
-
-function createFallbackKeyboardDesign(): IPersistKeyboardDesign {
-  return {
-    setup: {
-      placementUnit: 'mm',
-      placementAnchor: 'center',
-      keySizeUnit: 'KP',
-    },
-    keyEntities: [],
-    outlineShapes: [],
-    transGroups: [
-      {
-        x: 0,
-        y: 0,
-        angle: 0,
-      },
-    ],
-  };
-}
-
-function loadLocalStorageKeyboardDesignOrDefault(): IPersistKeyboardDesign {
-  const text = localStorage.getItem('kermite_ui_layouter_mock_savedDesign');
-  if (text) {
-    const obj = JSON.parse(text) as IPersistKeyboardDesign;
-    return obj;
-  } else {
-    return createFallbackKeyboardDesign();
-  }
-}
-
-function saveLocalStorageKeyboardDesign(design: IPersistKeyboardDesign) {
-  const text = JSON.stringify(design);
-  localStorage.setItem('kermite_ui_layouter_mock_savedDesign', text);
-}
+import {
+  loadLocalStorageKeyboardDesignOrDefault,
+  saveLocalStorageKeyboardDesign,
+} from '~/ui-mock-view/LocalStoragePersistKeyboardDesign';
 
 export const UiLayouterDevelopmentComponent = () => {
   Hook.useEffect(() => {
