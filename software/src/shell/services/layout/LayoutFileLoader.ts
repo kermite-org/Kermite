e86fe80d9525a5916aa85eef8278ed091d1fbcb0
yahiro <yahiro07@users.noreply.github.com>
2021-01-23 02:08:03 +0900

    ファイルの読み込み処理を実装

--- software/src/shell/services/layout/LayoutFileLoader.ts
+++ software/src/shell/services/layout/LayoutFileLoader.ts
@@ -1,13 +1,19 @@
 import { IPersistKeyboardDesign } from '~/shared';
+import { fsxReadJsonFile, fsxWriteJsonFile } from '~/shell/funcs';
 import { ILayoutFileLoader } from '~/shell/services/layout/interfaces';
 
 class LayoutFileLoader implements ILayoutFileLoader {
-  async loadLayoutFromFile(filePath: string): Promise<IPersistKeyboardDesign> {}
+  async loadLayoutFromFile(filePath: string): Promise<IPersistKeyboardDesign> {
+    // todo: ファイルのスキーマをチェックし、互換性がないファイルの場合は例外を投げる
+    return await fsxReadJsonFile(filePath);
+  }
 
   async saveLayoutToFile(
     filePath: string,
     design: IPersistKeyboardDesign,
-  ): Promise<void> {}
+  ): Promise<void> {
+    await fsxWriteJsonFile(filePath, design);
+  }
 }
 
 export const layoutFileLoader = new LayoutFileLoader();
