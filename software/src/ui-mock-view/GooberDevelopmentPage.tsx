fce86eb701357335a4c74d39352092e2e7afc9bd
yahiro <yahiro07@users.noreply.github.com>
2021-01-26 09:39:56 +0900

    実験コードを変更

--- software/src/ui-mock-view/GooberDevelopmentPage.tsx
+++ software/src/ui-mock-view/GooberDevelopmentPage.tsx
@@ -1,14 +1,35 @@
-import { h } from 'qx';
-import { css, setup } from '~/goober_ex';
+import { h, Hook, rerender } from 'qx';
+import { css, setup } from '~/goober_ex/goober';
+// import { css2 } from '~/goober_ex/css';
 
 setup(h);
 
 const cssRoot = css`
+  label: cssRoot;
   border: solid 4px orange;
   color: green;
   padding: 10px;
 `;
 
+const boxW = 100;
+const boxH = 50;
+
+const cssHeader = css`
+  label: cssHeaderBarSpecial;
+  width: ${boxW}px;
+  height: ${boxH}px;
+  border: solid 1px blue;
+`;
+
 export const GooberDevelopmentPage = () => {
-  return <div css={cssRoot}>hello</div>;
+  Hook.useEffect(() => {
+    setTimeout(rerender, 1000);
+  }, []);
+
+  console.log('render');
+  return (
+    <div css={cssRoot}>
+      <div css={cssHeader}>hello</div>
+    </div>
+  );
 };
