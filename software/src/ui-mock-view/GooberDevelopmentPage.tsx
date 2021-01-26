54e4cbb5d0e5bdb3b6b6a2d514c67c61679f60a7
yahiro <yahiro07@users.noreply.github.com>
2021-01-26 11:08:16 +0900

    動作確認用のページを追加

---
+++ software/src/ui-mock-view/GooberDevelopmentPage.tsx
@@ -0,0 +1,29 @@
+import { css, setup } from 'goober';
+import { h } from 'qx';
+
+setup(h);
+
+const cssRoot = css`
+  label: cssRoot;
+  border: solid 4px orange;
+  color: green;
+  padding: 10px;
+`;
+
+const boxW = 100;
+const boxH = 50;
+
+const cssHeader = css`
+  label: cssHeaderBarSpecial;
+  width: ${boxW}px;
+  height: ${boxH}px;
+  border: solid 1px blue;
+`;
+
+export const GooberDevelopmentPage = () => {
+  return (
+    <div css={cssRoot}>
+      <div css={cssHeader}>hello</div>
+    </div>
+  );
+};
