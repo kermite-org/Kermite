e31588682d123c1f12e24be1548b50ac114b419d
yahiro <yahiro07@users.noreply.github.com>
2021-02-07 07:48:54 +0900

    メイン画面に戻るボタンを復元

--- software/src/ui-widget/WidgetMainPage.tsx
+++ software/src/ui-widget/WidgetMainPage.tsx
@@ -1,23 +1,53 @@
 import { css } from 'goober';
 import { h } from 'qx';
 import { WidgetSvgKeyboardView } from '~/ui-common-svg/panels/WidgetSvgKeyboardView';
+import { siteModel } from '~/ui-common/sharedModels/SiteModel';
 import { makeWidgetMainPageViewModel } from '~/ui-widget/WidgetMainPageViewModel';
 
 const cssMainpanel = css`
+  position: relative;
   user-select: none;
   -webkit-app-region: drag;
   width: 100%;
   height: 100%;
 `;
 
+const cssConfigButton = css`
+  position: absolute;
+  right: 0px;
+  top: 0px;
+  -webkit-app-region: no-drag;
+  color: #fff;
+  width: 30px;
+  height: 30px;
+  margin: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  font-size: 20px;
+  background: #888;
+
+  &:hover {
+    background: #0cf;
+  }
+`;
+
 export function MainPanel() {
   const vm = makeWidgetMainPageViewModel();
   return (
-    <div css={cssMainpanel} onDblClick={vm.backToConfiguratorView}>
+    <div css={cssMainpanel}>
       <WidgetSvgKeyboardView
         keyboardDesign={vm.keyboardVM.keyboardDesign}
         cardsPartVM={vm.keyboardVM.cardsPartVM}
       />
+      <div
+        css={cssConfigButton}
+        onClick={vm.backToConfiguratorView}
+        qxIf={siteModel.isWindowActive}
+      >
+        <i className="fa fa-cog" />
+      </div>
     </div>
   );
 }
