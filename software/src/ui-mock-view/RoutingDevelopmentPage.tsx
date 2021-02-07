e02715c93cafafc3b1f1e5149f152f94dd61f974
yahiro <yahiro07@users.noreply.github.com>
2021-02-08 05:54:16 +0900

    実験ページを追加

---
+++ software/src/ui-mock-view/RoutingDevelopmentPage.tsx
@@ -0,0 +1,60 @@
+import { css, setup } from 'goober';
+import { FC, h, Hook } from 'qx';
+import { Link, linkTo, router } from '~/ui-common/base';
+
+setup(h);
+
+const cssNavBox = css`
+  display: flex;
+  border: solid 1px red;
+  padding: 4px;
+  > :not(:first-child) {
+    margin-left: 10px;
+  }
+`;
+
+const cssNavButton = css`
+  display: inline-block;
+  border: solid 1px orange;
+  color: orange;
+  padding: 4px;
+  border-radius: 4px;
+  background: #fff;
+  cursor: pointer;
+  &:hover {
+    background: #fdc;
+  }
+`;
+
+export const RoutingDevelopmentPage: FC = () => {
+  Hook.useEffect(router.rerenderEffectOnHashChange, []);
+
+  console.log(`render`, { hash: location.hash });
+
+  const path = router.getHashPath();
+
+  return (
+    <div>
+      {path === '/foo' && (
+        <div>
+          foo page
+          <Link to="/">top</Link>
+        </div>
+      )}
+      {(path === '' || path === '/') && (
+        <div>
+          root page
+          <Link to="/foo">foo</Link>
+          <div css={cssNavBox}>
+            <Link to="/foo" className={cssNavButton}>
+              foo
+            </Link>
+            <div onClick={linkTo('/foo')} css={cssNavButton}>
+              foo
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
