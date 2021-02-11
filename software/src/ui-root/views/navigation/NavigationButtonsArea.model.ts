09f886c0013a68264969db9a62beff4f114d81f1
yahiro <yahiro07@users.noreply.github.com>
2021-02-11 22:32:47 +0900

    ナビゲーションメニューを改良

--- software/src/ui-root/views/navigation/NavigationButtonsArea.model.ts
+++ software/src/ui-root/views/navigation/NavigationButtonsArea.model.ts
@@ -3,25 +3,57 @@ import { PagePaths } from '~/ui-common/sharedModels/UiStatusModel';
 
 export interface NavigationEntryViewModel {
   pagePath: PagePaths;
-  faIconName: string;
+  pageName: string;
+  iconSpec: string;
   isCurrent: boolean;
   onClick: () => void;
 }
 
 interface NavigationEntrySource {
   pagePath: PagePaths;
-  faIconName: string;
+  pageName: string;
+  iconSpec: string;
 }
 
 const entrySources: NavigationEntrySource[] = [
-  { pagePath: '/editor', faIconName: 'fa-keyboard' },
-  { pagePath: '/layouter', faIconName: 'fa-drafting-compass' },
-  { pagePath: '/presetBrowser', faIconName: 'fa-book' },
-  { pagePath: '/shapePreview', faIconName: 'fa-file-code' },
-  { pagePath: '/heatmap', faIconName: 'fa-chart-bar' },
-  { pagePath: '/firmwareUpdation', faIconName: 'fa-microchip' },
-  { pagePath: '/settings', faIconName: 'fa-cog' },
+  { pagePath: '/editor', pageName: 'Assigner', iconSpec: 'fa fa-keyboard' },
+  {
+    pagePath: '/layouter',
+    pageName: 'Drafter',
+    iconSpec: 'fa fa-drafting-compass',
+  },
+  { pagePath: '/presetBrowser', pageName: 'Presets', iconSpec: 'fa fa-book' },
+  {
+    pagePath: '/shapePreview',
+    pageName: 'Preview',
+    iconSpec: 'fa fa-file-code',
+  },
+  { pagePath: '/heatmap', pageName: 'Heatmap', iconSpec: 'fa fa-chart-bar' },
+  {
+    pagePath: '/firmwareUpdation',
+    pageName: 'Firmware',
+    iconSpec: 'fa fa-microchip',
+  },
+  { pagePath: '/settings', pageName: 'Settings', iconSpec: 'fa fa-cog' },
 ];
+
+// const entrySources: NavigationEntrySource[] = [
+//   { pagePath: '/editor', pageName: 'Assigner', iconSpec: 'keyboard' },
+//   {
+//     pagePath: '/layouter',
+//     pageName: 'Drafter',
+//     iconSpec: 'architecture',
+//   },
+//   { pagePath: '/presetBrowser', pageName: 'Presets', iconSpec: 'menu_book' },
+//   { pagePath: '/shapePreview', pageName: 'Preview', iconSpec: 'format_shapes' },
+//   { pagePath: '/heatmap', pageName: 'Heatmap', iconSpec: 'analytics' },
+//   {
+//     pagePath: '/firmwareUpdation',
+//     pageName: 'Firmware',
+//     iconSpec: 'memory',
+//   },
+//   { pagePath: '/settings', pageName: 'Settings', iconSpec: 'settings' },
+// ];
 
 export interface INavigationViewModel {
   entries: NavigationEntryViewModel[];
@@ -32,7 +64,8 @@ export function makeNavigationViewModel(): INavigationViewModel {
   return {
     entries: entrySources.map((it) => ({
       pagePath: it.pagePath,
-      faIconName: it.faIconName,
+      pageName: it.pageName,
+      iconSpec: it.iconSpec,
       isCurrent: it.pagePath === currentPagePath,
       onClick: () => router.navigateTo(it.pagePath),
     })),
--- software/src/ui-root/views/navigation/NavigationColumn.tsx
+++ software/src/ui-root/views/navigation/NavigationColumn.tsx
@@ -7,8 +7,14 @@ import { NavigationButtonsArea } from './NavigationButtonsArea';
 const cssNavigationColumn = css`
   width: 50px;
   flex-shrink: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 10px;
   background: ${uiTheme.colors.clNavigationColumn};
-  padding: 10px;
+  > :not(:first-child) {
+    margin-top: 20px;
+  }
 `;
 
 export const NavigationColumn = () => {
