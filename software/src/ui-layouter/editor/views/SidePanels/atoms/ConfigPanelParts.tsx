910d917dcfe16e7f5edb5b23a9428ea7791313fc
yahiro <yahiro07@users.noreply.github.com>
2021-02-12 08:25:12 +0900

    styledを使用

--- software/src/ui-layouter/editor/views/SidePanels/atoms/ConfigPanelParts.tsx
+++ software/src/ui-layouter/editor/views/SidePanels/atoms/ConfigPanelParts.tsx
@@ -1,23 +1,18 @@
-import { css } from 'goober';
-import { h } from 'qx';
+import { styled } from 'goober';
 import { uiTheme } from '~/ui-common';
 
-function styledDiv(cssDiv: string) {
-  return (props: { children: any }) => <div css={cssDiv}>{props.children}</div>;
-}
-
-export const ConfigPanel = styledDiv(css`
+export const ConfigPanel = styled('div')`
   margin: 5px;
-`);
+`;
 
-export const ConfigHeader = styledDiv(css`
+export const ConfigHeader = styled('div')`
   background: ${uiTheme.colors.clPrimary};
   border: solid 1px ${uiTheme.colors.clPrimary};
   color: ${uiTheme.colors.clDecal};
   padding: 2px 5px;
-`);
+`;
 
-export const ConfigContent = styledDiv(css`
+export const ConfigContent = styled('div')`
   border: solid 1px ${uiTheme.colors.clPrimary};
   border-top: none;
   padding: 6px 8px;
@@ -25,25 +20,25 @@ export const ConfigContent = styledDiv(css`
   > * + * {
     margin-top: 5px;
   }
-`);
+`;
 
-export const ConfigSubHeader = styledDiv(css`
+export const ConfigSubHeader = styled('div')`
   margin-bottom: 4px;
-`);
+`;
 
-export const ConfigSubContent = styledDiv(css`
+export const ConfigSubContent = styled('div')`
   padding-left: 8px;
-`);
+`;
 
-export const ConfigVStack = styledDiv(css`
+export const ConfigVStack = styled('div')`
   > * + * {
     margin-top: 4px;
   }
-`);
+`;
 
-export const ConfigRow = styledDiv(css`
+export const ConfigRow = styled('div')`
   display: flex;
   > * + * {
     margin-left: 4px;
   }
-`);
+`;
