be2cf6c33fec581ee9d03e30bd551acecd1ba1ad
yahiro <yahiro07@users.noreply.github.com>
2021-02-12 18:47:46 +0900

    ヒント表示の実験ページを追加

---
+++ software/src/ui-mock-view/HoverHintDevelopmentPage.tsx
@@ -0,0 +1,34 @@
+import { css } from 'goober';
+import { h } from 'qx';
+
+const globalHintMouseMoveHandler = (e: MouseEvent) => {
+  const el = e.target as HTMLElement;
+  const instArea = document.getElementById('domHintDisplayText');
+  if (!(el && instArea)) {
+    return;
+  }
+  instArea.innerText = el.dataset.hint || '';
+};
+
+const GlobalHintDisplayText = () => <span id="domHintDisplayText" />;
+
+const cssInstructionArea = css`
+  border: solid 3px #0af8;
+  height: 32px;
+`;
+
+export const HoverHintDevelopmentPage = () => {
+  return (
+    <div onMouseMove={globalHintMouseMoveHandler}>
+      <div css={cssInstructionArea}>
+        <GlobalHintDisplayText />
+      </div>
+      <div data-hint="hoge">foo</div>
+      <div data-hint="piyo">
+        bar
+        <div data-hint="poyo">zoo</div>
+      </div>
+      <div>buzz</div>
+    </div>
+  );
+};
