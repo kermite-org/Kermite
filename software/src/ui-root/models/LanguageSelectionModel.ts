f24730658bba865cf3ebef778d878811d926dd2d
yahiro <yahiro07@users.noreply.github.com>
2021-02-13 11:28:05 +0900

    言語の選択を実装

---
+++ software/src/ui-root/models/LanguageSelectionModel.ts
@@ -0,0 +1,21 @@
+import { Hook } from 'qx';
+import { ILanguageKey, ipcAgent, uiTextConfigLoader } from '~/ui-common/base';
+
+export interface ILanguageSelectionModel {
+  currrentLanguage: ILanguageKey;
+  changeLanguage(languageKey: ILanguageKey): void;
+}
+
+export function useLanguageSelectionModel(): ILanguageSelectionModel {
+  const currrentLanguage = Hook.useMemo(
+    () => uiTextConfigLoader.loadLanugageKey(),
+    [],
+  );
+  const changeLanguage = (languageKey: ILanguageKey) => {
+    if (languageKey !== currrentLanguage) {
+      uiTextConfigLoader.saveLanguageKey(languageKey);
+      ipcAgent.async.window_reloadPage();
+    }
+  };
+  return { currrentLanguage, changeLanguage };
+}
