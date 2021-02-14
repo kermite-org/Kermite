9e74e11e9eaeb306a1fdebf3f1a825897f8b40a3
yahiro <yahiro07@users.noreply.github.com>
2021-02-14 20:56:30 +0900

    素の例外とカスタム例外で表示を出し分け

--- software/src/ui-root/AppServicesInitializer.ts
+++ software/src/ui-root/AppServicesInitializer.ts
@@ -1,22 +1,24 @@
-import { getErrorText } from '~/shared';
+import { getErrorText, IAppErrorData } from '~/shared';
 import { ipcAgent } from '~/ui-common/base/uiGlobal';
 import { modalError } from '~/ui-common/fundamental/dialog/BasicModals';
+
+function getErrorMessage(errorData: IAppErrorData) {
+  if (errorData.info.type === 'RawException') {
+    return errorData.stack;
+  } else {
+    // todo: 多言語化対応時にエラーを出し分ける
+    // const summaryText = getErrorText(errorData.info, 'EN');
+    const summaryText = getErrorText(errorData.info, 'JP');
+    const detailText = errorData.stack;
+    return `${summaryText}\n\n詳細:\n${detailText}`;
+  }
+}
 
 export function globalAppServicesInitializerEffect() {
   const unsub = ipcAgent.subscribe(
     'global_appErrorEvents',
     async (errorData) => {
-      // const errorTextEN = getErrorText(errorInfo, 'EN');
-      const summaryText = getErrorText(errorData.info, 'JP');
-      // console.log(`ERROR`, {
-      //   errorInfo,
-      //   errorTextEN,
-      //   errorTextJP,
-      // });
-      // todo: 多言語化対応時にエラーを出し分ける
-      const detailText = errorData.stack;
-      const message = `${summaryText}\n\n詳細:\n${detailText}`;
-      await modalError(message);
+      await modalError(getErrorMessage(errorData));
     },
   );
   ipcAgent.async.global_triggerLazyInitializeServices();
