2cf45c33dd164a29106ba9abb1a75f38f146387a
yahiro <yahiro07@users.noreply.github.com>
2021-02-20 10:46:53 +0900

    ApplicationStorageで永続データのスキーマをチェック

--- software/src/shell/base/ApplicationStorage.ts
+++ software/src/shell/base/ApplicationStorage.ts
@@ -1,12 +1,16 @@
 import { Rectangle } from 'electron';
-import {
-  makeObjectPropsOverrideRecursive,
-  IGlobalSettings,
-  IKeyboardConfig,
-  ILayoutEditSource,
-} from '~/shared';
+import { IGlobalSettings, IKeyboardConfig, ILayoutEditSource } from '~/shared';
 import { appEnv } from '~/shell/base';
 import { fsExistsSync, fsxReadJsonFile, fsxWriteJsonFile } from '~/shell/funcs';
+import {
+  vBoolean,
+  vNumber,
+  vObject,
+  vSchemaOneOf,
+  vString,
+  vValueEquals,
+  vValueOneOf,
+} from '~/shell/loaders/SchemaValidationHelper';
 
 export interface IWindowPersistState {
   pagePath: string;
@@ -66,6 +70,59 @@ const defaultPersistData: IApplicationPersistData = {
   },
   windowState: makeFallbackWindowPersistState(),
 };
+
+const rectangleSchema = vObject({
+  x: vNumber(),
+  y: vNumber(),
+  width: vNumber(),
+  height: vNumber(),
+});
+
+export const applicationPersistDataSchemaChecker = vObject({
+  pageState: vObject({
+    currentPagePath: vString(),
+  }),
+  currentProfileName: vString(),
+  keyboardConfig: vObject({
+    behaviorMode: vValueOneOf(['Standalone', 'SideBrain']),
+    layoutStandard: vValueOneOf(['US', 'JIS']),
+  }),
+  layoutEditSource: vSchemaOneOf([
+    vObject({
+      type: vValueEquals('NewlyCreated'),
+    }),
+    vObject({
+      type: vValueEquals('CurrentProfile'),
+    }),
+    vObject({
+      type: vValueEquals('File'),
+      filePath: vString(),
+    }),
+    vObject({
+      type: vValueEquals('ProjectLayout'),
+      projectId: vString(),
+      layoutName: vString(),
+    }),
+  ]),
+  globalSettings: vObject({
+    useOnlineResources: vBoolean(),
+    useLocalResouces: vBoolean(),
+    localProjectRootFolderPath: vString(),
+  }),
+  windowState: vObject({
+    pagePath: vString(),
+    isDevtoolsVisible: vBoolean(),
+    placement: vObject({
+      main: vObject({
+        bounds: rectangleSchema,
+      }).optional,
+      widget: vObject({
+        projectId: vString(),
+        bounds: rectangleSchema,
+      }).optional,
+    }),
+  }),
+});
 class ApplicationStorage {
   private configFilePath = appEnv.resolveUserDataFilePath('data/config.json');
   private data: IApplicationPersistData = defaultPersistData;
@@ -88,8 +145,14 @@ class ApplicationStorage {
       const obj = (await fsxReadJsonFile(
         this.configFilePath,
       )) as IApplicationPersistData;
-      // todo: ここで永続データのスキーマをチェックし、データがおかしい場合は読み込まない
-      this.data = makeObjectPropsOverrideRecursive(this.data, obj);
+
+      const errors = applicationPersistDataSchemaChecker(obj);
+      if (errors) {
+        console.error(`application persist data schema error`);
+        console.log(JSON.stringify(errors, null, '  '));
+      } else {
+        this.data = obj;
+      }
     } else {
       console.log('config file not found!');
     }
