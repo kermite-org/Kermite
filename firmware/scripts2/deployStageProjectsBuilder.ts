699dd81883baf8ef8692972d5446d2e1717d7e25
yahiro <yahiro07@users.noreply.github.com>
2021-02-22 13:51:34 +0900

    CIスクリプトでプリセットフォルダ名,ファイル名の変更に対応

--- firmware/scripts2/deployStageProjectsBuilder.ts
+++ firmware/scripts2/deployStageProjectsBuilder.ts
@@ -207,8 +207,8 @@ function buildProjectEntry(
     fsCopyFileSync(`${srcDir}/${fileName}`, `${destDir}/${fileName}`)
   );
 
-  if (fsExistsSync(`${srcDir}/presets`)) {
-    fsxCopyDirectory(`${srcDir}/presets`, `${destDir}/presets`);
+  if (fsExistsSync(`${srcDir}/profiles`)) {
+    fsxCopyDirectory(`${srcDir}/profiles`, `${destDir}/profiles`);
   }
 
   try {
