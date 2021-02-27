d7ab886dade93df47551d2696a18b0cb37414ce5
yahiro <yahiro07@users.noreply.github.com>
2021-02-27 23:14:45 +0900

    cssInitializerを修正

--- software/src/ui-mock-view/cssInitializer.ts
+++ software/src/ui-mock-view/cssInitializer.ts
@@ -1,29 +1,29 @@
 import { h } from 'qx';
-import { glob, setup } from 'qx/cssinjs';
+import { applyGlobalStyle, css, setup } from 'qx/cssinjs';
 
 export function initializeCss() {
   setup(h);
 
-  glob`
+  applyGlobalStyle(css`
     * {
       margin: 0;
       padding: 0;
       box-sizing: border-box;
     }
-  
+
     html,
     body,
     #app {
       height: 100%;
     }
-  
+
     #app {
       font-family: 'Roboto', sans-serif;
     }
-  
+
     body {
       overflow: hidden;
-      background: #FFF;
+      background: #fff;
     }
-  `;
+  `);
 }
