9179c4cfd4812ef1b4d850a3c7dd511bf3685d2b
yahiro <yahiro07@users.noreply.github.com>
2021-02-28 18:02:29 +0900

    short cssを使用

--- software/src/ui-root/views/SiteRoot.tsx
+++ software/src/ui-root/views/SiteRoot.tsx
@@ -1,5 +1,5 @@
-import { jsx, Hook, applyGlobalStyle, css } from 'qx';
-import { router } from '~/ui-common';
+import { jsx, Hook, applyGlobalStyle, css, setShortCssProcessor } from 'qx';
+import { router, shortCssProcessor } from '~/ui-common';
 import { globalHintMouseMoveHandlerEffect } from '~/ui-common/base/GlobalHint';
 import { DebugOverlay } from '~/ui-common/fundamental/overlay/DebugOverlay';
 import { ForegroundModalLayerRoot } from '~/ui-common/fundamental/overlay/ForegroundModalLayer';
@@ -9,6 +9,8 @@ import { globalAppServicesInitializerEffect } from '~/ui-root/models/AppServices
 import { SiteDpiScaler } from '~/ui-root/views/window/SiteDpiScaler';
 import { WidgetZoneRoot } from '~/ui-widget';
 import { ConfiguratorZoneRoot } from './ConfiguratorZoneRoot';
+
+setShortCssProcessor(shortCssProcessor);
 
 const cssGlobal = css`
   * {
--- software/src/ui-root/views/navigation/NavigationButtonsArea.tsx
+++ software/src/ui-root/views/navigation/NavigationButtonsArea.tsx
@@ -1,15 +1,11 @@
-import { jsx, css } from 'qx';
+import { jsx } from 'qx';
 import { makeNavigationViewModel } from '~/ui-root/views/navigation/NavigationButtonsArea.model';
 import { NavigationButton } from './elements/NavigationButton';
-
-const cssNavigationButtonsArea = css`
-  width: 100%;
-`;
 
 export const NavigationButtonsArea = () => {
   const vm = makeNavigationViewModel();
   return (
-    <div css={cssNavigationButtonsArea}>
+    <div xw="w-100%">
       {vm.entries.map((entry) => (
         <NavigationButton vm={entry} key={entry.pagePath} />
       ))}
