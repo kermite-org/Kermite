82757ec6bb97f364d9ec8ea7beb8c9bf13d9f40f
yahiro <yahiro07@users.noreply.github.com>
2021-03-01 23:53:05 +0900

    GeneralSelectorでDOMに内部状態を持たないようにするオプションを追加

--- software/src/ui-common/components/controls/GeneralSelector.tsx
+++ software/src/ui-common/components/controls/GeneralSelector.tsx
@@ -1,5 +1,5 @@
-import { FC, jsx, css } from 'qx';
-import { uiTheme, reflectValue } from '~/ui-common';
+import { FC, jsx, css, Hook } from 'qx';
+import { uiTheme } from '~/ui-common';
 import { ISelectorOption } from '~/ui-common/base';
 
 interface Props {
@@ -10,6 +10,7 @@ interface Props {
   className?: string;
   disabled?: boolean;
   hint?: string;
+  forceControlled?: boolean;
 }
 
 const style = (width: number | undefined) => css`
@@ -41,19 +42,32 @@ export const GeneralSelector: FC<Props> = ({
   width,
   disabled,
   hint,
-}) => (
-  <select
-    value={options.length > 0 ? value : ''}
-    onChange={reflectValue(setValue)}
-    css={style(width)}
-    className={className}
-    disabled={disabled}
-    data-hint={hint}
-  >
-    {options.map((it, idx) => (
-      <option value={it.value} key={idx}>
-        {it.label}
-      </option>
-    ))}
-  </select>
-);
+  forceControlled,
+}) => {
+  const onChange = (event: Event) => {
+    const el = event.currentTarget as HTMLSelectElement;
+    const newValue = el.value;
+    if (forceControlled) {
+      // controlledなコンポーネントとして動作させるため現在選択されている値に一旦戻す
+      el.value = value;
+    }
+    setValue(newValue);
+  };
+
+  return (
+    <select
+      value={options.length > 0 ? value : ''}
+      onChange={onChange}
+      css={style(width)}
+      className={className}
+      disabled={disabled}
+      data-hint={hint}
+    >
+      {options.map((it, idx) => (
+        <option value={it.value} key={idx}>
+          {it.label}
+        </option>
+      ))}
+    </select>
+  );
+};
