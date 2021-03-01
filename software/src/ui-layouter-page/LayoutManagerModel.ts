fcbdbde8ec3ad38bbe9d7a01e7f1cb1079c9d8b9
yahiro <yahiro07@users.noreply.github.com>
2021-03-01 10:41:07 +0900

    レイヤ保存時の確認ダイアログの文言を変更

--- software/src/ui-layouter-page/LayoutManagerModel.ts
+++ software/src/ui-layouter-page/LayoutManagerModel.ts
@@ -149,8 +149,9 @@ export class LayoutManagerModel implements ILayoutManagerModel {
   }
 
   async save(design: IPersistKeyboardDesign) {
+    const isProfile = this.editSource.type === 'CurrentProfile';
     const ok = await modalConfirm({
-      message: 'File ovewritten. Are you sure?',
+      message: `${isProfile ? 'Profile' : 'File'} ovewritten. Are you ok?`,
       caption: 'Save',
     });
     if (ok) {
