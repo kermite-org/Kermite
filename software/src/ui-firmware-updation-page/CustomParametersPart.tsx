70d52d63888c9b72c12dc98e9cde2fdc622e96b2
yahiro <yahiro07@users.noreply.github.com>
2021-03-05 22:08:52 +0900

    UIで各種パラメタの種類に対応

--- software/src/ui-firmware-updation-page/CustomParametersPart.tsx
+++ software/src/ui-firmware-updation-page/CustomParametersPart.tsx
@@ -1,43 +1,99 @@
 import { css, FC, jsx } from 'qx';
-import { generateNumberSequence } from '~/shared';
+import { ICustromParameterSpec } from '~/shared';
 import {
   ipcAgent,
   ISelectorOption,
   useEventSource,
   useFetcher,
 } from '~/ui-common';
-import { GeneralSelector } from '~/ui-common/components';
+import { CheckBox, Slider, GeneralSelector } from '~/ui-common/components';
 
-interface ICustomParameterModel {
-  type: 'Selection';
-  index: number;
-  options: ISelectorOption[];
-  selectionKey: string;
-  setSelectionKey(selectionKey: string): void;
-}
+type ICustomParameterModel =
+  | {
+      type: 'toggle';
+      slotIndex: number;
+      label: string;
+      enabled: boolean;
+      setEnabled(enabled: boolean): void;
+    }
+  | {
+      type: 'selection';
+      slotIndex: number;
+      label: string;
+      options: ISelectorOption[];
+      selectionKey: string;
+      setSelectionKey(selectionKey: string): void;
+    }
+  | {
+      type: 'linear';
+      slotIndex: number;
+      label: string;
+      min: number;
+      max: number;
+      value: number;
+      setValue(value: number): void;
+    }
+  | {
+      type: 'invalidType';
+      slotIndex: number;
+      label: string;
+    };
 
 function makeParameterModel(
-  value: number,
-  index: number,
+  parameterSpec: ICustromParameterSpec,
+  currentValue: number,
 ): ICustomParameterModel {
-  const options = generateNumberSequence(10).map((it) => ({
-    label: it.toString(),
-    value: it.toString(),
-  }));
-  const selectionKey = value.toString();
+  const { label, slotIndex } = parameterSpec;
+  if (parameterSpec.type === 'toggle') {
+    return {
+      type: 'toggle',
+      slotIndex,
+      label,
+      enabled: currentValue !== 0,
+      setEnabled: (enabled: boolean) => {
+        ipcAgent.async.device_setCustomParameterValue(
+          slotIndex,
+          enabled ? 1 : 0,
+        );
+      },
+    };
+  }
+  if (parameterSpec.type === 'selection') {
+    const options: ISelectorOption[] = parameterSpec.options.map((it) => ({
+      value: it.value.toString(),
+      label: it.label,
+    }));
+    const selectionKey = currentValue.toString();
 
-  const setSelectionKey = (selectionKey: string) => {
-    const newValue = parseInt(selectionKey);
-    ipcAgent.async.device_setCustomParameterValue(index, newValue);
-  };
+    const setSelectionKey = (selectionKey: string) => {
+      const newValue = parseInt(selectionKey);
+      ipcAgent.async.device_setCustomParameterValue(slotIndex, newValue);
+    };
 
-  return {
-    type: 'Selection',
-    index,
-    options,
-    selectionKey,
-    setSelectionKey,
-  };
+    return {
+      type: 'selection',
+      slotIndex,
+      label,
+      options,
+      selectionKey,
+      setSelectionKey,
+    };
+  }
+  if (parameterSpec.type === 'linear') {
+    const setValue = (value: number) => {
+      ipcAgent.async.device_setCustomParameterValue(slotIndex, value >> 0);
+    };
+    return {
+      type: 'linear',
+      slotIndex,
+      label,
+      min: 0,
+      max: parameterSpec.maxValue,
+      value: currentValue,
+      setValue,
+    };
+  }
+  return { type: 'invalidType', slotIndex, label };
 }
 
 interface ICustomParametersPartViewModel {
@@ -68,31 +124,33 @@ function useCustomParametersPartViewModel(): ICustomParametersPartViewModel {
     (info) => info.projectId === deviceProjectId,
   );
 
-  console.log({ projectInfo });
-
-  // todo: projectInfoからパラメタ定義を取り出しUIの出し分けに使用
+  const customParameterSpecs = projectInfo?.customParameters;
 
   return {
     parameterModels:
-      parameterValues?.map((value, index) =>
-        makeParameterModel(value, index),
-      ) || [],
+      (customParameterSpecs &&
+        parameterValues &&
+        customParameterSpecs.map((spec) =>
+          makeParameterModel(spec, parameterValues[spec.slotIndex]),
+        )) ||
+      [],
   };
 }
 
-const cssParameterRow = css`
-  display: flex;
-  align-items: center;
-  > * + * {
-    margin-left: 10px;
-  }
-`;
-
 const cssBase = css`
-  .parameters-list {
+  > .parameters-list-outer {
+    display: inline-block;
     margin-top: 10px;
-    > * + * {
-      margin-top: 5px;
+
+    > .parameters-list {
+      display: grid;
+      grid-template-columns: auto auto;
+      gap: 8px 20px;
+      align-items: center;
+
+      > .row {
+        display: contents;
+      }
     }
   }
 `;
@@ -102,18 +160,38 @@ export const CustomParametersPart: FC = () => {
   return (
     <div css={cssBase}>
       <div>Custom Setting Parameters</div>
-      <div className="parameters-list">
-        {parameterModels.map((item) => (
-          <div key={item.index} css={cssParameterRow}>
-            <label>value {item.index}</label>
-            <GeneralSelector
-              options={item.options}
-              value={item.selectionKey}
-              setValue={item.setSelectionKey}
-              width={50}
-            />
-          </div>
-        ))}
+      <div className="parameters-list-outer">
+        <div className="parameters-list">
+          {parameterModels.map((item) => (
+            <div key={item.slotIndex} className="row">
+              <label>{item.label}</label>
+              <div>
+                {item.type === 'selection' && (
+                  <GeneralSelector
+                    options={item.options}
+                    value={item.selectionKey}
+                    setValue={item.setSelectionKey}
+                    width={100}
+                  />
+                )}
+                {item.type === 'toggle' && (
+                  <CheckBox
+                    checked={item.enabled}
+                    setChecked={item.setEnabled}
+                  />
+                )}
+                {item.type === 'linear' && (
+                  <Slider
+                    value={item.value}
+                    min={item.min}
+                    max={item.max}
+                    onChange={item.setValue}
+                  />
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
