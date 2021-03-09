d5a600a48248f36fa2953a4b52f5f4bea4219fe5
yahiro <yahiro07@users.noreply.github.com>
2021-03-10 01:13:46 +0900

    デバイス情報を表示

--- software/src/ui-firmware-updation-page/ConnectedDeviceAttrsPart.tsx
+++ software/src/ui-firmware-updation-page/ConnectedDeviceAttrsPart.tsx
@@ -1,9 +1,52 @@
-import { FC, jsx } from 'qx';
+/* eslint-disable react/jsx-key */
+import { css, FC, jsx } from 'qx';
+import { useConnectedDeviceAttributes } from '~/ui-firmware-updation-page/dataSource';
 
+const cssBase = css`
+  td + td {
+    padding-left: 20px;
+  }
+`;
 export const ConnectedDeviceAttrsPart: FC = () => {
+  const { deviceAttrs, projectInfo } = useConnectedDeviceAttributes();
+
+  const tableData =
+    (deviceAttrs &&
+      projectInfo && [
+        ['ポート', deviceAttrs.portName],
+        [
+          'リソースオリジン',
+          deviceAttrs.origin === 'local' ? 'ローカル' : 'オンライン',
+        ],
+        ['プロジェクトID', deviceAttrs.projectId],
+        ['プロジェクトパス', projectInfo.projectPath],
+        ['キーボード名', projectInfo.keyboardName],
+        ['個体番号', deviceAttrs.deviceInstanceCode],
+        ['ファームウェアビルドリビジョン', deviceAttrs.firmwareBuildRevision],
+        [
+          'キーマッピング領域サイズ',
+          deviceAttrs.assignStorageCapacity + ' bytes',
+        ],
+      ]) ||
+    undefined;
+
   return (
-    <div>
-      <div>Conntected Device Attributes</div>
+    <div css={cssBase}>
+      <div>デバイス情報</div>
+      {tableData && (
+        <div>
+          <table>
+            <tbody>
+              {tableData.map((item, idx) => (
+                <tr key={idx}>
+                  <td>{item[0]}</td>
+                  <td>{item[1]}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
--- software/src/ui-firmware-updation-page/index.tsx
+++ software/src/ui-firmware-updation-page/index.tsx
@@ -9,7 +9,7 @@ const cssPage = css`
   background: ${uiTheme.colors.clBackground};
   color: ${uiTheme.colors.clMainText};
   height: 100%;
-  padding: 20px;
+  padding: 30px;
 
   > * + * {
     margin-top: 40px;
