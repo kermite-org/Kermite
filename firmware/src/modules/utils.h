8a23fb2618d356b2f8c30cc0e5c8ef3fa3290bce
yahiro <yahiro07@users.noreply.github.com>
2021-03-11 01:04:02 +0900

    char*からwchar_t*に文字列を書き込むヘルパを追加

--- firmware/src/modules/utils.h
+++ firmware/src/modules/utils.h
@@ -13,4 +13,6 @@ void utils_copyBitFlagsBuf(uint8_t *dstBuf, uint8_t dstOffset, uint8_t *srcBuf, 
 
 bool utils_compareBytes(uint8_t *arr1, uint8_t *arr2, uint16_t len);
 
+void utils_copyStringToWideString(int16_t *dst, uint8_t *src, uint16_t len);
+
 #endif
