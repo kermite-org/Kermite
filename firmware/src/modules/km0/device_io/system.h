eb452fc5c3ee7c3ea8ccac6df64ecb0f69888472
yahiro <yahiro07@users.noreply.github.com>
2021-03-16 12:23:22 +0900

    systemの実体をソースファイルで実装

--- firmware/src/modules/km0/device_io/system.h
+++ firmware/src/modules/km0/device_io/system.h
@@ -3,43 +3,12 @@
 
 #include "types.h"
 
-#ifdef TARGET_MCU_DUMMY
+void delayMs(uint16_t ms);
 
-#define delayMs(ms) \
-  {}
-
-#define system_readRomByte(p) \
-  {}
-#define system_readRomWord(p) \
-  {}
-
-#define system_enableInterrupts() \
-  {}
-#define system_disableInterrupts() \
-  {}
-
-#define system_initializeUserProgram() \
-  {}
-
-#endif
-
-#ifdef TARGET_MCU_ATMEGA
-
-#include <avr/interrupt.h>
-#include <avr/pgmspace.h>
-#include <util/delay.h>
-
-#define delayMs _delay_ms
-
-#define system_readRomByte pgm_read_byte
-#define system_readRomWord pgm_read_word
-
-#define system_enableInterrupts sei
-#define system_disableInterrupts cli
-
-#define system_initializeUserProgram() \
-  { USBCON = 0; }
-
-#endif
+uint8_t system_readRomByte(uint8_t *ptr);
+uint16_t system_readRomWord(uint16_t *ptr);
+void system_enableInterrupts();
+void system_disableInterrupts();
+void system_initializeUserProgram();
 
 #endif
