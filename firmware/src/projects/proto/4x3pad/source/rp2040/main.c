d84bfc824fc8fed51dae0b35cf6243d84d920f27
yahiro <yahiro07@users.noreply.github.com>
2021-03-18 00:16:56 +0900

    実験用のプロジェクトを追加

---
+++ firmware/src/projects/proto/4x3pad/source/rp2040/main.c
@@ -0,0 +1,27 @@
+#include "config.h"
+#include "dio.h"
+#include "generalKeyboard.h"
+
+#define NumRows GK_NUM_ROWS
+#define NumColumns GK_NUM_COLUMNS
+#define NumKeySlots (NumRows * NumColumns)
+
+static const uint8_t rowPins[NumRows] = { GP2, GP3, GP4 };
+static const uint8_t columnPins[NumColumns] = { GP12, GP13, GP14, GP15 };
+
+// clang-format off
+static const int8_t keyIndexTable[NumKeySlots] = {
+   0,  1,  2,  3,  4,  5,
+   6,  7,  8,  9, 10, 11,
+  12, 13, 14, 15, 16, 17,
+  18, 19, 20, 21, 22, 23,
+};
+// clang-format on
+
+int main() {
+  generalKeyboard_useOnboardLeds(GP25, GP25);
+  generalKeyboard_useDebugUART(38400);
+  generalKeyboard_setup(rowPins, columnPins, keyIndexTable);
+  generalKeyboard_start();
+  return 0;
+}
---
+++ firmware/src/projects/proto/4x3pad/source/rp2040/rules.mk
@@ -0,0 +1,13 @@
+MODULE_SRCS += km0/common/utils.c
+MODULE_SRCS += km0/device_io/rp2040/system.c
+MODULE_SRCS += km0/device_io/rp2040/dio.c
+MODULE_SRCS += km0/device_io/rp2040/usbioCore.c
+MODULE_SRCS += km0/device_io/rp2040/dataMemory.c
+MODULE_SRCS += km0/device_io/rp2040/debugUart.c
+MODULE_SRCS += km0/keyboard/keyMatrixScanner2.c
+MODULE_SRCS += km0/keyboard/configuratorServant.c
+MODULE_SRCS += km0/keyboard/configValidator.c
+MODULE_SRCS += km0/keyboard/keyboardCoreLogic2.c
+MODULE_SRCS += km0/keyboard/generalKeyboard.c
+
+PROJECT_SRCS += main.c
