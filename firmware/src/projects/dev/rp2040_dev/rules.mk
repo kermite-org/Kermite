38e7e43a16bafa0169a89718b459817fdb79e83f
yahiro <yahiro07@users.noreply.github.com>
2021-03-17 13:24:33 +0900

    debugUartモジュールを実装

--- firmware/src/projects/dev/rp2040_dev/rules.mk
+++ firmware/src/projects/dev/rp2040_dev/rules.mk
@@ -1,4 +1,6 @@
 MODULE_SRCS += km0/device_io/rp2040/system.c
 MODULE_SRCS += km0/device_io/rp2040/dio.c
+MODULE_SRCS += km0/device_io/rp2040/debugUart.c
 
-PROJECT_SRCS += main.c
+# PROJECT_SRCS += main_blink.c
+PROJECT_SRCS += main_debug_uart.c
