07bfbb57036e563a032e454235bb1386677a2eeb
yahiro <yahiro07@users.noreply.github.com>
2021-03-18 23:16:15 +0900

    keyIndexTableの要素数を修正

--- firmware/src/projects/proto/4x3pad/source/rp2040/main.c
+++ firmware/src/projects/proto/4x3pad/source/rp2040/main.c
@@ -11,10 +11,9 @@ static const uint8_t columnPins[NumColumns] = { GP12, GP13, GP14, GP15 };
 
 // clang-format off
 static const int8_t keyIndexTable[NumKeySlots] = {
-   0,  1,  2,  3,  4,  5,
-   6,  7,  8,  9, 10, 11,
-  12, 13, 14, 15, 16, 17,
-  18, 19, 20, 21, 22, 23,
+   0,  1,  2,  3, 
+   4,  5,  6,  7,
+   8,  9, 10, 11
 };
 // clang-format on
 
