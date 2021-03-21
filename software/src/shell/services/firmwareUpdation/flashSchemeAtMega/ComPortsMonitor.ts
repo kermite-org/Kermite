eb8777db7de1a73f2e6373df624158f80fc32c1f
yahiro <yahiro07@users.noreply.github.com>
2021-03-22 04:08:18 +0900

    不要なコードを削除

--- software/src/shell/services/firmwareUpdation/flashSchemeAtMega/ComPortsMonitor.ts
+++ software/src/shell/services/firmwareUpdation/flashSchemeAtMega/ComPortsMonitor.ts
@@ -1,56 +1,6 @@
 import { getArrayDiff } from '~/shared';
 import { ComPortsResource } from './ComPortsResource';
-/*
-export interface IComPortDetectionEvent {
-  comPortName: string | undefined;
-}
 
-type IComPortDetectionCallback = (event: IComPortDetectionEvent) => void;
-
-export class ComPortsMonitor {
-  private timerWrapper = new IntervalTimerWrapper();
-  private comPortNames: string[] = [];
-  private iterationCount: number = 0;
-  private detectedComPortName: string | undefined;
-  private detectionCallback: IComPortDetectionCallback | undefined;
-
-  private updateComPortsMonitor = async () => {
-    const allComPortNames = await ComPortsResource.getComPortNames();
-
-    if (this.iterationCount > 0) {
-      const [added, removed] = getArrayDiff(this.comPortNames, allComPortNames);
-
-      if (removed.length > 0) {
-        this.detectionCallback?.({ comPortName: undefined });
-      }
-      if (added.length > 0) {
-        this.detectionCallback?.({ comPortName: added[0] });
-      }
-    }
-    this.comPortNames = allComPortNames;
-    this.iterationCount++;
-  };
-
-  startDetection(callback: IComPortDetectionCallback) {
-    this.detectionCallback = callback;
-    this.comPortNames = [];
-    this.iterationCount = 0;
-    this.timerWrapper.start(
-      withAppErrorHandler(
-        this.updateComPortsMonitor,
-        'ComPortsMonitor_updateComPortsMonitor',
-      ),
-      1000,
-      true,
-    );
-  }
-
-  stopDetection() {
-    this.detectionCallback = undefined;
-    this.timerWrapper.stop();
-  }
-}
-*/
 export class ComPortsMonitor {
   private comPortNames: string[] = [];
   private iterationCount: number = 0;
