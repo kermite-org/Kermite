0f291f523d591037d091827a9bcf19bd2d397d62
yahiro <yahiro07@users.noreply.github.com>
2021-03-30 11:28:14 +0900

    疑似オープンドレイン出力を実験

--- firmware/src/projects/dev/modules_dev/rp2040_pio/rules_post_declarations.mk
+++ firmware/src/projects/dev/modules_dev/rp2040_pio/rules_post_declarations.mk
@@ -1,6 +1,11 @@
-#PROJECT_PIO_ASM_SRCS += hello_pio/hello.pio
-#PROJECT_SRCS += hello_pio/hello.c
+# PROJECT_PIO_ASM_SRCS += hello_pio/hello.pio
+# PROJECT_SRCS += hello_pio/hello.c
 
-MODULE_SRCS += km0/device_io/rp2040/dio.c
-PROJECT_PIO_ASM_SRCS += sw_dev0/swtxrx.pio
-PROJECT_SRCS += sw_dev0/main.c
+#MODULE_SRCS += km0/device_io/rp2040/dio.c
+#PROJECT_PIO_ASM_SRCS += sw_dev0/swtxrx.pio
+#PROJECT_SRCS += sw_dev0/main.c
+
+#MODULE_SRCS += km0/device_io/rp2040/dio.c
+#PROJECT_PIO_ASM_SRCS += sw_dev0/swtxrx.pio
+PROJECT_SRCS += pseudo_open_drain/main.c
+
