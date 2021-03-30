6387fd4112d2acd2df9258b62a9152a4e1cb9ae7
yahiro <yahiro07@users.noreply.github.com>
2021-03-30 10:01:47 +0900

    フォルダ名を変更

--- firmware/src/projects/dev/modules_dev/rp2040_pio/rules_post_declarations.mk
+++ firmware/src/projects/dev/modules_dev/rp2040_pio/rules_post_declarations.mk
@@ -2,5 +2,5 @@
 #PROJECT_SRCS += hello_pio/hello.c
 
 MODULE_SRCS += km0/device_io/rp2040/dio.c
-PROJECT_PIO_ASM_SRCS += sw_dev0_tx/swtxrx.pio
-PROJECT_SRCS += sw_dev0_tx/main.c
+PROJECT_PIO_ASM_SRCS += sw_dev0/swtxrx.pio
+PROJECT_SRCS += sw_dev0/main.c
