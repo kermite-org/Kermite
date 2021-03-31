085dcca184d016a2c21c0151a085b14859cfc38f
yahiro <yahiro07@users.noreply.github.com>
2021-04-01 00:21:26 +0900

    ファイルの依存関係の記述を追加

--- firmware/src/projects/dev/modules_dev/rp2040_pio/rules_post_declarations.mk
+++ firmware/src/projects/dev/modules_dev/rp2040_pio/rules_post_declarations.mk
@@ -7,4 +7,7 @@ MODULE_SRCS += km0/device_io/rp2040/dio.c
 PROJECT_PIO_ASM_SRCS += sw_dev0/swtxrx.pio
 PROJECT_SRCS += sw_dev0/main.c
 
+$(OBJ_DIR)/$(PROJECT_CODE_DIR)/sw_dev0/main.c.obj: \
+ src/projects/dev/modules_dev/rp2040_pio/sw_dev0/main.c \
+ src/projects/dev/modules_dev/rp2040_pio/sw_dev0/swtxrx.pio.h \
 
