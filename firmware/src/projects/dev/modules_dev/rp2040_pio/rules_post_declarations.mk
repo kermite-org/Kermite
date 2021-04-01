676bed6e040d9f7421a942e954bf41e952b0d93f
yahiro <yahiro07@users.noreply.github.com>
2021-04-01 09:28:31 +0900

    コードをコピー

--- firmware/src/projects/dev/modules_dev/rp2040_pio/rules_post_declarations.mk
+++ firmware/src/projects/dev/modules_dev/rp2040_pio/rules_post_declarations.mk
@@ -1,8 +1,8 @@
 MODULE_SRCS += km0/device_io/rp2040/dio.c
-PROJECT_PIO_ASM_SRCS += sw_dev0/swtxrx.pio
-PROJECT_SRCS += sw_dev0/main2.c
+PROJECT_PIO_ASM_SRCS += sw_dev1/swtxrx.pio
+PROJECT_SRCS += sw_dev1/main3.c
 
-$(OBJ_DIR)/$(PROJECT_CODE_DIR)/sw_dev0/main2.c.obj: \
- src/projects/dev/modules_dev/rp2040_pio/sw_dev0/main2.c \
- src/projects/dev/modules_dev/rp2040_pio/sw_dev0/swtxrx.pio.h \
+$(OBJ_DIR)/$(PROJECT_CODE_DIR)/sw_dev1/main3.c.obj: \
+ src/projects/dev/modules_dev/rp2040_pio/sw_dev1/main3.c \
+ src/projects/dev/modules_dev/rp2040_pio/sw_dev1/swtxrx.pio.h \
 
