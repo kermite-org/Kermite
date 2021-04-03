02b102a76e064df44708162c63d310a64ec9b00f
yahiro <yahiro07@users.noreply.github.com>
2021-04-04 06:42:33 +0900

    ボーレートを変更

--- firmware/src/projects/proto/4x3pad/rpi_pico/main.c
+++ firmware/src/projects/proto/4x3pad/rpi_pico/main.c
@@ -20,7 +20,7 @@ static const int8_t keyIndexTable[NumKeySlots] = {
 int main() {
   generalKeyboard_useIndicatorLEDs(GP25, GP25, false); //RPi pico
   // generalKeyboard_useIndicatorRgbLED(GP25); //promicro rp2040
-  generalKeyboard_useDebugUART(38400);
+  generalKeyboard_useDebugUART(115200);
   generalKeyboard_setup(NumRows, NumColumns, rowPins, columnPins, keyIndexTable);
   generalKeyboard_start();
   return 0;
