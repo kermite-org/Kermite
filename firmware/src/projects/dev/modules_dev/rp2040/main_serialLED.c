a373c00d27303465908159a9e09231930f10b698
yahiro <yahiro07@users.noreply.github.com>
2021-04-05 02:20:13 +0900

    モジュール動作確認用コードを追加

---
+++ firmware/src/projects/dev/modules_dev/rp2040/main_serialLED.c
@@ -0,0 +1,24 @@
+#include "serialLED.h"
+#include "system.h"
+#include "dio.h"
+
+uint32_t colors[] = { 0xFF0000, 0x00FF00, 0x0000FF, 0x00FFFF, 0xFF00FF, 0xFFFF00, 0xFFFFFF };
+
+void main() {
+  serialLED_initialize(GP17);
+
+  int cnt = 0;
+  while (1) {
+    uint termMs = 2000;
+    float p = (cnt % termMs) / (float)termMs;
+    float q = p < 0.5 ? 2 * p : (2 - 2 * p);
+    uint32_t alpha = q * 20;
+    for (int i = 0; i < 7; i++) {
+      uint32_t col = (alpha << 24) | colors[i];
+      serialLED_putPixel(col);
+    }
+    delayMs(1);
+    cnt++;
+  }
+}
+
