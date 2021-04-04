eb1190215bd4b8deeeaecccc72b0b52d0eb2b3c7
yahiro <yahiro07@users.noreply.github.com>
2021-04-04 13:47:30 +0900

    promicro rp2040向けのコードを追加

---
+++ firmware/src/projects/proto/minivers/rev1_rp/rules.mk
@@ -0,0 +1,17 @@
+TARGET_MCU = rp2040
+
+MODULE_SRCS += km0/common/utils.c
+MODULE_SRCS += km0/device_io/rp2040/system.c
+MODULE_SRCS += km0/device_io/rp2040/dio.c
+MODULE_SRCS += km0/device_io/rp2040/usbioCore.c
+MODULE_SRCS += km0/device_io/rp2040/dataMemory.c
+MODULE_SRCS += km0/device_io/rp2040/debugUart.c
+MODULE_SRCS += km0/device_io/rp2040/boardLED_RGB.c
+MODULE_SRCS += km0/device_io/rp2040/singleWire4.c
+MODULE_SRCS += km0/keyboard/keyMatrixScanner.c
+MODULE_SRCS += km0/keyboard/configuratorServant.c
+MODULE_SRCS += km0/keyboard/configValidator.c
+MODULE_SRCS += km0/keyboard/keyboardCoreLogic2.c
+MODULE_SRCS += km0/keyboard/splitKeyboard.c
+
+PROJECT_SRCS += main.c
