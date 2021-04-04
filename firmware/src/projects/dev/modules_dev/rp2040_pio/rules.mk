ac036252584bd8c23513873e06845be58af9bb8c
yahiro <yahiro07@users.noreply.github.com>
2021-04-05 02:52:48 +0900

    boardLEDをatmegaとrp2040に複製して配置

--- firmware/src/projects/dev/modules_dev/rp2040_pio/rules.mk
+++ firmware/src/projects/dev/modules_dev/rp2040_pio/rules.mk
@@ -12,7 +12,7 @@ endif
 ifeq ($(TARGET), neoPixel)
 MODULE_SRCS += km0/device_io/rp2040/system.c
 MODULE_SRCS += km0/device_io/rp2040/dio.c
-MODULE_SRCS += km0/device_io/general/boardLED.c
+MODULE_SRCS += km0/device_io/rp2040/boardLED.c
 PROJECT_PIOASM_SRCS += neoPixel/neoPixelCore.pio
 PROJECT_SRCS += neoPixel/main.c
 endif
