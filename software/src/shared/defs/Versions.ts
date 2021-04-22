11fac64c983ba1d96c72336a8c013d2d71b4fc22
yahiro <yahiro07@users.noreply.github.com>
2021-04-22 09:09:24 +0900

    ConfigStorageFormatRevisionを更新

--- software/src/shared/defs/Versions.ts
+++ software/src/shared/defs/Versions.ts
@@ -1,2 +1,5 @@
-export const ConfigStorageFormatRevision = 3;
+// ConfigStorageFormatRevision
+// 210422 3-->4, OperationWordを2バイトの固定長から1~4バイトの可変長に変更
+export const ConfigStorageFormatRevision = 4;
+
 export const RawHidMessageProtocolRevision = 1;
