14423f44f6d70bb6ae1ec2496e9bf6b498e7f054
yahiro <yahiro07@users.noreply.github.com>
2021-04-24 20:57:32 +0900

    asteliaのmanufacturerとproductを設定

--- firmware/src/projects/astelia/atmega/config.h
+++ firmware/src/projects/astelia/atmega/config.h
@@ -1,7 +1,9 @@
 #ifndef __CONFIG_H__
 
 #define PROJECT_ID "79xv6ZbA"
-
 #define KM0_KEYBOARD__NUM_KEYSLOTS 48
+
+#define KM0_USB__MANUFACTURER_TEXT L"yahiro"
+#define KM0_USB__PRODUCT_TEXT L"Astelia"
 
 #endif
