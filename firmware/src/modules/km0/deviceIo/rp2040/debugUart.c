89f3c0b6cbf61888261fc882bbf9ce3a1ed9a474
yahiro <yahiro07@users.noreply.github.com>
2021-04-24 18:44:19 +0900

    デバッグ出力に使用するUARTのピンを指定できるように変更

--- firmware/src/modules/km0/deviceIo/rp2040/debugUart.c
+++ firmware/src/modules/km0/deviceIo/rp2040/debugUart.c
@@ -1,8 +1,24 @@
 #include "km0/deviceIo/debugUart.h"
+#include "config.h"
+#include "km0/deviceIo/dio.h"
 #include "pico_sdk/src/rp2_common/include/pico/stdio_uart.h"
 
+#define UART0 uart0
+#define UART1 uart1
+
+#ifndef KM0_RP_DEBUG_UART__UART_INSTANCE
+#define KM0_RP_DEBUG_UART__UART_INSTANCE UART0
+#endif
+
+#ifndef KM0_RP_DEBUG_UART__PIN_TX
+#define KM0_RP_DEBUG_UART__PIN_TX GP0
+#endif
+
+static uart_inst_t *const uart_instance = KM0_RP_DEBUG_UART__UART_INSTANCE;
+static const int pin_tx = KM0_RP_DEBUG_UART__PIN_TX;
+
 void debugUart_setup(uint32_t baud) {
-  stdio_uart_init_full(uart0, baud, 0, -1);
+  stdio_uart_init_full(uart_instance, baud, pin_tx, -1);
 }
 
 void debugUart_disable() {
