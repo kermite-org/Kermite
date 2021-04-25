09ab28fb9d7c011b6c1cc4848c6b248001a26f9a
yahiro <yahiro07@users.noreply.github.com>
2021-04-25 11:35:30 +0900

    直接配線の実験コードを追加

---
+++ firmware/src/projects/proto/testbed/directwired4/config.h
@@ -0,0 +1,7 @@
+#ifndef __CONFIG_H__
+
+#define PROJECT_ID "Mr158P4b"
+
+#define KM0_KEYBOARD__NUM_KEYSLOTS 4
+
+#endif
---
+++ firmware/src/projects/proto/testbed/directwired4/main.c
@@ -0,0 +1,17 @@
+#include "config.h"
+#include "km0/deviceIo/dio.h"
+#include "km0/keyboard/generalKeyboard.h"
+#include <avr/pgmspace.h>
+
+#define NumKeys 4
+
+static const uint8_t keyInputPins[NumKeys] = { P_D7, P_E6, P_B4, P_B5 };
+static const int8_t keyIndexTable[NumKeys] = { 0, 1, 2, 3 };
+
+int main() {
+  generalKeyboard_useIndicatorLeds(P_B0, P_D5, true);
+  generalKeyboard_useDebugUart(38400);
+  generalKeyboard_useDirectWiredKeyScanner(NumKeys, keyInputPins, keyIndexTable);
+  generalKeyboard_start();
+  return 0;
+}
