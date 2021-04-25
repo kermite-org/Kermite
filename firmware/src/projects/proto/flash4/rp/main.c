aee47175852eb3588cc328359ffbcb121d498785
yahiro <yahiro07@users.noreply.github.com>
2021-04-25 22:00:32 +0900

    LEDの色の制御を実装

--- firmware/src/projects/proto/flash4/rp/main.c
+++ firmware/src/projects/proto/flash4/rp/main.c
@@ -9,21 +9,54 @@
 static const uint8_t keyInputPins[NumKeys] = { GP12, GP13, GP14, GP15 };
 static const int8_t keyIndexTable[NumKeys] = { 0, 1, 2, 3 };
 
-void customParameterHandlerExtended(uint8_t slotIndex, uint8_t value) {
-  if (slotIndex == 4) {
-    uint32_t color = value ? 0x10FF00FF : 0;
-    for (int i = 0; i < 4; i++) {
-      serialLed_putPixelWithAlpha(color);
-    }
+static bool glowEnabled = false;
+static uint8_t glowColor = 0;
+static uint8_t glowBrightness = 0;
+
+static uint32_t colorTable[13] = {
+  0xFF0000,
+  0xFF4400,
+  0xFFFF00,
+  0x66FF00,
+  0x00FF00,
+  0x00FF44,
+  0x00FFFF,
+  0x0044FF,
+  0x0000FF,
+  0x4400FF,
+  0xFF00FF,
+  0xFF0044,
+  0xFFFFFF
+};
+
+static void updateGlowLeds() {
+  uint32_t rgb = colorTable[glowColor % 13];
+  float p = (float)glowBrightness / 255;
+  uint32_t alpha = (uint32_t)(p * p * 255);
+  uint32_t argb = glowEnabled ? ((alpha << 24) | rgb) : 0;
+  for (int i = 0; i < 4; i++) {
+    serialLed_putPixelWithAlpha(argb);
   }
 }
 
-KeyboardCallbackSet callbacks = {
+static void customParameterHandlerExtended(uint8_t slotIndex, uint8_t value) {
+  if (slotIndex == 4) {
+    glowEnabled = value;
+    updateGlowLeds();
+  } else if (slotIndex == 5) {
+    glowColor = value;
+    updateGlowLeds();
+  } else if (slotIndex == 6) {
+    glowBrightness = value;
+    updateGlowLeds();
+  }
+}
+
+static KeyboardCallbackSet callbacks = {
   .customParameterHandlerExtended = customParameterHandlerExtended
 };
 
 int main() {
-  serialLed_initialize(GP2);
   generalKeyboard_useOptionFixed(0, true);
   generalKeyboard_useOptionFixed(1, true);
   generalKeyboard_useIndicatorLeds(GP25, GP25, false); //RPi pico
@@ -31,6 +64,7 @@ int main() {
   keyScanner_initializeDirectWired(NumKeys, keyInputPins);
   generalKeyboard_setKeyIndexTable(keyIndexTable);
   generalKeyboard_setCallbacks(&callbacks);
+  serialLed_initialize(GP2);
   generalKeyboard_start();
   return 0;
 }
