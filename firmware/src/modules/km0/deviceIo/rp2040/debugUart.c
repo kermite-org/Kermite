1b27105856cc535ca7df21eb33f83f37ee0688ab
yahiro <yahiro07@users.noreply.github.com>
2021-04-27 00:04:34 +0900

    __has_includeを追加

--- firmware/src/modules/km0/deviceIo/rp2040/debugUart.c
+++ firmware/src/modules/km0/deviceIo/rp2040/debugUart.c
@@ -1,5 +1,7 @@
 #include "km0/deviceIo/debugUart.h"
+#if __has_include("config.h")
 #include "config.h"
+#endif
 #include "km0/deviceIo/dio.h"
 #include "pico_sdk/src/rp2_common/include/pico/stdio_uart.h"
 
