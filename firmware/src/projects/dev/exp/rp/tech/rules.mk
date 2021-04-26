93043da1924a7187dce104089a6ec5257c1aebfa
yahiro <yahiro07@users.noreply.github.com>
2021-04-27 00:40:47 +0900

    ロータリーエンコーダの実験コードを追加

--- firmware/src/projects/dev/exp/rp/tech/rules.mk
+++ firmware/src/projects/dev/exp/rp/tech/rules.mk
@@ -1,2 +1,9 @@
 TARGET_MCU = rp2040
-PROJECT_SRCS += main_pseudo_open_drain.c
+
+MODULE_SRCS += km0/common/utils.c
+MODULE_SRCS += km0/deviceIo/rp2040/system.c
+MODULE_SRCS += km0/deviceIo/rp2040/dio.c
+MODULE_SRCS += km0/deviceIo/rp2040/debugUart.c
+
+# PROJECT_SRCS += main_pseudo_open_drain.c
+PROJECT_SRCS += main_rotary_encoder.c
