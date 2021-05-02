31f7637e06701d15c1ef31c1b0f2df71ec9cbd4b
yahiro <yahiro07@users.noreply.github.com>
2021-05-03 00:28:45 +0900

    画像を表示

--- firmware/src/projects/dev/exp/rp/tech/main_oled_ssd1306_128x32.c
+++ firmware/src/projects/dev/exp/rp/tech/main_oled_ssd1306_128x32.c
@@ -37,40 +37,38 @@ void __i2c_write(uint8_t *buf, int len) {
 
 //----------------------------------------------------------------------
 
-// clang-format off
 static uint8_t commandInitializationBytes[] = {
-  0x00, //Control Byte
-  0xAE, //Display Off
+  0x00,       //Control Byte
+  0xAE,       //Display Off
   0xA8, 0x1F, //MUX Ratio
   0xD3, 0x00, //Display Offset
-  0x40, //Display Start Line
-  0xA1, //Segment re-map
+  0x40,       //Display Start Line
+  0xA1,       //Segment re-map
   // 0xA0, //Segment re-map
   0xC8, //COM Output Scan Direction
   // 0xC0, //COM Output Scan Direction
   0xDA, 0x02, //COM Pins hadware configuration
   // 0x81, 0x8F, //Contrast Control
   0x81, 0x3F, //Contrast Control
-  0xA4, //Disable Entire Display On
-  0xA6, //Normal Display
-  0xD5, 0x80,  //Osc Frequency
+  0xA4,       //Disable Entire Display On
+  0xA6,       //Normal Display
+  0xD5, 0x80, //Osc Frequency
   0x8D, 0x14, //Enable charge pump regulator
   // 0xD9, 0xF1, //pre charge
   // 0xDB, 0x40, //vcom detect
   // 0x2E, //deactivate scroll
   // 0x20, 0x00,   //Memory Addressing Mode, Horizontal
-  0x20, 0x01,   //Memory Addressing Mode, Vertical
-  0xAF  //Display On
+  0x20, 0x01, //Memory Addressing Mode, Vertical
+  0xAF        //Display On
 };
 
 static uint8_t commandResetPositionBytes[] = {
-  0x00, //Control Byte
+  0x00,             //Control Byte
   0x22, 0x00, 0x03, //Page Start Address
   0x21, 0x00, 0x7F, //Column Start Address
   // 0x22, 0x00, 0x00, //Page Start Address
   // 0x21, 0x00, 0x00, //Column Start Address
 };
-// clang-format on
 
 void __oled_initialize() {
   __i2c_initialize(400000, 0x3C);
@@ -118,8 +116,40 @@ void __gr_drawLine(int x0, int y0, int x1, int y1) {
   }
 }
 
+void __gr_drawFullImage(uint32_t *pLineBuffers128) {
+  memcpy(scanLines, pLineBuffers128, 512);
+}
+
+void __gr_flush() {
+  __oled_flushScreenPixels((uint8_t *)scanLines);
+}
+
 //----------------------------------------------------------------------
 
+void initLcd() {
+  __oled_initialize();
+  __gr_clearScreen();
+  __oled_flushScreenPixels((uint8_t *)scanLines);
+}
+
+void runMainLoop(void (*updateFunc)(void)) {
+  uint32_t cnt = 0;
+  while (true) {
+    if (cnt % 50 == 0) {
+      if (updateFunc) {
+        updateFunc();
+      }
+    }
+    if (cnt % 1000 == 0) {
+      boardIo_toggleLed1();
+    }
+    cnt++;
+    delayMs(1);
+  }
+}
+
+//----------------------------------------------------------------------
+//scene lineart
 typedef struct {
   float x;
   float y;
@@ -157,16 +187,13 @@ void ball_move(Ball *ball) {
   }
 }
 
-void initLcd() {
-  __oled_initialize();
-  __gr_clearScreen();
-  __oled_flushScreenPixels((uint8_t *)scanLines);
+void scene1_init() {
   for (int i = 0; i < NumBalls; i++) {
     ball_init(&balls[i]);
   }
 }
 
-void updateLcd() {
+void scene1_update() {
   __gr_clearScreen();
   // __gr_drawLine(10, 10, 100, 20);
 
@@ -183,19 +210,60 @@ void updateLcd() {
   __oled_flushScreenPixels((uint8_t *)scanLines);
 }
 
+void scene1_entry() {
+  scene1_init();
+  runMainLoop(scene1_update);
+}
+
+//----------------------------------------------------------------------
+
+uint32_t screenImageSource[] = {
+  0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  0x07ffff80, 0x07ffff80, 0x07ffff80, 0x07ffff80,
+  0x00070000, 0x000f8000, 0x001fc000, 0x003ff000,
+  0x007ff800, 0x00fcfc00, 0x01f87e00, 0x03e03f80,
+  0x07c00f80, 0x07800780, 0x07000380, 0x06000180,
+  0x04000000, 0x00000000, 0x00000000, 0x07ffff80,
+  0x07ffff80, 0x07ffff80, 0x07ffff80, 0x07878780,
+  0x07878780, 0x07878780, 0x07878780, 0x07878780,
+  0x07878780, 0x07878780, 0x07878780, 0x07878780,
+  0x07878780, 0x07800780, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x07ffff80, 0x07ffff80,
+  0x07ffff80, 0x07ffff80, 0x001e0780, 0x001e0780,
+  0x001e0780, 0x001e0780, 0x007e0780, 0x00fe0780,
+  0x01ff0f80, 0x07ff9f80, 0x07efff00, 0x078fff00,
+  0x0707fe00, 0x0601f800, 0x00000000, 0x00000000,
+  0x00000000, 0x07ffff80, 0x07ffff00, 0x07fffe00,
+  0x07fff800, 0x0003f000, 0x0007e000, 0x000fc000,
+  0x001f8000, 0x007f0000, 0x00fc0000, 0x00fc0000,
+  0x007f0000, 0x001f8000, 0x000fc000, 0x0007e000,
+  0x0003f000, 0x07fff800, 0x07fffe00, 0x07ffff00,
+  0x07ffff80, 0x00000000, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x07ffff80, 0x07ffff80,
+  0x07ffff80, 0x07ffff80, 0x00000000, 0x00000000,
+  0x00000000, 0x00000780, 0x00000780, 0x00000780,
+  0x00000780, 0x00000780, 0x00000780, 0x07ffff80,
+  0x07ffff80, 0x07ffff80, 0x07ffff80, 0x00000780,
+  0x00000780, 0x00000780, 0x00000780, 0x00000780,
+  0x00000780, 0x00000000, 0x07ffff80, 0x07ffff80,
+  0x07ffff80, 0x07ffff80, 0x07878780, 0x07878780,
+  0x07878780, 0x07878780, 0x07878780, 0x07878780,
+  0x07878780, 0x07878780, 0x07878780, 0x07878780,
+  0x07800780, 0x00000000, 0x00000000, 0x00000000
+};
+
+void scene2_entry() {
+  __gr_clearScreen();
+  __gr_drawFullImage(screenImageSource);
+  __gr_flush();
+  runMainLoop(NULL);
+}
+
+//----------------------------------------------------------------------
+
 void main() {
   boardIo_setupLeds(GP25, GP25, false);
   initLcd();
-
-  uint32_t cnt = 0;
-  while (true) {
-    if (cnt % 50 == 0) {
-      updateLcd();
-    }
-    if (cnt % 1000 == 0) {
-      boardIo_toggleLed1();
-    }
-    cnt++;
-    delayMs(1);
-  }
+  // scene1_entry();
+  scene2_entry();
 }
