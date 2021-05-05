10944c78188ad64015314a7c9becdcaa714f635f
yahiro <yahiro07@users.noreply.github.com>
2021-05-06 01:24:32 +0900

    確認用プロジェクトを追加

---
+++ firmware/src/projects/dev/testbed/withLcd/main.c
@@ -0,0 +1,24 @@
+#include "config.h"
+#include "km0/deviceIo/dio.h"
+#include "km0/keyboard/generalKeyboard.h"
+#include "km0/keyboard/keyScanner_directWired.h"
+#include "km0/keyboard/keyboardMain.h"
+#include "km0/keyboard/oledDisplay.h"
+
+#define NumScanSlots 4
+
+static const uint8_t keyInputPins[NumScanSlots] = { GP12, GP13, GP14, GP15 };
+
+static const int8_t keyIndexTable[NumScanSlots] = { 0, 1, 2, 3 };
+
+int main() {
+  oledDisplay_initialize();
+  keyScanner_directWired_initialize(NumScanSlots, keyInputPins);
+  keyboardMain_useDebugUart(115200);
+  keyboardMain_useIndicatorLeds(GP25, GP25, false);
+  keyboardMain_useKeyScanner(keyScanner_directWired_update);
+  keyboardMain_setKeyIndexTable(keyIndexTable);
+  keyboardMain_useDisplayModule(oledDispaly_updateFrame, 20);
+  generalKeyboard_start();
+  return 0;
+}
