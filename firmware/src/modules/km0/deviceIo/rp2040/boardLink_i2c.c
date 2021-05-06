b915b3101a8615e0721291da446b29d98a79227c
yahiro <yahiro07@users.noreply.github.com>
2021-05-06 09:08:41 +0900

    プリプロセッサ変数名を変更

--- firmware/src/modules/km0/deviceIo/rp2040/boardLink_i2c.c
+++ firmware/src/modules/km0/deviceIo/rp2040/boardLink_i2c.c
@@ -7,51 +7,51 @@
 
 //----------------------------------------------------------------------
 
-#ifndef KM0_RP_BOARD_SYNC_I2C__I2C_INSTANCE_INDEX
-#define KM0_RP_BOARD_SYNC_I2C__I2C_INSTANCE_INDEX 1
+#ifndef KM0_RP_BOARD_LINK_I2C__I2C_INSTANCE_INDEX
+#define KM0_RP_BOARD_LINK_I2C__I2C_INSTANCE_INDEX 1
 #endif
 
-#ifndef KM0_RP_BOARD_SYNC_I2C__FREQUENCY
-#define KM0_RP_BOARD_SYNC_I2C__FREQUENCY 1000000
+#ifndef KM0_RP_BOARD_LINK_I2C__FREQUENCY
+#define KM0_RP_BOARD_LINK_I2C__FREQUENCY 1000000
 #endif
 
-#ifndef KM0_RP_BOARD_SYNC_I2C__SLAVE_ADDRESS
-#define KM0_RP_BOARD_SYNC_I2C__SLAVE_ADDRESS 0x11
+#ifndef KM0_RP_BOARD_LINK_I2C__SLAVE_ADDRESS
+#define KM0_RP_BOARD_LINK_I2C__SLAVE_ADDRESS 0x11
 #endif
 
-#ifndef KM0_RP_BOARD_SYNC_I2C__PIN_SDA
-#define KM0_RP_BOARD_SYNC_I2C__PIN_SDA 2
+#ifndef KM0_RP_BOARD_LINK_I2C__PIN_SDA
+#define KM0_RP_BOARD_LINK_I2C__PIN_SDA 2
 #endif
 
-#ifndef KM0_RP_BOARD_SYNC_I2C__PIN_SCL
-#define KM0_RP_BOARD_SYNC_I2C__PIN_SCL 3
+#ifndef KM0_RP_BOARD_LINK_I2C__PIN_SCL
+#define KM0_RP_BOARD_LINK_I2C__PIN_SCL 3
 #endif
 
-#ifndef KM0_RP_BOARD_SYNC_I2C__BUFFER_SIZE
-#define KM0_RP_BOARD_SYNC_I2C__BUFFER_SIZE 16
+#ifndef KM0_RP_BOARD_LINK_I2C__BUFFER_SIZE
+#define KM0_RP_BOARD_LINK_I2C__BUFFER_SIZE 16
 #endif
 
-static const uint32_t i2cFrequency = KM0_RP_BOARD_SYNC_I2C__FREQUENCY;
+static const uint32_t i2cFrequency = KM0_RP_BOARD_LINK_I2C__FREQUENCY;
 
 //I2Cスレーブのアドレス, 7ビットで指定
-static const uint8_t i2cSlaveAddress = KM0_RP_BOARD_SYNC_I2C__SLAVE_ADDRESS;
+static const uint8_t i2cSlaveAddress = KM0_RP_BOARD_LINK_I2C__SLAVE_ADDRESS;
 
-#if KM0_RP_BOARD_SYNC_I2C__I2C_INSTANCE_INDEX == 0
+#if KM0_RP_BOARD_LINK_I2C__I2C_INSTANCE_INDEX == 0
 static struct i2c_inst *i2c_instance = i2c0;
 #define I2C_INSTANCE_IRQ I2C0_IRQ
 #define i2c_instance_irq_handler i2c0_irq_handler
-#elif KM0_RP_BOARD_SYNC_I2C__I2C_INSTANCE_INDEX == 1
+#elif KM0_RP_BOARD_LINK_I2C__I2C_INSTANCE_INDEX == 1
 static struct i2c_inst *i2c_instance = i2c1;
 #define I2C_INSTANCE_IRQ I2C1_IRQ
 #define i2c_instance_irq_handler i2c1_irq_handler
 #else
-#error invalid KM0_RP_BOARD_SYNC_I2C__I2C_INSTANCE_INDEX
+#error invalid KM0_RP_BOARD_LINK_I2C__I2C_INSTANCE_INDEX
 #endif
 
-static const uint8_t pin_sda = KM0_RP_BOARD_SYNC_I2C__PIN_SDA;
-static const uint8_t pin_scl = KM0_RP_BOARD_SYNC_I2C__PIN_SCL;
+static const uint8_t pin_sda = KM0_RP_BOARD_LINK_I2C__PIN_SDA;
+static const uint8_t pin_scl = KM0_RP_BOARD_LINK_I2C__PIN_SCL;
 
-#define RawBufferSize (KM0_RP_BOARD_SYNC_I2C__BUFFER_SIZE + 1)
+#define RawBufferSize (KM0_RP_BOARD_LINK_I2C__BUFFER_SIZE + 1)
 
 //送信/受信フレーム形式
 //0x~ SS BB BB BB ...
