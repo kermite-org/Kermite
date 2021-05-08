f26e165311b4218955723e536694304c33a896db
yahiro <yahiro07@users.noreply.github.com>
2021-05-08 14:13:32 +0900

    StorageFormatRevisionを更新

--- software/src/shared/defs/Versions.ts
+++ software/src/shared/defs/Versions.ts
@@ -1,5 +1,6 @@
 // ConfigStorageFormatRevision
 // 210422 3-->4, OperationWordを2バイトの固定長から1~4バイトの可変長に変更
-export const ConfigStorageFormatRevision = 4;
+// 210508 4-->5, 論理キーコードを使用するように変更
+export const ConfigStorageFormatRevision = 5;
 
 export const RawHidMessageProtocolRevision = 1;
