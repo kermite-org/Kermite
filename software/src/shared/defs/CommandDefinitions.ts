dd147c314921f58f3f7e57e42241596f30570a5e
yahiro <yahiro07@users.noreply.github.com>
2021-05-23 09:05:08 +0900

    SystemActionの定義をUI向けに調整

--- software/src/shared/defs/CommandDefinitions.ts
+++ software/src/shared/defs/CommandDefinitions.ts
@@ -16,28 +16,44 @@ export const enum SystemParameter {
   GlowSpeed,
 }
 
-export const enum SystemAction {
-  None = 0,
-  GlowOn,
-  GlowOff,
-  GlowToggle,
+export type SystemAction = 'None' | 'GlowOn' | 'GlowOff' | 'GlowToggle';
 
-  // SetEmitKeyStroke = 0,
-  // SetEmitRealtimeEvents,
-  // SetKeyHoldIndicatorLed,
-  // SetHeartbeatLed,
-  // SetMasterSide,
+export const SystemActionToCodeMap: { [key in SystemAction]: number } = {
+  None: 0,
+  GlowOn: 1,
+  GlowOff: 2,
+  GlowToggle: 3,
+};
 
-  // SetSystemLayout = 1, // 1:US, 2:JIS
-  // SetWiringMode, // 0:Main, 1:Alter
-  // ShiftGlowPattern,
-  // SetGlowColor,
-  // SetGlowBrightness,
-  // SetGlowPattern,
-  // SetGlowDirection,
-  // SetGlowSpeed,
-  // ShiftGlowColor,
-  // ShiftGlowBrightness,
-  // ShiftGlowDirection,
-  // ShiftGlowSpeed,
-}
+export const SystemActionToLabelTextMap: { [key in SystemAction]: string } = {
+  None: 'none',
+  GlowOn: 'led on',
+  GlowOff: 'led off',
+  GlowToggle: 'led x',
+};
+
+// export const enum SystemActionCode {
+//   None = 0,
+//   GlowOn,
+//   GlowOff,
+//   GlowToggle,
+
+//   // SetEmitKeyStroke = 0,
+//   // SetEmitRealtimeEvents,
+//   // SetKeyHoldIndicatorLed,
+//   // SetHeartbeatLed,
+//   // SetMasterSide,
+
+//   // SetSystemLayout = 1, // 1:US, 2:JIS
+//   // SetWiringMode, // 0:Main, 1:Alter
+//   // ShiftGlowPattern,
+//   // SetGlowColor,
+//   // SetGlowBrightness,
+//   // SetGlowPattern,
+//   // SetGlowDirection,
+//   // SetGlowSpeed,
+//   // ShiftGlowColor,
+//   // ShiftGlowBrightness,
+//   // ShiftGlowDirection,
+//   // ShiftGlowSpeed,
+// }
