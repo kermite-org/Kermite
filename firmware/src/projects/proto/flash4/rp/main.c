6986dba3c2bc49528b989e7f66818c0e5a1dfd53
yahiro <yahiro07@users.noreply.github.com>
2021-06-08 08:53:01 +0900

    モジュールの初期化順序を修正

--- firmware/src/projects/proto/flash4/rp/main.c
+++ firmware/src/projects/proto/flash4/rp/main.c
@@ -14,10 +14,10 @@ static const uint8_t keyInputPins[NumKeys] = { GP12, GP13, GP14, GP15 };
 static const int8_t keyIndexTable[NumKeys] = { 0, 1, 2, 3 };
 
 int main() {
+  debugUart_initialize(115200);
   rgbLighting_initialize();
   keyScanner_directWired_initialize(NumKeys, keyInputPins);
   boardIo_setupLeds_rpiPico();
-  debugUart_initialize(115200);
   keyboardMain_setKeyIndexTable(keyIndexTable);
   generalKeyboard_start();
   return 0;
