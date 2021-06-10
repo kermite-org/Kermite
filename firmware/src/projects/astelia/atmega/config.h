2b4204a31c332cd1e7ab05cfcf138f7999032dab
yahiro <yahiro07@users.noreply.github.com>
2021-06-10 22:53:01 +0900

    keyIndexTableを削除

--- firmware/src/projects/astelia/atmega/config.h
+++ firmware/src/projects/astelia/atmega/config.h
@@ -6,4 +6,16 @@
 #define KM0_USB__MANUFACTURER_TEXT L"yahiro"
 #define KM0_USB__PRODUCT_TEXT L"Astelia"
 
+#define KS_KEY_INDEX_TABLE      \
+  {                             \
+    24, 25, 26, 27, 28, 29,     \
+        30, 31, 32, 33, 34, 35, \
+        36, 37, 38, 39, 40, 41, \
+        42, 43, 44, 45, 46, 47, \
+        0, 1, 2, 3, 4, 5,       \
+        6, 7, 8, 9, 10, 11,     \
+        12, 13, 14, 15, 16, 17, \
+        18, 19, 20, 21, 22, 23, \
+  }
+
 #endif
