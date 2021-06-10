e921624b56a7447fb2480cc10dc63d8fe7aaed86
yahiro <yahiro07@users.noreply.github.com>
2021-06-11 00:37:57 +0900

    crkbdでstencilを使用

--- firmware/src/projects/nora/crkbd/atmega/config.h
+++ firmware/src/projects/nora/crkbd/atmega/config.h
@@ -9,4 +9,13 @@
 
 #define KM0_OLED__ORIENTATION_HORIZONTALVIEW_ROT180
 
+#define KS_NUM_COLUMNS 6
+#define KS_NUM_ROWS 4
+
+#define KS_COLUMN_PINS \
+  { B3, B1, F7, F6, F5, F4 }
+
+#define KS_ROW_PINS \
+  { D4, C6, D7, E6 }
+
 #endif
