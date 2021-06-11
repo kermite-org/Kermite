a29ed78e008830a8ea2801ba50db6e0a0410a6b2
yahiro <yahiro07@users.noreply.github.com>
2021-06-11 22:15:36 +0900

    MP2105でstencilを使用

--- firmware/src/projects/proto/mp2105/rp/config.h
+++ firmware/src/projects/proto/mp2105/rp/config.h
@@ -7,4 +7,21 @@
 #define KM0_RP_SERIAL_LED__PIN_LED GP29
 #define KM0_RGB_LIGHTING__NUM_LEDS 12
 
+#define KS_NUM_COLUMNS 4
+#define KS_NUM_ROWS 3
+
+#define KS_COLUMN_PINS \
+  { GP6, GP7, GP8, GP9 }
+
+#define KS_ROW_PINS \
+  { GP20, GP23, GP21 }
+
+#define KS_NUM_ENCODERS 1
+
+// clang-format off
+#define KS_ENCODER_CONFIGS { \
+  { .pinA = GP26, .pinB = GP22, .scanIndexBase = 12 } \
+}
+// clang-format on
+
 #endif
