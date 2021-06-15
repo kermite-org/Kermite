7f54afc42d1719a9679e803b2075b4ee346fa240
yahiro <yahiro07@users.noreply.github.com>
2021-06-15 09:52:52 +0900

    呼び出し側で引数を追加

--- firmware/src/projects/dev/testbed/asymmetrical_split_direct/main.c
+++ firmware/src/projects/dev/testbed/asymmetrical_split_direct/main.c
@@ -13,10 +13,10 @@ static const uint8_t keyInputPinsR[NumKeysR] = { E6, B4, B5, B6 };
 
 static void setupBoard(uint8_t side) {
   if (side == 0) {
-    keyScanner_directWired_initialize(NumKeysL, keyInputPinsL);
+    keyScanner_directWired_initialize(NumKeysL, keyInputPinsL, 0);
     keyboardMain_useKeyScanner(keyScanner_directWired_update);
   } else {
-    keyScanner_directWired_initialize(NumKeysR, keyInputPinsR);
+    keyScanner_directWired_initialize(NumKeysR, keyInputPinsR, 2);
     keyboardMain_useKeyScanner(keyScanner_directWired_update);
   }
 }
