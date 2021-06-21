90980b63eb49f33d040ec19a74ae00ecd1ba43b7
yahiro <yahiro07@users.noreply.github.com>
2021-06-22 05:25:26 +0900

    projectIdが未指定の場合はプリセットとして保存できないように変更

--- software/src/ui/editor-page/ui_bar_profileManagement/viewModels/ProfileSelectionMenuPartViewModel.ts
+++ software/src/ui/editor-page/ui_bar_profileManagement/viewModels/ProfileSelectionMenuPartViewModel.ts
@@ -76,7 +76,10 @@ function createMenuItemSources(
       text: texts.label_assigner_menu_saveAsPreset,
       hint: texts.hint_assigner_menu_saveAsPreset,
       handler: vm.openExportingPresetSelectionModal,
-      enabled: isLocalProjectsAvailable && !!vm.currentProfileProjectId,
+      enabled:
+        isLocalProjectsAvailable &&
+        !!vm.currentProfileProjectId &&
+        vm.currentProfileProjectId !== 'none',
     },
     {
       key: 'openUserProfilesFolder',
