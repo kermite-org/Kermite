3edc184c7e4b6b5dae3c226c74e09d85b1a219d6
yahiro <yahiro07@users.noreply.github.com>
2021-06-23 01:40:46 +0900

    プロファイルの対象キーボード名を表示

--- software/src/ui/editor-page/ui_editor_sideConfigPart/ProfileConfigurationPart.tsx
+++ software/src/ui/editor-page/ui_editor_sideConfigPart/ProfileConfigurationPart.tsx
@@ -1,17 +1,29 @@
 import { jsx, css } from 'qx';
-import { texts } from '~/ui/common';
+import { texts, useProjectResourceInfos } from '~/ui/common';
 import { editorModel } from '~/ui/editor-page/models/EditorModel';
 
+const style = css`
+  padding: 5px;
+
+  > * + * {
+    margin-top: 5px;
+  }
+`;
+
 export const ProfileConfigurationPart = () => {
-  const cssBase = css`
-    padding: 5px;
-  `;
+  const projectInfos = useProjectResourceInfos('projectsSortedByKeyboardName');
+  const projectId = editorModel.profileData.projectId;
+  const info = projectInfos.find((it) => it.projectId === projectId);
+  const keyboardName = info?.keyboardName;
 
   const currentAssignType = editorModel.profileData.settings.assignType;
 
   return (
-    <div css={cssBase} data-hint={texts.hint_assigner_configs_assignModel}>
-      {texts.label_assigner_configs_assignModel}: {currentAssignType}
+    <div css={style}>
+      <div>keyboard: {keyboardName}</div>
+      <div data-hint={texts.hint_assigner_configs_assignModel}>
+        {texts.label_assigner_configs_assignModel}: {currentAssignType}
+      </div>
     </div>
   );
 };
