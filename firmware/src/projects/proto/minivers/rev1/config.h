38606c58863fd96437b1b94d03f4e624bf00b239
yahiro <yahiro07@users.noreply.github.com>
2021-06-26 23:23:29 +0900

    miniversのコードを修正

--- firmware/src/projects/proto/minivers/rev1/config.h
+++ firmware/src/projects/proto/minivers/rev1/config.h
@@ -4,20 +4,11 @@
 
 #define KS_SNGLEWIRE_PIN_PD2
 
-//左右非対称のキーマトリクスを実験
-#define KS_NUM_SCAN_SLOTS 60
-#define KS_RIGHTHAND_SCAN_SLOTS_OFFSET 40
+#define KS_NUM_SCAN_SLOTS 80
 
 #define KS_NUM_COLUMNS 8
 #define KS_NUM_ROWS 5
 #define KS_COLUMN_PINS \
   { P_F4, P_F5, P_F6, P_F7, P_B1, P_B3, P_B2, P_B6 }
 #define KS_ROW_PINS \
-  { P_C6, P_D7, P_E6, P_B4, P_B5 }
-
-#define KS_NUM_COLUMNS_RIGHT 4
-#define KS_NUM_ROWS_RIGHT 5
-#define KS_COLUMN_PINS_RIGHT \
-  { P_F6, P_F7, P_B1, P_B3 }
-#define KS_ROW_PINS_RIGHT \
   { P_C6, P_D7, P_E6, P_B4, P_B5 }
