356ef4e880e6894e34c960470b119440cac4d61e
yahiro <yahiro07@users.noreply.github.com>
2021-06-26 23:34:30 +0900

    AVRのファームウェアを追加

---
+++ firmware/src/projects/proto/minivers/rev1/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define KERMITE_PROJECT_ID "Di6V4KvF"
+
+#define KS_SNGLEWIRE_PIN_PD2
+
+#define KS_NUM_SCAN_SLOTS 80
+
+#define KS_NUM_COLUMNS 8
+#define KS_NUM_ROWS 5
+#define KS_COLUMN_PINS \
+  { P_F4, P_F5, P_F6, P_F7, P_B1, P_B3, P_B2, P_B6 }
+#define KS_ROW_PINS \
+  { P_C6, P_D7, P_E6, P_B4, P_B5 }
