6f5d556ce2820c0d08f0402c34fd85c88393fa80
yahiro <yahiro07@users.noreply.github.com>
2021-06-28 07:07:38 +0900

    ブートローダにジャンプする前にMCUの状態を初期化

--- firmware/src/modules/km0/device/atmega/system.c
+++ firmware/src/modules/km0/device/atmega/system.c
@@ -41,8 +41,39 @@ void system_initializeUserProgram() {
 }
 
 void system_jumpToDfuBootloader() {
-  system_disableInterrupts();
+  printf("reset to dfu bootloader");
+  _delay_ms(100);
+  //https://www.pjrc.com/teensy/jump_to_bootloader.html
+  cli();
+  UDCON = 1;
+  USBCON = (1 << FRZCLK);
+  UCSR1B = 0;
+  _delay_ms(5);
+  EIMSK = 0;
+  PCICR = 0;
+  SPCR = 0;
+  ACSR = 0;
+  EECR = 0;
+  ADCSRA = 0;
+  TIMSK0 = 0;
+  TIMSK1 = 0;
+  TIMSK3 = 0;
+  TIMSK4 = 0;
+  UCSR1B = 0;
+  TWCR = 0;
+  DDRB = 0;
+  DDRC = 0;
+  DDRD = 0;
+  DDRE = 0;
+  DDRF = 0;
+  TWCR = 0;
+  PORTB = 0;
+  PORTC = 0;
+  PORTD = 0;
+  PORTE = 0;
+  PORTF = 0;
   asm volatile("jmp 0x7000");
+  while (1) {}
 }
 
 static void putCharDummy() {}
