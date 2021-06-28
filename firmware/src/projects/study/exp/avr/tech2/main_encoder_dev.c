679cb70722314aff8c090ab499d0e5b279014249
yahiro <yahiro07@users.noreply.github.com>
2021-06-28 20:32:12 +0900

    エンコーダ回転検知イベントの出力バッファを実装

--- firmware/src/projects/study/exp/avr/tech2/main_encoder_dev.c
+++ firmware/src/projects/study/exp/avr/tech2/main_encoder_dev.c
@@ -4,34 +4,49 @@
 #include "km0/device/digitalIo.h"
 #include "km0/device/system.h"
 #include <avr/interrupt.h>
-#include <avr/io.h>
 #include <stdio.h>
 
 //board ProMicro
+//B2: encoder pin for phase A
+//B3: encoder pin for phase B
 
-static const int delta_none = 2;
-static const int delta_dec = 0;
-static const int delta_inc = 1;
+static const int rot_none = 2;
+static const int rot_ccw = 0;
+static const int rot_cw = 1;
 
 static const int pinA = P_B2;
 static const int pinB = P_B3;
 
-int prev_a = 0;
+static int prev_a = 0;
 
-static void updateEncoderInterrupt() {
+static uint32_t rots_buf = 0;
+static int rots_num = 0;
+
+static void readEncoderInputOnPinChangeInterrupt() {
   int a = digitalIo_read(pinA);
   int b = digitalIo_read(pinB);
-  int delta = delta_none;
+  int delta = rot_none;
   if (!prev_a && a) {
-    delta = (b == 0) ? delta_inc : delta_dec;
+    delta = (b == 0) ? rot_cw : rot_ccw;
   }
   if (prev_a && !a) {
-    delta = (b == 1) ? delta_inc : delta_dec;
+    delta = (b == 1) ? rot_cw : rot_ccw;
   }
   prev_a = a;
-  if (delta != delta_none) {
-    // printf("%d\n", delta);
+  if (delta != rot_none) {
+    bit_spec(rots_buf, rots_num, delta);
+    rots_num++;
   }
+}
+
+static int pullEncoderEventOne() {
+  if (rots_num > 0) {
+    int rot = rots_buf & 1;
+    rots_buf >>= 1;
+    rots_num--;
+    return rot == rot_cw ? 1 : -1;
+  }
+  return 0;
 }
 
 static void initEncoder() {
@@ -42,8 +57,15 @@ static void initEncoder() {
   PCICR |= _BV(PCIE0);
 }
 
+static void processEncoderUpdate() {
+  int dir = pullEncoderEventOne();
+  if (dir != 0) {
+    printf("%d\n", dir);
+  }
+}
+
 ISR(PCINT0_vect) {
-  updateEncoderInterrupt();
+  readEncoderInputOnPinChangeInterrupt();
 }
 
 void start() {
@@ -52,9 +74,15 @@ void start() {
   initEncoder();
   printf("start\n");
   sei();
+  uint32_t tick = 0;
   while (1) {
-    boardIo_toggleLed1();
-    delayMs(500);
+    if (tick % 1000 == 0) {
+      boardIo_toggleLed1();
+    }
+    if (tick % 5 == 0) {
+      processEncoderUpdate();
+    }
+    delayMs(1);
   }
 }
 
