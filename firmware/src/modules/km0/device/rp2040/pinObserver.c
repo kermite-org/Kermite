cce471e66ce97f0c5992990f3ba76b199b134a65
yahiro <yahiro07@users.noreply.github.com>
2021-07-01 03:53:00 +0900

    RP2040のpinObserverを実装

---
+++ firmware/src/modules/km0/device/rp2040/pinObserver.c
@@ -0,0 +1,28 @@
+#include "km0/device/pinObserver.h"
+#include "km0/base/bitOperations.h"
+#include "km0/device/digitalIo.h"
+#include "km0/device/system.h"
+#include "pico_sdk/src/rp2_common/include/hardware/gpio.h"
+
+typedef void (*PinObserverCallback)(int, int);
+
+static PinObserverCallback pin_observer_callbacks[32] = { 0 };
+
+static void pinStateChangedHanderRaw(uint pin, uint32_t events) {
+  bool isRise = (events & GPIO_IRQ_EDGE_RISE) > 0;
+  bool isFall = (events & GPIO_IRQ_EDGE_FALL) > 0;
+  int edge = isFall ? PIN_OBSERVER_EDGE_FALL : PIN_OBSERVER_EDGE_RISE; //fallを優先
+  if (pin_observer_callbacks[pin]) {
+    pin_observer_callbacks[pin](pin, edge);
+  }
+}
+
+void pinObserver_observePin(int pin, void (*callback)(int, int)) {
+  pin_observer_callbacks[pin] = callback;
+  gpio_set_irq_enabled_with_callback(pin, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &pinStateChangedHanderRaw);
+}
+
+void pinObserver_unobservePin(int pin) {
+  gpio_set_irq_enabled(pin, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, false);
+  pin_observer_callbacks[pin] = 0;
+}
