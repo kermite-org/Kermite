7f3a5d58c4c094a75cb968e04ddc26786c54bf5f
yahiro <yahiro07@users.noreply.github.com>
2021-07-02 17:09:49 +0900

    エンコーダのロジックを変更

--- firmware/src/modules/km0/scanner/keyScanner_encoders.c
+++ firmware/src/modules/km0/scanner/keyScanner_encoders.c
@@ -18,8 +18,16 @@ static const int EncoderPinChangeInterruptPinStateReadWaitTimeUs = 50;
 static const int rot_none = 2;
 static const int rot_ccw = 0;
 static const int rot_cw = 1;
+
+static const int ev_none = 0;
+static const int ev_b_low_a_rise = 1;
+static const int ev_b_low_a_fall = 2;
+static const int ev_b_high_a_rise = 3;
+static const int ev_b_high_a_fall = 4;
+
 typedef struct {
   int prev_a;
+  int prev_ev;
   uint32_t rots_buf;
   int rots_num;
 } EncoderState;
@@ -28,6 +36,26 @@ static int numEncoders = 0;
 static EncoderConfig *encoderConfigs;
 static EncoderState encoderStates[NumEncodersMax];
 
+static int decodeSignalEdgeEvent(int b, int prev_a, int a) {
+  bool b_low = b == 0;
+  bool b_high = b == 1;
+  bool a_rise = !prev_a && a;
+  bool a_fall = prev_a && !a;
+  if (b_low && a_rise) {
+    return ev_b_low_a_rise;
+  }
+  if (b_low && a_fall) {
+    return ev_b_low_a_fall;
+  }
+  if (b_high && a_rise) {
+    return ev_b_high_a_rise;
+  }
+  if (b_high && a_fall) {
+    return ev_b_high_a_fall;
+  }
+  return ev_none;
+}
+
 static void encoderInstance_decodeInputOnPhaseAEdge(EncoderConfig *config, EncoderState *state, int edge) {
   if (EncoderPinChangeInterruptPinStateReadWaitTimeUs > 0) {
     delayUs(EncoderPinChangeInterruptPinStateReadWaitTimeUs);
@@ -35,18 +63,20 @@ static void encoderInstance_decodeInputOnPhaseAEdge(EncoderConfig *config, Encod
   int a = digitalIo_read(config->pinA);
   if (a != state->prev_a) {
     int b = digitalIo_read(config->pinB);
-    int delta = rot_none;
-    if (!state->prev_a && a) {
-      delta = (b == 0) ? rot_cw : rot_ccw;
+    int ev = decodeSignalEdgeEvent(b, state->prev_a, a);
+    int rot = rot_none;
+    if (state->prev_ev == ev_b_high_a_fall && ev == ev_b_low_a_rise) {
+      rot = rot_cw;
     }
-    if (state->prev_a && !a) {
-      delta = (b == 1) ? rot_cw : rot_ccw;
+    if (state->prev_ev == ev_b_low_a_fall && ev == ev_b_high_a_rise) {
+      rot = rot_ccw;
     }
-    if (delta != rot_none) {
-      bit_spec(state->rots_buf, state->rots_num, delta);
+    if (rot != rot_none) {
+      bit_spec(state->rots_buf, state->rots_num, rot);
       state->rots_num++;
     }
     state->prev_a = a;
+    state->prev_ev = ev;
   }
 }
 
