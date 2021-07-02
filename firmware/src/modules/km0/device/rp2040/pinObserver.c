2a9b142ef83d4e0449b01b191de0963d22bbce72
yahiro <yahiro07@users.noreply.github.com>
2021-07-02 17:18:47 +0900

    張り込みハンドラ終了時に割り込み要求フラグを解除

--- firmware/src/modules/km0/device/rp2040/pinObserver.c
+++ firmware/src/modules/km0/device/rp2040/pinObserver.c
@@ -8,18 +8,19 @@ typedef void (*PinObserverCallback)(int, int);
 
 static PinObserverCallback pin_observer_callbacks[32] = { 0 };
 
-static void pinStateChangedHanderRaw(uint pin, uint32_t events) {
+static void pinStateChangedHandlerRaw(uint pin, uint32_t events) {
   bool isRise = (events & GPIO_IRQ_EDGE_RISE) > 0;
   bool isFall = (events & GPIO_IRQ_EDGE_FALL) > 0;
   int edge = isFall ? PIN_OBSERVER_EDGE_FALL : PIN_OBSERVER_EDGE_RISE; //fallを優先
   if (pin_observer_callbacks[pin]) {
     pin_observer_callbacks[pin](pin, edge);
   }
+  gpio_acknowledge_irq(pin, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL);
 }
 
 void pinObserver_observePin(int pin, void (*callback)(int, int)) {
   pin_observer_callbacks[pin] = callback;
-  gpio_set_irq_enabled_with_callback(pin, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &pinStateChangedHanderRaw);
+  gpio_set_irq_enabled_with_callback(pin, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &pinStateChangedHandlerRaw);
 }
 
 void pinObserver_unobservePin(int pin) {
