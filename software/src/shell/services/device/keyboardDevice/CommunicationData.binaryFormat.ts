e627c01f054c07d32ac308e105e38c488bb33a9e
yahiro <yahiro07@users.noreply.github.com>
2021-07-05 21:48:15 +0900

    ドキュメントコードを更新

--- software/src/shell/services/device/keyboardDevice/CommunicationData.binaryFormat.ts
+++ software/src/shell/services/device/keyboardDevice/CommunicationData.binaryFormat.ts
@@ -20,171 +20,143 @@ namespace CommunicationDataBinaryForamt {
   type Reserved = any;
 
   // --------------------
-  // config storage data writing
+  // general
 
-  type PktMemoryWriteTransactionStart = PacketHostToDevice & {
-    [0]: { category: 0xb0 }; // 0xb0 for memory operation
-    [1]: { dataKind: 0x01 }; // 0x01 for config storage data
-    [2]: { command: 0x10 }; // 0x10 for write transaction start
+  type PktConnectionOpened = PacketHostToDevice & {
+    [0]: { opcode: 0xf0 };
   };
 
-  type PktMemoryWriteTransactionDone = PacketHostToDevice & {
-    [0]: { category: 0xb0 }; // 0xb0 for memory operation
-    [1]: { dataKind: 0x01 }; // 0x01 for config storage data
-    [2]: { command: 0x11 }; // 0x11 for write transaction done
+  type PktConnectionClosing = PacketHostToDevice & {
+    [0]: { opcode: 0xf1 };
   };
 
-  type PktMemoryWritingOperation = PacketHostToDevice & {
-    [0]: { category: 0xb0 }; // 0xb0 for memory operation
-    [1]: { dataKind: 0x01 }; // 0x01 for config storage data
-    [2]: { command: 0x20 }; // 0x20 for write request
-    [3_4]: { offset: u16 };
-    [5]: { dataLength: u8 };
-    '6__': { data: VariableLength }; // dataLength bytes
+  type PktDeviceAttributesRequest = PacketHostToDevice & {
+    [0]: { opcode: 0xf2 };
   };
 
-  type PktMemoryChecksumRequestOperation = PacketHostToDevice & {
-    [0]: { category: 0xb0 }; // 0xb0 for memory operation
-    [1]: { dataKind: 0x01 }; // 0x01 for config storage data
-    [2]: { command: 0x21 }; // 0x21 for checksum request
-    [3_4]: { offset: u16 };
-    [5_6]: { dataLength: u16 };
+  type PktDeviceAttributesResponse = PacketDeviceToHost & {
+    [0]: { opcode: 0xf3 };
+    [1_2]: { projectReleaseBuildRevision: u16 };
+    [3]: { configStorageFormatRevision: u8 };
+    [4]: { rawHidMessageProtocolRevision: u8 };
+    [5_12]: { projectId: Bytes<8> };
+    [13]: { isOnlineProject: u8 };
+    [14]: { padding: u8 };
+    [15_22]: { deviceInstanceCode: Bytes<8> };
+    [23_24]: { assignStorageCapacity: u16 };
+    [25_40]: { variationName: Bytes<16> };
+    [41_48]: { kermiteMcuCode: Bytes<8> };
+    [49]: { profileBinaryFormatRevision: u8 };
+    [50]: { configParametersRevision: u8 };
   };
-
-  type PktMemoryChecksumResponse = PacketDeviceToHost & {
-    [0]: { category: 0xb0 }; // 0xb0 for memory operation
-    [1]: { dataKind: 0x01 }; // 0x01 for config storage data
-    [2]: { command: 0x22 }; // 0x22 for checksum response
-    [3]: { checksumValue: u8 };
-  };
-
-  // --------------------
-  // custom paramters data read/write
-
-  type PktCustomParametersBulkReadRequest = PacketHostToDevice & {
-    [0]: { category: 0xb0 }; // 0xb0 for memory operation
-    [1]: { dataKind: 0x02 }; // 0x02 for custom parameters
-    [2]: { command: 0x80 }; // 0x80 for bulk read request
-  };
-
-  type PktCustomParametersBulkReadResponse = PacketDeviceToHost & {
-    [0]: { category: 0xb0 }; // 0xb0 for memory operation
-    [1]: { dataKind: 0x02 }; // 0x02 for custom parameters
-    [2]: { command: 0x81 }; // 0x81 for bulk read response
-    [3]: { numParameters: u8 };
-    // [3]: { initializedFlag: 0 | 1 };
-    // [4_13]: { data: Bytes<10> };
-    '4__': { values: BytesOf<'numParameters'> };
-    '4+numParameters__': { maxValues: BytesOf<'numParameters'> };
-  };
-
-  type PktCustomParametersBulkWriteOperation = PacketHostToDevice & {
-    [0]: { category: 0xb0 }; // 0xb0 for memory operation
-    [1]: { dataKind: 0x02 }; // 0x02 for custom parameters
-    [2]: { command: 0x90 }; // 0x90 for bulk write request
-    [3]: { parameterIndexBase: u8 };
-    [4]: { numParameters: u8 };
-    '5__': { data: BytesOf<'numParameters'> };
-  };
-
-  type PktCustomParameterSingleWriteOperation = PacketHostToDevice & {
-    [0]: { category: 0xb0 }; // 0xb0 for memory operation
-    [1]: { dataKind: 0x02 }; // 0x02 for custom parameters
-    [2]: { command: 0xa0 }; // 0xa0 for single write request
-    [3]: { index: u8 }; // paramter index
-    [4]: { value: u8 }; // parameter value
-  };
-
-  type PktCustomParametersResetOperation = PacketHostToDevice & {
-    [0]: { category: 0xb0 }; // 0xb0 for memory operation
-    [1]: { dataKind: 0x02 }; // 0x02 for custom parameters
-    [2]: { command: 0xb0 }; // 0xb0 for reset request
-  };
-
-  type PktCustomParameterChangedNotification = PacketDeviceToHost & {
-    [0]: { category: 0xb0 }; // 0xb0 for memory operation
-    [1]: { dataKind: 0x02 }; // 0x02 for custom parameters
-    [2]: { command: 0xe1 }; // 0xe0 for notification
-    [3]: { index: u8 }; // paramter index
-    [4]: { value: u8 }; // parameter value
-  };
-
-  // --------------------
-  // device instance code write
 
   type PktDeviceInstanceCodeWriteOperation = PacketHostToDevice & {
-    [0]: { category: 0xb0 }; // 0xb0 for memory operation
-    [1]: { dataKind: 0x03 }; // 0x03 for device instance code
-    [2]: { command: 0x90 }; // 0x80 for write request
-    [3_10]: { data: Bytes<8> };
+    [0]: { opcode: 0xf4 };
+    [1_8]: { data: Bytes<8> };
   };
+
   // --------------------
   // realtime event
 
   type PktRealtimeKeyStateEvent = PacketDeviceToHost & {
-    [0]: { category: 0xe0 }; // 0xe0 for realtime event
-    [1]: { command: 0x90 }; // 0x90 for key state changed
+    [0]: { opcode: 0xa0 };
     [2]: { keyIndex: u8 };
     [3]: { isDown: u8 }; // (1:down, 0: up)
   };
 
   type PktRealtimeLayerStateEvent = PacketDeviceToHost & {
-    [0]: { category: 0xe0 }; // 0xe0 for realtime event
-    [1]: { command: 0x91 }; // 0x91 for layer changed
+    [0]: { opcode: 0xa1 };
     [2_3]: { layerActiveFlags: u16 };
   };
 
   type __draft__PktRealtimeOperationHitEvent = PacketDeviceToHost & {
-    [0]: { category: 0xe0 }; // 0xe0 for realtime event
-    [1]: { command: 0x92 }; // 0x92 for operation hit
+    [0]: { opcode: -1 };
     [2]: { keyIndex: u8 };
     [3]: { layerIndex: u8 };
     [4]: { operationSlot: u8 }; // [1, 2, 3] for [primary, secondary, tertiary]
   };
 
   // --------------------
-  // general
+  // config storage data writing
 
-  type PktDeviceAttributesRequest = PacketHostToDevice & {
-    [0]: { category: 0xf0 }; // 0xf0 for general
-    [1]: { command: 0x10 }; // 0x10 for device attributes request
+  type PktMemoryWriteTransactionStart = PacketHostToDevice & {
+    [0]: { opcode: 0xb0 };
   };
 
-  type PktDeviceAttributesResponse = PacketDeviceToHost & {
-    [0]: { category: 0xf0 }; // 0xf0 for general
-    [1]: { command: 0x11 }; // 0x10 for device attributes response
-    [2_3]: { projectReleaseBuildRevision: u16 };
-    [4]: { configStorageFormatRevision: u8 };
-    [5]: { rawHidMessageProtocolRevision: u8 };
-    [6_13]: { projectId: Bytes<8> };
-    [14]: { isOnlineProject: u8 };
-    [15]: { padding: u8 };
-    [16_23]: { deviceInstanceCode: Bytes<8> };
-    [24_25]: { assignStorageCapacity: u16 };
-    [26_41]: { variationName: Bytes<16> };
-    [42_49]: { kermiteMcuCode: Bytes<8> };
-    [50]: { profileBinaryFormatRevision: u8 };
-    [51]: { configParametersRevision: u8 };
+  type PktMemoryWriteTransactionDone = PacketHostToDevice & {
+    [0]: { opcode: 0xb1 };
   };
 
-  type __draft__PktKeyboardSideConfiguration = PacketHostToDevice & {
-    [0]: { category: 0xf0 }; // 0xf0 for general
-    [1]: { command: 0x20 }; // 0x20 for set kebyoard side
-    [2]: { keyboardSide: u8 }; // (0:unset, 1:left, 2:right)
+  type PktMemoryWritingOperation = PacketHostToDevice & {
+    [0]: { opcode: 0xb2 };
+    [1_2]: { offset: u16 };
+    [3]: { dataLength: u8 };
+    '4__': { data: VariableLength }; // dataLength bytes
+  };
+
+  type PktMemoryChecksumRequestOperation = PacketHostToDevice & {
+    [0]: { opcode: 0xb3 };
+    [1_2]: { offset: u16 };
+    [3_4]: { dataLength: u16 };
+  };
+
+  type PktMemoryChecksumResponse = PacketDeviceToHost & {
+    [0]: { opcode: 0xb4 };
+    [1]: { checksumValue: u8 };
+  };
+
+  // --------------------
+  // custom paramters data read/write
+
+  type PktCustomParametersBulkReadRequest = PacketHostToDevice & {
+    [0]: { opcode: 0xc0 };
+  };
+
+  type PktCustomParametersBulkReadResponse = PacketDeviceToHost & {
+    [0]: { opcode: 0xc1 };
+    [1]: { numParameters: u8 };
+    '2__': { values: BytesOf<'numParameters'> };
+    '2+numParameters__': { maxValues: BytesOf<'numParameters'> };
+  };
+
+  type PktCustomParametersBulkWriteOperation = PacketHostToDevice & {
+    [0]: { opcode: 0xc2 };
+    [1]: { parameterIndexBase: u8 };
+    [2]: { numParameters: u8 };
+    '3__': { data: BytesOf<'numParameters'> };
+  };
+
+  type PktCustomParameterSingleWriteOperation = PacketHostToDevice & {
+    [0]: { opcode: 0xc3 };
+    [1]: { index: u8 }; // paramter index
+    [2]: { value: u8 }; // parameter value
+  };
+
+  type PktCustomParametersResetOperation = PacketHostToDevice & {
+    [0]: { opcode: 0xc4 };
+  };
+
+  type PktCustomParameterChangedNotification = PacketDeviceToHost & {
+    [0]: { opcode: 0xc5 };
+    [1]: { index: u8 }; // paramter index
+    [2]: { value: u8 }; // parameter value
   };
 
   // --------------------
   // device operation
 
+  type PktSpecMuteMode = PacketHostToDevice & {
+    [0]: { opcode: 0xd0 };
+    [1]: { isMuteMode: u8 }; // (0:normal, 1:muted)
+  };
+
   type PktSpecSimulatorMode = PacketHostToDevice & {
-    [0]: { category: 0xd0 }; // 0xd0 for device operation
-    [1]: { command: 0x10 }; // 0x10 for side brain mode config
-    [2]: { isSimulatorMode: u8 }; // (0:normal, 1:simulator)
+    [0]: { opcode: 0xd1 };
+    [1]: { isSimulatorMode: u8 }; // (0:normal, 1:simulator)
   };
 
   type PktSetSimulatorHidReport = PacketHostToDevice & {
-    [0]: { category: 0xd0 }; // 0xd0 for device operation
-    [1]: { command: 0x20 }; // 0x20 for set simulator hid report
-    [2_9]: { hidReportBuf: Bytes<8> };
+    [0]: { opcode: 0xd2 };
+    [1_8]: { hidReportBuf: Bytes<8> };
   };
 }
