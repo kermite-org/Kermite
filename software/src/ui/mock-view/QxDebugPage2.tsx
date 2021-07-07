a8f81ddbdc02ecc0fe8145d07159a7a873ce83fa
yahiro <yahiro07@users.noreply.github.com>
2021-07-08 04:06:59 +0900

    実験ページを追加

--- software/src/ui/mock-view/QxDebugPage2.tsx
+++ software/src/ui/mock-view/QxDebugPage2.tsx
@@ -4,7 +4,30 @@ const cssBase = css`
   padding: 10px;
   color: blue;
 `;
+
+let count = 0;
+
+const cssCounterCard = css`
+  width: 100px;
+  height: 50px;
+  border: solid 1px blue;
+`;
+
+const CounterCard: FC = () => {
+  return (
+    <div css={cssCounterCard} onClick={() => count++}>
+      {count}
+      {count % 5 === 0 && <div>fizz</div>}
+    </div>
+  );
+};
+
 export const QxDebugPage2: FC = () => {
   console.log(`render`);
-  return <div css={cssBase}>Hello</div>;
+  return (
+    <div css={cssBase}>
+      Hello
+      <CounterCard />
+    </div>
+  );
 };
