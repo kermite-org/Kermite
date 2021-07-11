4bc728e9e77c012823aadf106020711db167aecf
yahiro <yahiro07@users.noreply.github.com>
2021-07-11 13:50:01 +0900

    dom属性操作関数をファイルに分離

--- software/src/qx/qxinternal_qxcore/qxcore.ts
+++ software/src/qx/qxinternal_qxcore/qxcore.ts
@@ -1,5 +1,6 @@
+import { applyDomAttributes } from 'qx/qxinternal_qxcore/dom';
 import { createVBlank } from 'qx/qxinternal_qxcore/jsx';
-import { IProps, IVComponent, IVElement, IVNode } from './types';
+import { IProps, IVComponent, IVNode } from './types';
 
 // ------------------------------------------------------------
 
@@ -8,42 +9,6 @@ import { IProps, IVComponent, IVElement, IVNode } from './types';
 //     parentDom.removeChild(parentDom.firstChild);
 //   }
 // }
-
-function applyDomAttributes(
-  el: Element,
-  vnode: IVElement,
-  oldVnode: IVElement | undefined,
-) {
-  // const newClassNames = vnode.props.className?.split(' ');
-  // const oldClassNames = oldVnode?.props.className?.split(' ');
-  // const added = newClassNames?.filter((it) => !oldClassNames?.includes(it));
-  // const removed = oldClassNames?.filter((it) => !newClassNames?.includes(it));
-  // if (added) {
-  //   added.forEach((it) => el.classList.add(it));
-  // }
-  // if (removed) {
-  //   removed.forEach((it) => el.classList.remove(it));
-  // }
-
-  for (const key in vnode.props) {
-    if (key === 'key' || key === 'children' || key === 'qxIf') {
-      continue;
-    }
-    const value = vnode.props[key];
-
-    if (value === false || value === null || value === undefined) {
-      el.removeAttribute(key);
-    } else if (key.startsWith('on') && typeof value === 'function') {
-      (el as any)[key.toLocaleLowerCase()] = value;
-    } else {
-      el.setAttribute(key, value?.toString() || '');
-    }
-  }
-
-  if (vnode.marker) {
-    el.setAttribute('data-fc', vnode.marker);
-  }
-}
 
 // function cleanupDomAttributes(el: Element, vnode: IVElement) {
 //   for (const key in vnode.props) {
