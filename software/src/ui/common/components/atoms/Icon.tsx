a4add027fded48f6781f0f899d72bb45354a146f
yahiro <yahiro07@users.noreply.github.com>
2021-07-12 14:04:24 +0900

    material-iconsのサイズを補正するプロパティを追加

--- software/src/ui/common/components/atoms/Icon.tsx
+++ software/src/ui/common/components/atoms/Icon.tsx
@@ -3,17 +3,18 @@ import { FC, jsx, css } from 'qx';
 interface Props {
   spec: string;
   size?: number;
+  sizeMi?: number;
 }
 
 const style = (size?: number) => css`
   font-size: ${size ? `${size}px` : 'inherit'};
 `;
 
-export const Icon: FC<Props> = ({ spec, size }) =>
+export const Icon: FC<Props> = ({ spec, size, sizeMi }) =>
   spec.startsWith('fa ') ? (
     <i class={spec} css={style(size)} />
   ) : (
-    <i class="material-icons" css={style(size)}>
+    <i class="material-icons" css={style(sizeMi || size)}>
       {spec}
     </i>
   );
