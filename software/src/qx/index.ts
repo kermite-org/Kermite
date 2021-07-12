7ee1e647edfb3051a8a2a9f6f496783c9aa7d42f
yahiro <yahiro07@users.noreply.github.com>
2021-07-12 12:25:17 +0900

    petit-dom向けの記述をコメントアウト

--- software/src/qx/index.ts
+++ software/src/qx/index.ts
@@ -40,17 +40,17 @@ function setupAsyncRenderLoop() {
   }
 }
 
-const domRefDirective = {
-  mount(element: Element, ref: { current: Element } | ((el: Element) => void)) {
-    if (typeof ref === 'function') {
-      ref(element);
-    } else {
-      ref.current = element;
-    }
-  },
-  patch() {},
-  unmount() {},
-};
+// const domRefDirective = {
+//   mount(element: Element, ref: { current: Element } | ((el: Element) => void)) {
+//     if (typeof ref === 'function') {
+//       ref(element);
+//     } else {
+//       ref.current = element;
+//     }
+//   },
+//   patch() {},
+//   unmount() {},
+// };
 
 export function render(
   renderFn: () => JSX.Element,
