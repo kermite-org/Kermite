6beb0cd18797dd7f23aba93dc21d64c03b7a21e7
yahiro <yahiro07@users.noreply.github.com>
2021-07-17 05:41:25 +0900

    ヘルパ関数を移動

--- software/src/ui/layouter/views/editSvgView/CoordHelpers.ts
+++ software/src/ui/layouter/views/editSvgView/CoordHelpers.ts
@@ -1,4 +1,5 @@
-import { editReader } from '~/ui/layouter/models';
+import { degToRad } from '~/shared';
+import { editReader, IEditTransGroup } from '~/ui/layouter/models';
 
 export function screenToWorld(sx: number, sy: number): [x: number, y: number] {
   const { sight } = editReader;
@@ -15,6 +16,23 @@ export function getWorldMousePositionOnEditSvg(
   const sx = e.pageX - bounds.left;
   const sy = e.pageY - bounds.top;
   return screenToWorld(sx, sy);
+}
+
+export function applyInverseGroupTransform(
+  wx: number,
+  wy: number,
+  group: IEditTransGroup | undefined,
+  isMirror: boolean,
+) {
+  const mirrorMultX = isMirror ? -1 : 1;
+  const ox = group?.x || 0;
+  const oy = group?.y || 0;
+  const theta = -degToRad(group?.angle || 0) * mirrorMultX;
+  const m0x = wx - ox * mirrorMultX;
+  const m0y = wy - oy;
+  const mx = m0x * Math.cos(theta) - m0y * Math.sin(theta);
+  const my = m0x * Math.sin(theta) + m0y * Math.cos(theta);
+  return [mx, my];
 }
 
 export function getGroupOuterSvgTransformSpec(
--- software/src/ui/layouter/views/editSvgView/svgParts/KeyboardOutline.tsx
+++ software/src/ui/layouter/views/editSvgView/svgParts/KeyboardOutline.tsx
@@ -7,29 +7,12 @@ import {
   editMutations,
   IEditOutlinePoint,
   IEditOutlineShape,
-  IEditTransGroup,
 } from '~/ui/layouter/models';
 import {
+  applyInverseGroupTransform,
   getGroupOuterSvgTransformSpec,
   getWorldMousePositionOnEditSvg,
 } from '../CoordHelpers';
-
-function applyInverseGroupTransform(
-  wx: number,
-  wy: number,
-  group: IEditTransGroup | undefined,
-  isMirror: boolean,
-) {
-  const mirrorMultX = isMirror ? -1 : 1;
-  const ox = group ? group.x : 0;
-  const oy = group ? group.y : 0;
-  const theta = -degToRad(group?.angle || 0) * mirrorMultX;
-  const m0x = wx - ox * mirrorMultX;
-  const m0y = wy - oy;
-  const mx = m0x * Math.cos(theta) - m0y * Math.sin(theta);
-  const my = m0x * Math.sin(theta) + m0y * Math.cos(theta);
-  return [mx, my];
-}
 
 const cssKeyboardOutlineShapeView = css`
   fill: none;
