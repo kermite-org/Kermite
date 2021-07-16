bc92cc3bf30e134356ef1ea2bc006f693ea8a1af
yahiro <yahiro07@users.noreply.github.com>
2021-07-17 00:20:31 +0900

    グリッドピッチの選択肢を追加

--- software/src/ui/layouter/models/GridDefinitions.ts
+++ software/src/ui/layouter/models/GridDefinitions.ts
@@ -8,19 +8,25 @@ export type IGridSpecKey =
   | 'kp_div1'
   | 'kp_div2'
   | 'kp_div4'
+  | 'kp_div8'
+  | 'kp_div16'
   | 'mm_pitch10'
   | 'mm_pitch5'
   | 'mm_pitch2'
-  | 'mm_pitch1';
+  | 'mm_pitch1'
+  | 'mm_pitch05';
 
 const gridPitchSelectionKeys: IGridSpecKey[] = [
   'kp_div1',
   'kp_div2',
   'kp_div4',
+  'kp_div8',
+  'kp_div16',
   'mm_pitch10',
   'mm_pitch5',
   'mm_pitch2',
   'mm_pitch1',
+  'mm_pitch05',
 ];
 
 const gridPitchSelectionDisplayTexts: {
@@ -29,10 +35,13 @@ const gridPitchSelectionDisplayTexts: {
   kp_div1: '1/1 KP',
   kp_div2: '1/2 KP',
   kp_div4: '1/4 KP',
+  kp_div8: '1/8 KP',
+  kp_div16: '1/16 KP',
   mm_pitch10: '10mm',
   mm_pitch5: '5mm',
   mm_pitch2: '2mm',
   mm_pitch1: '1mm',
+  mm_pitch05: '0.5mm',
 };
 
 export const gridPitchSelectorOptions: ISelectorOption[] = gridPitchSelectionKeys.map(
@@ -50,10 +59,13 @@ const gridPitchSelectionKeyToGridSpecValuesTable: {
   kp_div1: { unit: 'KP', division: 1 },
   kp_div2: { unit: 'KP', division: 2 },
   kp_div4: { unit: 'KP', division: 4 },
+  kp_div8: { unit: 'KP', division: 8 },
+  kp_div16: { unit: 'KP', division: 16 },
   mm_pitch10: { unit: 'mm', pitch: 10 },
   mm_pitch5: { unit: 'mm', pitch: 5 },
   mm_pitch2: { unit: 'mm', pitch: 2 },
   mm_pitch1: { unit: 'mm', pitch: 1 },
+  mm_pitch05: { unit: 'mm', pitch: 0.5 },
 };
 
 export function decodeGridSpec(key: IGridSpecKey): IGridSpec {
