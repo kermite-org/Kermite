d5a7fcbebd967d6ed1b8dc2d0845a055ebcb7244
yahiro <yahiro07@users.noreply.github.com>
2021-07-17 13:52:11 +0900

    シリアルナンバーからのデータの読み取りを修正

--- software/src/shell/services/device/keyboardDevice/DeviceEnumerator.ts
+++ software/src/shell/services/device/keyboardDevice/DeviceEnumerator.ts
@@ -50,8 +50,8 @@ function makeKeyboardDeviceInfoFromDeviceSpec(
 ): IKeyboardDeviceInfo {
   const { path, serialNumber } = spec;
   const portName = getPortNameFromDevicePath(path) || index.toString();
-  const projectId = serialNumber.slice(0, 8);
-  const deviceInstanceCode = serialNumber.slice(8, 16);
+  const projectId = serialNumber.slice(8, 16);
+  const deviceInstanceCode = serialNumber.slice(16, 24);
   return {
     path,
     portName,
