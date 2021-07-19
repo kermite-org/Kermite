b3f32245f3bb07a396326e9f68539d78a84daa06
yahiro <yahiro07@users.noreply.github.com>
2021-07-19 20:47:34 +0900

    各プロジェクトのIDを6桁に変更

--- firmware/src/projects/dev/one/debug1/config.h
+++ firmware/src/projects/dev/one/debug1/config.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define KERMITE_PROJECT_ID "519k3PeG"
+#define KERMITE_PROJECT_ID "519k3P"
 
 #define KM0_KEYBOARD__NUM_SCAN_SLOTS 1
--- firmware/src/projects/dev/testbed/withLcd_atmega/config.h
+++ firmware/src/projects/dev/testbed/withLcd_atmega/config.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define KERMITE_PROJECT_ID "RcYEeqNX"
+#define KERMITE_PROJECT_ID "RcYEeq"
 
 #define KM0_KEYBOARD__NUM_SCAN_SLOTS 4
 
