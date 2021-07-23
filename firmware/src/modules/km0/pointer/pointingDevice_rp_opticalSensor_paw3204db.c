8930d6ff484ff1525b4019c8a36e3e63ec7a178b
yahiro <yahiro07@users.noreply.github.com>
2021-07-23 19:58:49 +0900

    halfDuplexSerialモジュールを使用

--- firmware/src/modules/km0/pointer/pointingDevice_rp_opticalSensor_paw3204db.c
+++ firmware/src/modules/km0/pointer/pointingDevice_rp_opticalSensor_paw3204db.c
@@ -1,22 +1,10 @@
 
+#include "halfDuplexSerial.h"
 #include "km0/base/bitOperations.h"
 #include "km0/device/boardIo.h"
 #include "km0/device/debugUart.h"
 #include "km0/device/digitalIo.h"
 #include "km0/device/system.h"
-#include "pico_sdk/src/rp2_common/include/hardware/gpio.h"
-#include <stdio.h>
-
-#ifndef KM0_RP_OPTICALSENSOR_PAW3204DB_PIN_CLOCK
-#error KM0_RP_OPTICALSENSOR_PAW3204DB_PIN_CLOCK is not defined
-#endif
-
-#ifndef KM0_RP_OPTICALSENSOR_PAW3204DB_PIN_DATA
-#error KM0_RP_OPTICALSENSOR_PAW3204DB_PIN_DATA is not defined
-#endif
-
-static const int pin_sclk = KM0_RP_OPTICALSENSOR_PAW3204DB_PIN_CLOCK;
-static const int pin_sdio = KM0_RP_OPTICALSENSOR_PAW3204DB_PIN_DATA;
 
 enum {
   OpticalSensorRegister_ProductId1 = 0x00,
@@ -26,95 +14,16 @@ enum {
   OpticalSensorRegister_DeltaY = 0x04
 };
 
-static void initPorts() {
-  digitalIo_setOutput(pin_sclk);
-  digitalIo_write(pin_sclk, 1);
-
-  //configure sdio as pseudo open drain
-  gpio_init(pin_sdio);
-  gpio_pull_up(pin_sdio);
-  gpio_set_dir(pin_sdio, 0);
-}
-
-static void clockHigh() {
-  digitalIo_setHigh(pin_sclk);
-}
-
-static void clockLow() {
-  digitalIo_setLow(pin_sclk);
-}
-
-static void signalOut(bool value) {
-  gpio_set_dir(pin_sdio, !value); //drive low for 0, hi-z for 1
-}
-
-static void signalHigh() {
-  gpio_set_dir(pin_sdio, 0); //sdio hi-z
-}
-
-static void signalLow() {
-  gpio_set_dir(pin_sdio, 1); //sdio drive low
-}
-
-static bool signalRead() {
-  digitalIo_read(pin_sdio);
-}
-
-static void reSyncSerial() {
-  clockLow();
-  delayUs(1); //T_RESYNC
-  clockHigh();
-  delayMs(2); //T_SIWTT, 1.7ms for normal mode
-  // delayMs(400); //T_SIWTT, 320ms(+-20%) for sleep2 mode
-}
-
-static void writeByte(uint8_t byte) {
-  for (int i = 7; i >= 0; i--) {
-    uint8_t bit = bit_read(byte, i);
-    clockLow();
-    signalOut(bit);
-    delayUs(5);
-    clockHigh();
-    delayUs(5);
-  }
-}
-
-static uint8_t readByte() {
-  uint8_t data = 0;
-  for (int i = 7; i >= 0; i--) {
-    clockLow();
-    delayUs(5);
-    clockHigh();
-    uint8_t bit = signalRead();
-    delayUs(5);
-    bit_spec(data, i, bit);
-  }
-  return data;
-}
-
-static uint8_t readData(uint8_t addr) {
-  writeByte(addr);
-  signalHigh();
-  delayUs(3); //T_HOLD
-  return readByte();
-}
-
-static void writeData(uint8_t addr, uint8_t data) {
-  addr |= 0x80;
-  writeByte(addr);
-  writeByte(data);
-}
-
 void pointingDevice_initialize() {
-  initPorts();
-  reSyncSerial();
+  halfDuplexSerial_initialize();
+  halfDuplexSerial_reSyncSerial();
 }
 
 void pointingDevice_update(int8_t *outDeltaX, int8_t *outDeltaY) {
-  uint8_t motionStatus = readData(OpticalSensorRegister_MotionStatus);
+  uint8_t motionStatus = halfDuplexSerial_readData(OpticalSensorRegister_MotionStatus);
   if (bit_read(motionStatus, 7)) {
-    int8_t deltaX = readData(OpticalSensorRegister_DeltaX);
-    int8_t deltaY = readData(OpticalSensorRegister_DeltaY);
+    int8_t deltaX = halfDuplexSerial_readData(OpticalSensorRegister_DeltaX);
+    int8_t deltaY = halfDuplexSerial_readData(OpticalSensorRegister_DeltaY);
     *outDeltaX = deltaX;
     *outDeltaY = deltaY;
   }
