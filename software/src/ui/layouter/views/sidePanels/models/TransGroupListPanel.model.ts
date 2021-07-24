5efa6118a181e764ec6269490cd9d218d84bff48
yahiro <yahiro07@users.noreply.github.com>
2021-07-24 20:56:44 +0900

    グループを選択していない状態に対応するボタンを追加

--- software/src/ui/layouter/views/sidePanels/models/TransGroupListPanel.model.ts
+++ software/src/ui/layouter/views/sidePanels/models/TransGroupListPanel.model.ts
@@ -16,15 +16,24 @@ interface ITransGroupListPartModel {
 export function useTransGroupListPartModel(): ITransGroupListPartModel {
   const { allTransGroups, currentTransGroupId } = editReader;
 
+  const groupItems = allTransGroups.map((group) => ({
+    id: group.id,
+    isActive: group.id === currentTransGroupId,
+    setActive: () => editMutations.setCurrentTransGroupById(group.id),
+  }));
+  if (groupItems.length > 0) {
+    groupItems.unshift({
+      id: '--',
+      isActive: currentTransGroupId === undefined,
+      setActive: () => editMutations.setCurrentTransGroupById(undefined),
+    });
+  }
+
   return {
     canAddGroup: true,
     canDeleteGroup: allTransGroups.length > 1 && !!currentTransGroupId,
     addGroup: () => editMutations.addTransGroup(),
     deleteGroup: () => editMutations.deleteLastTransGroup(),
-    groupItems: allTransGroups.map((group) => ({
-      id: group.id,
-      isActive: group.id === currentTransGroupId,
-      setActive: () => editMutations.setCurrentTransGroupById(group.id),
-    })),
+    groupItems,
   };
 }
