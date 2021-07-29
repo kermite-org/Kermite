2b79730e06e4f7128410cc1840833a95fe4f7d89
yahiro <yahiro07@users.noreply.github.com>
2021-07-30 01:22:16 +0900

    ファイルを移動

--- software/src/ui/components/editorParts/controls/index.ts
+++ software/src/ui/components/editorParts/controls/index.ts
@@ -1,5 +1,4 @@
 export * from './CommonStyles';
-export * from './ComponentCatalog';
 export * from './ConfigurationButton';
 export * from './DefaultSchemeButton';
 export * from './LinkIndicator';
