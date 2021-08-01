bb850f7a52b709f116152b4b964e1865a6fee221
yahiro <yahiro07@users.noreply.github.com>
2021-08-02 02:58:31 +0900

    ナビゲーションメニューを調整

--- software/src/ui/root/organisms/NavigationButtonsArea.model.ts
+++ software/src/ui/root/organisms/NavigationButtonsArea.model.ts
@@ -20,18 +20,6 @@ interface NavigationEntrySource {
 
 const entrySources: NavigationEntrySource[] = [
   {
-    pagePath: '/home',
-    pageName: 'home',
-    iconSpec: 'fa fa-home',
-    hint: 'home',
-  },
-  {
-    pagePath: '/projectSelection',
-    pageName: texts.label_sideMenu_app_projectSelection,
-    iconSpec: 'fa fa-book-open',
-    hint: 'project selection',
-  },
-  {
     pagePath: '/editor',
     pageName: texts.label_sideMenu_app_assigner,
     iconSpec: 'fa fa-keyboard',
@@ -72,6 +60,18 @@ const entrySources: NavigationEntrySource[] = [
     pageName: texts.label_sideMenu_app_settings,
     iconSpec: 'fa fa-cog',
     hint: texts.hint_sideMenu_app_settings,
+  },
+  {
+    pagePath: '/projectSelection',
+    pageName: texts.label_sideMenu_app_projectSelection,
+    iconSpec: 'fa fa-globe',
+    hint: 'project selection',
+  },
+  {
+    pagePath: '/home',
+    pageName: 'entrance',
+    iconSpec: 'fa fa-door-open',
+    hint: 'start',
   },
 ];
 
