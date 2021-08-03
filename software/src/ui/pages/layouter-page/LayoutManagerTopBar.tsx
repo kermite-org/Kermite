101d2c1bfc0a54c5eac32bee908181dc4aefb364
yahiro <yahiro07@users.noreply.github.com>
2021-08-04 01:28:28 +0900

    layouterのトップバーに編集対象選択のラジオボタンを追加

--- software/src/ui/pages/layouter-page/LayoutManagerTopBar.tsx
+++ software/src/ui/pages/layouter-page/LayoutManagerTopBar.tsx
@@ -4,6 +4,7 @@ import {
   OperationButtonWithIcon,
   ProjectAttachmentFileSelectorModal,
 } from '~/ui/components';
+import { RadioButtonLine } from '~/ui/components/molecules/RadioButtonLine';
 import { LayoutManagerMenu } from '~/ui/pages/layouter-page/LayoutManagerMenu';
 import { useLayoutManagerViewModel } from '~/ui/pages/layouter-page/LayoutManagerViewModel';
 import { makeLayoutSelectorModalViewModel } from '~/ui/pages/layouter-page/ProjectLayoutSelectorModalViewModel';
@@ -11,16 +12,35 @@ import { makeLayoutSelectorModalViewModel } from '~/ui/pages/layouter-page/Proje
 export const LayoutManagerTopBar: FC = () => {
   const vm = useLayoutManagerViewModel();
   const modalVm = makeLayoutSelectorModalViewModel(vm);
+
   return (
     <div css={style}>
-      <LayoutManagerMenu baseVm={vm} />
-      <div class="targetDisplayArea">{vm.editSourceText}</div>
-      <OperationButtonWithIcon
-        icon="save"
-        label="save"
-        disabled={!vm.canOverwrite}
-        onClick={vm.overwriteLayout}
-      />
+      <div className="first-row">
+        Edit Target
+        <RadioButtonLine
+          text="Current Profile"
+          checked={vm.editTargetRadioSelection === 'CurrentProfile'}
+          onClick={() => vm.setEditTargetRadioSelection('CurrentProfile')}
+        />
+        <RadioButtonLine
+          text="Layout File"
+          checked={vm.editTargetRadioSelection === 'LayoutFile'}
+          onClick={() => vm.setEditTargetRadioSelection('LayoutFile')}
+        />
+      </div>
+      <div
+        className="second-row"
+        qxIf={vm.editTargetRadioSelection === 'LayoutFile'}
+      >
+        <LayoutManagerMenu baseVm={vm} />
+        <div class="targetDisplayArea">{vm.editSourceText}</div>
+        <OperationButtonWithIcon
+          icon="save"
+          label="save"
+          disabled={!vm.canOverwrite}
+          onClick={vm.overwriteLayout}
+        />
+      </div>
       {modalVm && <ProjectAttachmentFileSelectorModal vm={modalVm} />}
     </div>
   );
@@ -28,20 +48,32 @@ export const LayoutManagerTopBar: FC = () => {
 
 const style = css`
   color: ${uiTheme.colors.clMainText};
-  display: flex;
+
   padding: 6px;
   padding-bottom: 0;
   gap: 0 4px;
 
-  > * {
-    flex-shrink: 0;
-  }
-
-  > .targetDisplayArea {
-    flex-grow: 1;
-    border: solid 1px ${uiTheme.colors.clPrimary};
+  > .first-row {
     display: flex;
     align-items: center;
-    padding: 0 5px;
+    gap: 15px;
+    padding: 2px 0;
+  }
+
+  > .second-row {
+    margin-top: 4px;
+    display: flex;
+
+    > * {
+      flex-shrink: 0;
+    }
+
+    > .targetDisplayArea {
+      flex-grow: 1;
+      border: solid 1px ${uiTheme.colors.clPrimary};
+      display: flex;
+      align-items: center;
+      padding: 0 5px;
+    }
   }
 `;
