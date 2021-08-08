d269b6d4a6a596c940fa400c6c05f69529a6ecb1
yahiro <yahiro07@users.noreply.github.com>
2021-08-09 08:58:32 +0900

    使われていない関数を削除

--- software/src/ui/commonModels/ProjectDataSource.ts
+++ software/src/ui/commonModels/ProjectDataSource.ts
@@ -1,8 +1,6 @@
 import { useMemo } from 'qx';
 import { IProjectResourceInfo, IResourceOrigin } from '~/shared';
-import { appUi, ipcAgent } from '~/ui/base';
-import { readSettingsResouceOrigin } from '~/ui/commonModels/DomainHelpers';
-import { globalSettingsModel } from '~/ui/commonModels/GlobalSettingsModel';
+import { ipcAgent } from '~/ui/base';
 import { useFetcher } from '~/ui/helpers';
 
 export async function fetchAllProjectResourceInfos(): Promise<
@@ -15,27 +13,8 @@ export function useAllProjectResourceInfos(): IProjectResourceInfo[] {
   return useFetcher(ipcAgent.async.projects_getAllProjectResourceInfos, []);
 }
 
-export function useProjectResourcePresenceChecker(
-  origin: IResourceOrigin,
-): boolean {
-  if (!appUi.isExecutedInApp) {
-    return false;
-  }
-  const resourceInfos = useAllProjectResourceInfos();
-  return resourceInfos.some((info) => info.origin === origin);
-}
-
 export function useProjectResourceInfos() {
   return useAllProjectResourceInfos();
-}
-
-export function useProjectResourceInfosBasedOnGlobalSettings() {
-  const origin = readSettingsResouceOrigin(globalSettingsModel.globalSettings);
-  const allResourceInfos = useAllProjectResourceInfos();
-  return useMemo(
-    () => allResourceInfos.filter((info) => info.origin === origin),
-    [allResourceInfos],
-  );
 }
 
 export function useLocalProjectResourceInfos(): IProjectResourceInfo[] {
@@ -56,11 +35,4 @@ export function useProjectInfo(
       (info) => info.origin === origin && info.projectId === projectId,
     ) || resourceInfos.find((info) => info.projectId === projectId)
   );
-}
-
-export function projectResourceInfoFilter_affectGlobalProjectSelection(
-  info: IProjectResourceInfo,
-): boolean {
-  const { globalProjectId } = globalSettingsModel.globalSettings;
-  return globalProjectId === '' || info.projectId === globalProjectId;
 }
