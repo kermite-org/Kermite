6e04534211e52d680d78915a3489a0062f43f3bb
yahiro <yahiro07@users.noreply.github.com>
2021-08-09 09:07:00 +0900

    デバイス情報欄で参照するリソースを切り替え

--- software/src/ui/commonModels/ProjectDataSource.ts
+++ software/src/ui/commonModels/ProjectDataSource.ts
@@ -1,6 +1,11 @@
 import { useMemo } from 'qx';
-import { IProjectResourceInfo, IResourceOrigin } from '~/shared';
+import {
+  IProjectPackageInfo,
+  IProjectResourceInfo,
+  IResourceOrigin,
+} from '~/shared';
 import { ipcAgent } from '~/ui/base';
+import { uiGlobalStore } from '~/ui/commonModels/UiGlobalStore';
 import { useFetcher } from '~/ui/helpers';
 
 export async function fetchAllProjectResourceInfos(): Promise<
@@ -17,8 +22,8 @@ export function useProjectResourceInfos() {
   return useAllProjectResourceInfos();
 }
 
-export function useLocalProjectResourceInfos(): IProjectResourceInfo[] {
-  const allResourceInfos = useAllProjectResourceInfos();
+export function useLocalProjectResourceInfos(): IProjectPackageInfo[] {
+  const allResourceInfos = uiGlobalStore.allProjectPackageInfos;
   return useMemo(
     () => allResourceInfos.filter((info) => info.origin === 'local'),
     [allResourceInfos],
@@ -28,8 +33,8 @@ export function useLocalProjectResourceInfos(): IProjectResourceInfo[] {
 export function useProjectInfo(
   origin?: IResourceOrigin,
   projectId?: string,
-): IProjectResourceInfo | undefined {
-  const resourceInfos = useAllProjectResourceInfos();
+): IProjectPackageInfo | undefined {
+  const resourceInfos = uiGlobalStore.allProjectPackageInfos;
   return (
     resourceInfos.find(
       (info) => info.origin === origin && info.projectId === projectId,
