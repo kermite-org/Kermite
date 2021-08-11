09f3c75a5bd99d125a83c0e15c32de3fd7ac921e
yahiro <yahiro07@users.noreply.github.com>
2021-08-12 04:43:21 +0900

    アイコンの並び順を変更

--- software/src/ui/root/organisms/NavigationButtonsArea.model.ts
+++ software/src/ui/root/organisms/NavigationButtonsArea.model.ts
@@ -57,12 +57,6 @@ const entrySources: NavigationEntrySource[] = [
     hint: texts.hint_sideMenu_app_firmwareUpdation,
   },
   {
-    pagePath: '/projectSelection',
-    pageName: texts.label_sideMenu_app_projectSelection,
-    iconSpec: 'fa fa-globe',
-    hint: 'project selection',
-  },
-  {
     pagePath: '/projectEdit',
     pageName: 'project',
     iconSpec: 'fa fa-globe',
@@ -70,12 +64,17 @@ const entrySources: NavigationEntrySource[] = [
     isAvailable: () => globalSettingsModel.isLocalProjectSelectedForEdit,
   },
   {
+    pagePath: '/projectSelection',
+    pageName: texts.label_sideMenu_app_projectSelection,
+    iconSpec: 'fa fa-globe',
+    hint: 'project selection',
+  },
+  {
     pagePath: '/settings',
     pageName: texts.label_sideMenu_app_settings,
     iconSpec: 'fa fa-cog',
     hint: texts.hint_sideMenu_app_settings,
   },
-
   {
     pagePath: '/home',
     pageName: 'entrance',
