7c9652bbda97d3a6026c3807d0acec737c2e2830
yahiro <yahiro07@users.noreply.github.com>
2021-08-13 00:26:58 +0900

    パッチでRP2040の共通キーボードに対応

--- firmware/workers2/CoreDefinitions.ts
+++ firmware/workers2/CoreDefinitions.ts
@@ -1,5 +1,7 @@
 export type IKermiteStandardKeyboaredSpec = {
-  useBoardsLedsProMicroAvr?: boolean;
+  useBoardLedsProMicroAvr?: boolean;
+  useBoardLedsProMicroRp?: boolean;
+  useBoardLedsRpiPico?: boolean;
   useDebugUart?: boolean;
   useMatrixKeyScanner?: boolean;
   useDirectWiredKeyScanner?: boolean;
@@ -50,4 +52,35 @@ export const enum Pins {
   PF5,
   PF6,
   PF7,
+
+  GP0 = 0,
+  GP1,
+  GP2,
+  GP3,
+  GP4,
+  GP5,
+  GP6,
+  GP7,
+  GP8,
+  GP9,
+  GP10,
+  GP11,
+  GP12,
+  GP13,
+  GP14,
+  GP15,
+  GP16,
+  GP17,
+  GP18,
+  GP19,
+  GP20,
+  GP21,
+  GP22,
+  GP23,
+  GP24,
+  GP25,
+  GP26,
+  GP27,
+  GP28,
+  GP29,
 }
