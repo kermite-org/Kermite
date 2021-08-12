66ebaa5617e0127285e4279226fe23e02c97c134
yahiro <yahiro07@users.noreply.github.com>
2021-08-13 00:35:39 +0900

    ヘルパ関数をファイルに分離

--- firmware/workers2/CustomKeyboardDataSerializer.ts
+++ firmware/workers2/CustomKeyboardDataSerializer.ts
@@ -1,31 +1,10 @@
 import { IKermiteStandardKeyboaredRawSpec } from '@/CoreDefinitions';
-
-function makeOutputBytes(arr: (number | boolean | undefined)[]): number[] {
-  return arr.map((value) => {
-    if (value === undefined) {
-      return 0;
-    }
-    if (value === false) {
-      return 0;
-    }
-    if (value === true) {
-      return 1;
-    }
-    if (isFinite(value)) {
-      return value;
-    }
-    return 0;
-  });
-}
-
-function padZeros(bytes: number[] | undefined, length: number): number[] {
-  return new Array(length).fill(0).map((_, i) => bytes?.[i] || 0);
-}
+import { convertArrayElementsToBytes, padZeros } from '@/Helpers';
 
 export function serializeCustomKeyboardSpec(
   spec: IKermiteStandardKeyboaredRawSpec
 ): number[] {
-  return makeOutputBytes([
+  return convertArrayElementsToBytes([
     spec.useBoardLedsProMicroAvr,
     spec.useBoardLedsProMicroRp,
     spec.useBoardLedsRpiPico,
