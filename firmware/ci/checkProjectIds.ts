aab4e4c4dcbad235ed091140f9ae203edb29cdd9
yahiro <yahiro07@users.noreply.github.com>
2021-08-13 03:08:37 +0900

    config.hのみをチェックするように変更

--- firmware/ci/checkProjectIds.ts
+++ firmware/ci/checkProjectIds.ts
@@ -19,12 +19,9 @@ const AbortOnError = process.argv.includes("--abortOnError");
 function getAllProjectVariationPaths() {
   return globSync("src/projects/**/rules.mk")
     .map((fpath) => pathDirname(fpath))
-    .filter(
-      (fpath) =>
-        fsExistsSync(pathJoin(fpath, "config.h")) &&
-        fsExistsSync(pathJoin(pathDirname(fpath), "project.json"))
-    )
-    .map((fpath) => pathRelative("src/projects", fpath));
+    .filter((fpath) => fsExistsSync(pathJoin(fpath, "config.h")))
+    .map((fpath) => pathRelative("src/projects", fpath))
+    .filter((vp) => !(vp.startsWith("dev/") || vp.startsWith("study/")));
 }
 
 interface IProjectInfo {
@@ -34,32 +31,22 @@ interface IProjectInfo {
 
 function loadProjectInfo(projectVariationPath: string): IProjectInfo {
   const projectPath = pathDirname(projectVariationPath);
-  const projectFilePath = `./src/projects/${projectPath}/project.json`;
   const configFilePath = `./src/projects/${projectVariationPath}/config.h`;
-  const projectObj = fsxReadJsonFile(projectFilePath);
-  const projectId = projectObj.projectId as string;
 
   const configContent = fsxReadTextFile(configFilePath);
-  const configProjectId = getMatched(
-    configContent,
-    /^#define KERMITE_FIRMWARE_ID "([a-zA-Z0-9]+)"$/m
-  );
+  const projectId =
+    getMatched(
+      configContent,
+      /^#define KERMITE_FIRMWARE_ID "([a-zA-Z0-9]+)"$/m
+    ) || "";
 
   try {
     if (!projectId) {
-      throw `projectId is not defined in ${projectPath}/project.json`;
-    }
-
-    if (!configProjectId) {
       throw `KERMITE_FIRMWARE_ID is not defined in ${projectVariationPath}/config.h`;
     }
 
     if (!projectId?.match(/^[a-zA-Z0-9]{6}$/)) {
       throw `invalid Project ID ${projectId} for ${projectPath}`;
-    }
-
-    if (projectId !== configProjectId) {
-      throw `inconsistent Project IDs in ${projectVariationPath}/config.h and ${projectPath}/project.json`;
     }
   } catch (error) {
     console.log(error);
