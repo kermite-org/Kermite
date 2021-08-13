f89ff07539770445b3cff9d9ccb8cdc43f79250f
yahiro <yahiro07@users.noreply.github.com>
2021-08-13 12:43:20 +0900

    リモートからプロジェクトパッケージを取得

--- software/src/shell/projectPackages/ProjectPackageProvider.ts
+++ software/src/shell/projectPackages/ProjectPackageProvider.ts
@@ -6,10 +6,10 @@ import {
 } from '~/shared';
 import { appEnv } from '~/shell/base';
 import {
+  fetchJson,
   fsxListFileBaseNames,
   fsxReadJsonFile,
   pathJoin,
-  pathResolve,
 } from '~/shell/funcs';
 
 interface IProjectPackageProvider {
@@ -54,9 +54,32 @@ async function loadProjectPackageFiles(
   );
 }
 
-async function loadMasterProjectPackageInfos(): Promise<IProjectPackageInfo[]> {
-  const packagesRoot = pathResolve('../firmware/project_packages');
-  return await loadProjectPackageFiles(packagesRoot, 'online');
+type IIndexContent = {
+  files: Record<string, string>;
+};
+
+async function loadRemoteProjectPackageInfos(): Promise<IProjectPackageInfo[]> {
+  const remoteBaseUrl = 'https://app.kermite.org/krs/resources2';
+  const indexContent = (await fetchJson(
+    `${remoteBaseUrl}/index.json`,
+  )) as IIndexContent;
+  const origin = 'online' as const;
+
+  const targetPaths = Object.keys(indexContent.files).filter((it) =>
+    it.endsWith('.kmpkg.json'),
+  );
+  return await Promise.all(
+    targetPaths.map(async (path) => {
+      const data = (await fetchJson(
+        `${remoteBaseUrl}/${path}`,
+      )) as IProjectPackageFileContent;
+      return {
+        sig: `${origin}#${data.projectId}`,
+        origin,
+        ...data,
+      };
+    }),
+  );
 }
 
 async function loadLocalProjectPackageInfos(): Promise<IProjectPackageInfo[]> {
@@ -73,7 +96,7 @@ export class ProjectPackageProvider implements IProjectPackageProvider {
   async getAllProjectPackageInfos(): Promise<IProjectPackageInfo[]> {
     if (!this.cached) {
       this.cached = [
-        ...(await loadMasterProjectPackageInfos()),
+        ...(await loadRemoteProjectPackageInfos()),
         ...(await loadLocalProjectPackageInfos()),
       ];
     }
