50b48a18ead84a7a2eef33b13928b9ddf7dfa4ea
yahiro <yahiro07@users.noreply.github.com>
2021-08-14 12:53:11 +0900

    実験コードを無効化

--- software/src/ui/commonModels/UiState.ts
+++ software/src/ui/commonModels/UiState.ts
@@ -1,12 +1,11 @@
 import { defaultUiState, IUiState } from '~/shared';
 import { ipcAgent } from '~/ui/base';
-import { dispatchCoreAction } from '~/ui/commonModels/ActionDispatcher';
 
 export const uiState: IUiState = defaultUiState;
 
 export const uiStateDriverEffect = () => {
-  dispatchCoreAction({ loadAppVersion: 1 });
-  dispatchCoreAction({ greet: { name: 'yamada', age: 20 } });
+  // dispatchCoreAction({ loadAppVersion: 1 });
+  // dispatchCoreAction({ greet: { name: 'yamada', age: 20 } });
   return ipcAgent.events.global_coreStateEvents.subscribe((diff) => {
     uiState.core = { ...uiState.core, ...diff };
     console.log({ uiState });
