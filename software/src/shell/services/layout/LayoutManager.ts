dc47ee8093b1bb4fc57511e345b3512edf7cbb30
yahiro <yahiro07@users.noreply.github.com>
2021-08-22 02:41:08 +0900

    layoutManagerModuleを実装

--- software/src/shell/services/layout/LayoutManager.ts
+++ software/src/shell/services/layout/LayoutManager.ts
@@ -4,8 +4,6 @@ import {
   createFallbackPersistKeyboardDesign,
   duplicateObjectByJsonStringifyParse,
   ILayoutEditSource,
-  ILayoutManagerCommand,
-  IPersistKeyboardDesign,
   IProjectPackageInfo,
 } from '~/shared';
 import {
@@ -18,6 +16,7 @@ import { appEnv, applicationStorage } from '~/shell/base';
 import {
   commitCoreState,
   coreState,
+  createCoreModule,
   dispatchCoreAction,
   profilesReader,
 } from '~/shell/global';
@@ -41,6 +40,13 @@ const layoutEditSourceSchema = vSchemaOneOf([
   }),
 ]);
 
+function getProjectInfo(projectId: string): IProjectPackageInfo | undefined {
+  const projectInfos = coreState.allProjectPackageInfos;
+  return projectInfos.find(
+    (info) => info.origin === 'local' && info.projectId === projectId,
+  );
+}
+
 export class LayoutManager {
   private initialized = false;
 
@@ -53,7 +59,7 @@ export class LayoutManager {
       );
       try {
         // 前回起動時に編集していたファイルの読み込みを試みる
-        await this.loadLayoutByEditSource(editSource);
+        await loadLayoutByEditSource(editSource);
       } catch (error) {
         // 読み込めない場合は初期状態のままで、特にエラーを通知しない
         console.log(`error while loading previous edit layout file`);
@@ -68,7 +74,7 @@ export class LayoutManager {
       //   this.createNewLayout();
       // }
       // 一旦CurrentProfileの編集を無効化
-      this.createNewLayout();
+      layoutManagerModule.layout_createNewLayout(1);
     }
   }
 
@@ -79,142 +85,7 @@ export class LayoutManager {
     );
   }
 
-  // eslint-disable-next-line @typescript-eslint/require-await
-  private async createNewLayout() {
-    commitCoreState({
-      layoutEditSource: { type: 'LayoutNewlyCreated' },
-      loadedLayoutData: createFallbackPersistKeyboardDesign(),
-    });
-  }
-
-  // eslint-disable-next-line @typescript-eslint/require-await
-  private async loadCurrentProfileLayout() {
-    const profile = profilesReader.getCurrentProfile();
-    if (profile) {
-      commitCoreState({
-        layoutEditSource: { type: 'CurrentProfile' },
-        loadedLayoutData: profile.keyboardDesign,
-      });
-    }
-  }
-
-  private async loadLayoutFromFile(filePath: string) {
-    const loadedDesign = await LayoutFileLoader.loadLayoutFromFile(filePath);
-    commitCoreState({
-      layoutEditSource: { type: 'File', filePath },
-      loadedLayoutData: loadedDesign,
-    });
-  }
-
-  private async saveLayoutToFile(
-    filePath: string,
-    design: IPersistKeyboardDesign,
-  ) {
-    await LayoutFileLoader.saveLayoutToFile(filePath, design);
-    commitCoreState({
-      layoutEditSource: { type: 'File', filePath },
-    });
-  }
-
-  private getProjectInfo(projectId: string): IProjectPackageInfo | undefined {
-    const projectInfos = coreState.allProjectPackageInfos;
-    return projectInfos.find(
-      (info) => info.origin === 'local' && info.projectId === projectId,
-    );
-  }
-
-  private createLayoutForProject(projectId: string, layoutName: string) {
-    const projectInfo = this.getProjectInfo(projectId);
-    if (projectInfo) {
-      const design = createFallbackPersistKeyboardDesign();
-      commitCoreState({
-        layoutEditSource: {
-          type: 'ProjectLayout',
-          projectId,
-          layoutName,
-        },
-        loadedLayoutData: design,
-      });
-    }
-  }
-
-  private loadLayoutFromProject(projectId: string, layoutName: string) {
-    const projectInfo = this.getProjectInfo(projectId);
-    if (projectInfo) {
-      const layout = projectInfo.layouts.find(
-        (it) => it.layoutName === layoutName,
-      )?.data;
-      if (layout) {
-        commitCoreState({
-          layoutEditSource: {
-            type: 'ProjectLayout',
-            projectId,
-            layoutName,
-          },
-          loadedLayoutData: layout,
-        });
-      }
-    }
-  }
-
-  private saveLayoutToProject(
-    projectId: string,
-    layoutName: string,
-    design: IPersistKeyboardDesign,
-  ) {
-    const projectInfo = this.getProjectInfo(projectId);
-    if (projectInfo) {
-      const newProjectInfo = produce(projectInfo, (draft) => {
-        const layout = draft.layouts.find((it) => it.layoutName === layoutName);
-        if (layout) {
-          layout.data = design;
-        } else {
-          draft.layouts.push({ layoutName, data: design });
-        }
-      });
-      dispatchCoreAction({
-        project_saveLocalProjectPackageInfo: newProjectInfo,
-      });
-    }
-  }
-
-  private async loadLayoutByEditSource(editSource: ILayoutEditSource) {
-    if (editSource.type === 'LayoutNewlyCreated') {
-      await this.createNewLayout();
-    } else if (editSource.type === 'CurrentProfile') {
-      await this.loadCurrentProfileLayout();
-    } else if (editSource.type === 'File') {
-      const { filePath } = editSource;
-      await this.loadLayoutFromFile(filePath);
-    } else if (editSource.type === 'ProjectLayout') {
-      const { projectId, layoutName } = editSource;
-      this.loadLayoutFromProject(projectId, layoutName);
-    }
-  }
-
-  private async overwriteCurrentLayout(design: IPersistKeyboardDesign) {
-    const { layoutEditSource } = coreState;
-    if (layoutEditSource.type === 'LayoutNewlyCreated') {
-      throw new Error('cannot save newly created layout');
-    } else if (layoutEditSource.type === 'CurrentProfile') {
-      const profile = profilesReader.getCurrentProfile();
-      if (profile) {
-        const newProfile = duplicateObjectByJsonStringifyParse(profile);
-        newProfile.keyboardDesign = design;
-        await dispatchCoreAction({
-          profile_saveCurrentProfile: { profileData: newProfile },
-        });
-      }
-    } else if (layoutEditSource.type === 'File') {
-      const { filePath } = layoutEditSource;
-      await LayoutFileLoader.saveLayoutToFile(filePath, design);
-    } else if (layoutEditSource.type === 'ProjectLayout') {
-      const { projectId, layoutName } = layoutEditSource;
-      this.saveLayoutToProject(projectId, layoutName, design);
-    }
-    commitCoreState({ loadedLayoutData: design });
-  }
-
+  /*
   private async executeCommand(command: ILayoutManagerCommand) {
     // console.log(`execute layout manager command`, JSON.stringify(command));
 
@@ -249,12 +120,13 @@ export class LayoutManager {
     }
     return true;
   }
+  */
 
   private getCurrentEditLayoutFilePath(): string | undefined {
     const { layoutEditSource } = coreState;
     if (layoutEditSource.type === 'ProjectLayout') {
       const { projectId } = layoutEditSource;
-      const projectInfo = this.getProjectInfo(projectId);
+      const projectInfo = getProjectInfo(projectId);
       if (projectInfo) {
         return appEnv.resolveUserDataFilePath(
           `data/projects/${projectInfo?.packageName}.kmpkg.json`,
@@ -273,3 +145,122 @@ export class LayoutManager {
   }
 }
 export const layoutManager = new LayoutManager();
+
+async function loadLayoutByEditSource(editSource: ILayoutEditSource) {
+  if (editSource.type === 'LayoutNewlyCreated') {
+    await layoutManagerModule.layout_createNewLayout(1);
+  } else if (editSource.type === 'CurrentProfile') {
+    await layoutManagerModule.layout_loadCurrentProfileLayout(1);
+  } else if (editSource.type === 'File') {
+    const { filePath } = editSource;
+    await layoutManagerModule.layout_loadFromFile({ filePath });
+  } else if (editSource.type === 'ProjectLayout') {
+    const { projectId, layoutName } = editSource;
+    layoutManagerModule.layout_loadProjectLayout({ projectId, layoutName });
+  }
+}
+
+export const layoutManagerModule = createCoreModule({
+  layout_createNewLayout() {
+    commitCoreState({
+      layoutEditSource: { type: 'LayoutNewlyCreated' },
+      loadedLayoutData: createFallbackPersistKeyboardDesign(),
+    });
+  },
+  layout_loadCurrentProfileLayout() {
+    const profile = profilesReader.getCurrentProfile();
+    if (profile) {
+      commitCoreState({
+        layoutEditSource: { type: 'CurrentProfile' },
+        loadedLayoutData: profile.keyboardDesign,
+      });
+    }
+  },
+  async layout_overwriteCurrentLayout({ design }) {
+    const { layoutEditSource } = coreState;
+    if (layoutEditSource.type === 'LayoutNewlyCreated') {
+      throw new Error('cannot save newly created layout');
+    } else if (layoutEditSource.type === 'CurrentProfile') {
+      const profile = profilesReader.getCurrentProfile();
+      if (profile) {
+        const newProfile = duplicateObjectByJsonStringifyParse(profile);
+        newProfile.keyboardDesign = design;
+        await dispatchCoreAction({
+          profile_saveCurrentProfile: { profileData: newProfile },
+        });
+      }
+    } else if (layoutEditSource.type === 'File') {
+      const { filePath } = layoutEditSource;
+      await LayoutFileLoader.saveLayoutToFile(filePath, design);
+    } else if (layoutEditSource.type === 'ProjectLayout') {
+      const { projectId, layoutName } = layoutEditSource;
+      layoutManagerModule.layout_saveProjectLayout({
+        projectId,
+        layoutName,
+        design,
+      });
+    }
+    commitCoreState({ loadedLayoutData: design });
+  },
+  async layout_loadFromFile({ filePath }) {
+    const loadedDesign = await LayoutFileLoader.loadLayoutFromFile(filePath);
+    commitCoreState({
+      layoutEditSource: { type: 'File', filePath },
+      loadedLayoutData: loadedDesign,
+    });
+  },
+  async layout_saveToFile({ filePath, design }) {
+    await LayoutFileLoader.saveLayoutToFile(filePath, design);
+    commitCoreState({
+      layoutEditSource: { type: 'File', filePath },
+    });
+  },
+  layout_createProjectLayout({ projectId, layoutName }) {
+    const projectInfo = getProjectInfo(projectId);
+    if (projectInfo) {
+      const design = createFallbackPersistKeyboardDesign();
+      commitCoreState({
+        layoutEditSource: {
+          type: 'ProjectLayout',
+          projectId,
+          layoutName,
+        },
+        loadedLayoutData: design,
+      });
+    }
+  },
+  layout_loadProjectLayout({ projectId, layoutName }) {
+    const projectInfo = getProjectInfo(projectId);
+    if (projectInfo) {
+      const layout = projectInfo.layouts.find(
+        (it) => it.layoutName === layoutName,
+      )?.data;
+      if (layout) {
+        commitCoreState({
+          layoutEditSource: {
+            type: 'ProjectLayout',
+            projectId,
+            layoutName,
+          },
+          loadedLayoutData: layout,
+        });
+      }
+    }
+  },
+  layout_saveProjectLayout({ projectId, layoutName, design }) {
+    const projectInfo = getProjectInfo(projectId);
+    if (projectInfo) {
+      const newProjectInfo = produce(projectInfo, (draft) => {
+        const layout = draft.layouts.find((it) => it.layoutName === layoutName);
+        if (layout) {
+          layout.data = design;
+        } else {
+          draft.layouts.push({ layoutName, data: design });
+        }
+      });
+      dispatchCoreAction({
+        project_saveLocalProjectPackageInfo: newProjectInfo,
+      });
+    }
+  },
+});
