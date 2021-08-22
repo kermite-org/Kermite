6562f4721494c89ad183bf86719bcab9c04144ef
yahiro <yahiro07@users.noreply.github.com>
2021-08-22 10:52:33 +0900

    関数をオブジェクトの外に移動

--- software/src/shell/services/layout/LayoutManager.ts
+++ software/src/shell/services/layout/LayoutManager.ts
@@ -27,46 +27,47 @@ const layoutEditSourceSchema = vSchemaOneOf([
   }),
 ]);
 
-export const layoutManager = {
-  async loadLayoutByEditSource(editSource: ILayoutEditSource) {
-    if (editSource.type === 'LayoutNewlyCreated') {
-      layoutManagerModule.layout_createNewLayout(1);
-    } else if (editSource.type === 'CurrentProfile') {
-      layoutManagerModule.layout_loadCurrentProfileLayout(1);
-    } else if (editSource.type === 'File') {
-      const { filePath } = editSource;
-      await layoutManagerModule.layout_loadFromFile({ filePath });
-    } else if (editSource.type === 'ProjectLayout') {
-      const { projectId, layoutName } = editSource;
-      layoutManagerModule.layout_loadProjectLayout({ projectId, layoutName });
-    }
-  },
-  async initializeAsync() {
-    const editSource = applicationStorage.readItemSafe<ILayoutEditSource>(
-      'layoutEditSource',
-      layoutEditSourceSchema,
-      { type: 'CurrentProfile' },
-    );
-    try {
-      // 前回起動時に編集していたファイルの読み込みを試みる
-      await this.loadLayoutByEditSource(editSource);
-    } catch (error) {
-      // 読み込めない場合は初期状態のままで、特にエラーを通知しない
-      console.log(`error while loading previous edit layout file`);
-      console.log(error);
-    }
+async function loadLayoutByEditSource(editSource: ILayoutEditSource) {
+  if (editSource.type === 'LayoutNewlyCreated') {
+    layoutManagerModule.layout_createNewLayout(1);
+  } else if (editSource.type === 'CurrentProfile') {
+    layoutManagerModule.layout_loadCurrentProfileLayout(1);
+  } else if (editSource.type === 'File') {
+    const { filePath } = editSource;
+    await layoutManagerModule.layout_loadFromFile({ filePath });
+  } else if (editSource.type === 'ProjectLayout') {
+    const { projectId, layoutName } = editSource;
+    layoutManagerModule.layout_loadProjectLayout({ projectId, layoutName });
+  }
+}
 
-    if (coreState.layoutEditSource.type === 'CurrentProfile') {
-      const profile = profilesReader.getCurrentProfile();
-      if (!profile) {
-        layoutManagerModule.layout_createNewLayout(1);
-      }
+async function initializeAsync() {
+  const editSource = applicationStorage.readItemSafe<ILayoutEditSource>(
+    'layoutEditSource',
+    layoutEditSourceSchema,
+    { type: 'CurrentProfile' },
+  );
+  try {
+    // 前回起動時に編集していたファイルの読み込みを試みる
+    await loadLayoutByEditSource(editSource);
+  } catch (error) {
+    // 読み込めない場合は初期状態のままで、特にエラーを通知しない
+    console.log(`error while loading previous edit layout file`);
+    console.log(error);
+  }
+
+  if (coreState.layoutEditSource.type === 'CurrentProfile') {
+    const profile = profilesReader.getCurrentProfile();
+    if (!profile) {
+      layoutManagerModule.layout_createNewLayout(1);
     }
-  },
-  terminate() {
-    applicationStorage.writeItem(
-      'layoutEditSource',
-      coreState.layoutEditSource,
-    );
-  },
+  }
+}
+function terminate() {
+  applicationStorage.writeItem('layoutEditSource', coreState.layoutEditSource);
+}
+
+export const layoutManager = {
+  initializeAsync,
+  terminate,
 };
