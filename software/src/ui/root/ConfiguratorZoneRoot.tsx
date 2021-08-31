21cf1de655b2d911fea3fecd135f1f1c70671b23
yahiro <yahiro07@users.noreply.github.com>
2021-09-01 06:29:10 +0900

    MainColumnRoutesをファイルに分離

--- software/src/ui/root/ConfiguratorZoneRoot.tsx
+++ software/src/ui/root/ConfiguratorZoneRoot.tsx
@@ -1,67 +1,17 @@
-import { css, jsx } from 'qx';
+import { css, FC, jsx } from 'qx';
 import { appUi, uiTheme } from '~/ui/base';
-import { siteModel, uiReaders, uiState } from '~/ui/commonStore';
+import { siteModel, uiState } from '~/ui/commonStore';
 import { CustomWindowFrame, DevToolPullTab } from '~/ui/components';
 import { LoadingOverlay } from '~/ui/components/overlay/LoadingOverlay';
 import { OnboardingFrame } from '~/ui/features';
-import {
-  ProjectEditPage,
-  ProjectLayoutEditPage,
-  ProjectPresetEditPage,
-  ProjectSelectionPage,
-  SettingsPage,
-  WelcomePage,
-} from '~/ui/pages';
-import { EditorPage } from '~/ui/pages/editor-page';
-import { FirmwareUpdatePage } from '~/ui/pages/firmware-update-page';
-import { UiLayouterPageComponent } from '~/ui/pages/layouter-page';
-import { PresetBrowserPage } from '~/ui/pages/preset-browser-page';
-import { PresetBrowserPage2 } from '~/ui/pages/preset-browser-page2';
-import { ShapePreviewPage } from '~/ui/pages/shape-preview-page';
+import { MainColumnRoutes } from '~/ui/root/MainCoulmnRoutes';
 import {
   NavigationColumn,
   WindowStatusBarSection,
   WindowTitleBarSection,
 } from '~/ui/root/sections';
 
-const MainColumnRoutes = () => {
-  const { pageSpec } = uiState;
-  if (pageSpec) {
-    return (
-      <div css={cssMainColumn}>
-        {pageSpec.type === 'projectLayoutEdit' && (
-          <ProjectLayoutEditPage spec={pageSpec} />
-        )}
-        {pageSpec.type === 'projectPresetEdit' && (
-          <ProjectPresetEditPage spec={pageSpec} />
-        )}
-      </div>
-    );
-  }
-  const { pagePath } = uiReaders;
-  return (
-    <div css={cssMainColumn}>
-      {pagePath === '/editor' && <EditorPage />}
-      {pagePath === '/layouter' && <UiLayouterPageComponent />}
-      {pagePath === '/shapePreview' && <ShapePreviewPage />}
-      {pagePath === '/firmwareUpdate' && <FirmwareUpdatePage />}
-      {pagePath === '/presetBrowser' && <PresetBrowserPage />}
-      {pagePath === '/presetBrowser2' && <PresetBrowserPage2 />}
-      {pagePath === '/settings' && <SettingsPage />}
-      {pagePath === '/projectSelection' && <ProjectSelectionPage />}
-      {pagePath === '/home' && <WelcomePage />}
-      {pagePath === '/projectEdit' && <ProjectEditPage />}
-    </div>
-  );
-};
-
-const cssMainColumn = css`
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-`;
-
-export const ConfiguratorZoneRoot = () => {
+export const ConfiguratorZoneRoot: FC = () => {
   const showOnboarding = uiState.settings.showOnboardingPanel;
   return (
     <CustomWindowFrame
