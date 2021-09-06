71b726116a6f34586f2b515fd9401591e1196860
yahiro <yahiro07@users.noreply.github.com>
2021-09-06 18:48:58 +0900

    カスタムファームウェア設定モーダルのオーバーレイの上端に不要な余白が入ってしまう問題を改善

--- software/src/ui/pages/ProjectEditPage/ProjectEditPage.tsx
+++ software/src/ui/pages/ProjectEditPage/ProjectEditPage.tsx
@@ -158,45 +158,52 @@ export const ProjectEditPage: FC = () => {
 
   return (
     <div css={style}>
-      <div>project resource edit page</div>
+      <div className="content">
+        <div>project resource edit page</div>
 
-      <div>
         <div>
-          <button onClick={createStandardFirmware}>
-            Create Standard Firmware
-          </button>
-        </div>
-        <div>
-          <button onClick={createCustomFirmware}>Create Custom Firmware</button>
-        </div>
-      </div>
-      <div>
-        <label>
-          <span>keyboard name</span>
-          <input
-            className="keyboard-name-input"
-            type="text"
-            value={keyboardName}
-            onChange={reflectValue(handleKeyboardNameChange)}
-          />
-        </label>
-      </div>
-      <div className="items-box">
-        {resourceItems.map((item) => (
-          <div key={item.itemKey}>
-            <span>
-              [{item.itemType}]{item.additionalInfoText} {item.itemName}
-            </span>
-            <button onClick={() => editResourceItem(item.itemKey)}>edit</button>
-            <button onClick={() => deleteResourceItem(item.itemKey)}>
-              delete
-            </button>
-            <button onClick={() => renameResourceItem(item.itemKey)}>
-              rename
+          <div>
+            <button onClick={createStandardFirmware}>
+              Create Standard Firmware
             </button>
           </div>
-        ))}
+          <div>
+            <button onClick={createCustomFirmware}>
+              Create Custom Firmware
+            </button>
+          </div>
+        </div>
+        <div>
+          <label>
+            <span>keyboard name</span>
+            <input
+              className="keyboard-name-input"
+              type="text"
+              value={keyboardName}
+              onChange={reflectValue(handleKeyboardNameChange)}
+            />
+          </label>
+        </div>
+        <div className="items-box">
+          {resourceItems.map((item) => (
+            <div key={item.itemKey}>
+              <span>
+                [{item.itemType}]{item.additionalInfoText} {item.itemName}
+              </span>
+              <button onClick={() => editResourceItem(item.itemKey)}>
+                edit
+              </button>
+              <button onClick={() => deleteResourceItem(item.itemKey)}>
+                delete
+              </button>
+              <button onClick={() => renameResourceItem(item.itemKey)}>
+                rename
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
+
       {editCustomFirmwareVariationName !== undefined && (
         <ProjectCustomFirmwareSetupModal
           variationName={editCustomFirmwareVariationName}
@@ -213,34 +220,36 @@ const style = css`
   height: 100%;
   padding: 15px;
 
-  > * + * {
-    margin-top: 10px;
-  }
-
-  .items-box {
-    display: inline-block;
-    border: solid 1px #888;
-    color: ${uiTheme.colors.clAltText};
-    padding: 10px;
-
+  > .content {
     > * + * {
-      margin-top: 5px;
+      margin-top: 10px;
     }
 
-    button {
-      margin-left: 5px;
+    .items-box {
+      display: inline-block;
+      border: solid 1px #888;
+      color: ${uiTheme.colors.clAltText};
+      padding: 10px;
+
+      > * + * {
+        margin-top: 5px;
+      }
+
+      button {
+        margin-left: 5px;
+      }
     }
-  }
 
-  .keyboard-name-input {
-    margin-left: 10px;
-  }
+    .keyboard-name-input {
+      margin-left: 10px;
+    }
 
-  pre {
-    border: solid 1px #888;
-    color: #222;
-    padding: 10px;
-    width: 300px;
-    font-size: 14px;
+    pre {
+      border: solid 1px #888;
+      color: #222;
+      padding: 10px;
+      width: 300px;
+      font-size: 14px;
+    }
   }
 `;
