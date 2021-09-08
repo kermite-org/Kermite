276c12620ddd8480259e4d29c5b902d85e388c58
yahiro <yahiro07@users.noreply.github.com>
2021-09-09 06:15:08 +0900

    リソース名のチェック処理とモーダルの表示を分離

--- software/src/ui/helpers/ResourceManagementUtils.ts
+++ software/src/ui/helpers/ResourceManagementUtils.ts
@@ -1,33 +1,26 @@
 import { modalAlert, modalTextEdit } from '~/ui/components';
 
 export const resourceManagementUtils = {
-  async checkValidResourceName(
+  checkValidResourceName(
     resourceName: string,
     existingResourceNames: string[],
     resourceTypeNameText: string,
     checkCaseSensitive?: boolean,
-  ): Promise<boolean> {
+  ): string | 'ok' {
     // eslint-disable-next-line no-irregular-whitespace
     // eslint-disable-next-line no-misleading-character-class
     if (!resourceName.match(/^[^/./\\:*?"<>| \u3000\u0e49]+$/)) {
-      await modalAlert(
-        `${resourceName} is not a valid ${resourceTypeNameText}. operation cancelled.`,
-      );
-      return false;
+      return `${resourceName} is not a valid ${resourceTypeNameText}.`;
     }
     const existingName = checkCaseSensitive
       ? existingResourceNames.find((it) => it === resourceName)
       : existingResourceNames.find(
           (it) => it.toLowerCase() === resourceName.toLowerCase(),
         );
-
     if (existingName) {
-      await modalAlert(
-        `${existingName} is already exists. operation cancelled.`,
-      );
-      return false;
+      return `${existingName} is already exists.`;
     }
-    return true;
+    return 'ok';
   },
 
   async inputSavingResourceName(args: {
@@ -52,16 +45,17 @@ export const resourceManagementUtils = {
       defaultText,
     });
     if (resourceName !== undefined) {
-      if (
-        await resourceManagementUtils.checkValidResourceName(
-          resourceName,
-          existingResourceNames,
-          resourceTypeNameText,
-          checkCaseSensitive,
-        )
-      ) {
-        return resourceName;
+      const res = resourceManagementUtils.checkValidResourceName(
+        resourceName,
+        existingResourceNames,
+        resourceTypeNameText,
+        checkCaseSensitive,
+      );
+      if (res !== 'ok') {
+        await modalAlert(`${res} operation cancelled.`);
+        return undefined;
       }
+      return resourceName;
     }
     return undefined;
   },
