d7b2c6fd141271e2f0d7e6ebac831a4427fe1257
yahiro <yahiro07@users.noreply.github.com>
2021-09-11 23:08:00 +0900

    書き込み時にキーボード定義のフォーマットをチェック

--- software/src/shell/services/firmwareUpdate/firmwareBinaryPatchApplier/CustomKeyboardDataSerializer.ts
+++ software/src/shell/services/firmwareUpdate/firmwareBinaryPatchApplier/CustomKeyboardDataSerializer.ts
@@ -1,4 +1,4 @@
-import { IKermiteStandardKeyboardSpec } from '~/shared';
+import { IKermiteStandardKeyboardSpec, isNumberInRange } from '~/shared';
 import { PinNameToPinNumberMap } from '~/shell/services/firmwareUpdate/firmwareBinaryPatchApplier/DataTables';
 import {
   convertArrayElementsToBytes,
@@ -10,13 +10,84 @@ import { IStandardKeyboardInjectedMetaData } from '~/shell/services/firmwareUpda
 
 function mapPinNameToPinNumber(pinName: string | undefined): number {
   const pinNumber = PinNameToPinNumberMap[pinName || ''];
-  return isFinite(pinNumber) ? pinNumber : -1;
+  if (pinNumber === undefined) {
+    throw new Error(`invalid pinName ${pinName}`);
+  }
+  return pinNumber;
+}
+
+function checkKeyboardSpec(spec: IKermiteStandardKeyboardSpec): boolean {
+  const {
+    baseFirmwareType,
+    useBoardLedsProMicroAvr,
+    useBoardLedsProMicroRp,
+    useBoardLedsRpiPico,
+    useMatrixKeyScanner,
+    matrixRowPins,
+    matrixColumnPins,
+    useDirectWiredKeyScanner,
+    directWiredPins,
+    useEncoder,
+    encoderPins,
+    useLighting,
+    lightingPin,
+    lightingNumLeds,
+  } = spec;
+
+  if (baseFirmwareType === 'AvrUnified' || baseFirmwareType === 'AvrSplit') {
+    if (useBoardLedsProMicroRp || useBoardLedsRpiPico) {
+      return false;
+    }
+  } else if (
+    baseFirmwareType === 'RpUnified' ||
+    baseFirmwareType === 'RpSplit'
+  ) {
+    if (useBoardLedsProMicroAvr) {
+      return false;
+    }
+  } else {
+    return false;
+  }
+  if (
+    useMatrixKeyScanner &&
+    !(
+      matrixRowPins &&
+      matrixColumnPins &&
+      matrixRowPins.length > 0 &&
+      matrixColumnPins.length > 0
+    )
+  ) {
+    return false;
+  }
+  if (
+    useDirectWiredKeyScanner &&
+    !(directWiredPins && directWiredPins.length > 0)
+  ) {
+    return false;
+  }
+  if (useEncoder && !(encoderPins && encoderPins.length === 2)) {
+    return false;
+  }
+  if (
+    useLighting &&
+    !(
+      lightingPin &&
+      lightingNumLeds !== undefined &&
+      isNumberInRange(lightingNumLeds, 0, 256)
+    )
+  ) {
+    return false;
+  }
+  return true;
 }
 
 export function serializeCustomKeyboardSpec(
   spec: IKermiteStandardKeyboardSpec,
   meta: IStandardKeyboardInjectedMetaData,
 ): number[] {
+  if (!checkKeyboardSpec(spec)) {
+    throw new Error(`invalid keyboard spec ${JSON.stringify(spec)}`);
+  }
   let numMatrixRows = 0;
   let numMatrixColumns = 0;
   let numDirectWiredKeys = 0;
@@ -28,14 +99,11 @@ export function serializeCustomKeyboardSpec(
   ) {
     numMatrixRows = spec.matrixRowPins.length;
     numMatrixColumns = spec.matrixColumnPins.length;
-    keyScannerPins.push(
-      ...(spec.matrixRowPins || []),
-      ...(spec.matrixColumnPins || []),
-    );
+    keyScannerPins.push(...spec.matrixRowPins, ...spec.matrixColumnPins);
   }
-  if (spec.useDirectWiredKeyScanner) {
-    numDirectWiredKeys = spec.directWiredPins?.length || 0;
-    keyScannerPins.push(...(spec.directWiredPins || []));
+  if (spec.useDirectWiredKeyScanner && !!spec.directWiredPins) {
+    numDirectWiredKeys = spec.directWiredPins.length;
+    keyScannerPins.push(...spec.directWiredPins);
   }
 
   const pinDefinitionsBytes = padByteArray(
