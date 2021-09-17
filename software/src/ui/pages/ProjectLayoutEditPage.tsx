5bd44386de0a17383cfcddd1bce072deccac4d47
yahiro <yahiro07@users.noreply.github.com>
2021-09-17 19:18:34 +0900

    プロジェクトレイアウト編集画面で保存ボタン押下時にページを閉じないように変更

--- software/src/ui/pages/ProjectLayoutEditPage.tsx
+++ software/src/ui/pages/ProjectLayoutEditPage.tsx
@@ -2,11 +2,12 @@ import { css, FC, jsx } from 'qx';
 import { fallbackProjectLayoutEntry, IProjectLayoutEntry } from '~/shared';
 import { uiTheme } from '~/ui/base';
 import { IPageSpec_ProjectLayoutEdit } from '~/ui/commonModels';
-import { projectPackagesWriter, uiActions, uiReaders } from '~/ui/commonStore';
+import { projectPackagesWriter, uiReaders } from '~/ui/commonStore';
 import { RouteHeaderBar } from '~/ui/components/organisms/RouteHeaderBar/RouteHeaderBar';
 import {
   LayouterGeneralComponent,
   LayouterGeneralComponent_OutputPropsSupplier,
+  UiLayouterCore,
 } from '~/ui/features';
 import { useMemoEx } from '~/ui/helpers';
 
@@ -14,7 +15,7 @@ type Props = {
   spec: IPageSpec_ProjectLayoutEdit;
 };
 
-const readers = {
+const helpers = {
   getSourceLayoutEntryOrCreate(layoutName: string): IProjectLayoutEntry {
     const projectInfo = uiReaders.editTargetProject;
     const layoutEntry = projectInfo?.layouts.find(
@@ -25,18 +26,20 @@ const readers = {
 };
 
 export const ProjectLayoutEditPage: FC<Props> = ({ spec: { layoutName } }) => {
-  const sourceLayoutEntry = useMemoEx(readers.getSourceLayoutEntryOrCreate, [
+  const sourceLayoutEntry = useMemoEx(helpers.getSourceLayoutEntryOrCreate, [
     layoutName,
   ]);
   const { isModified, emitSavingDesign } =
     LayouterGeneralComponent_OutputPropsSupplier;
 
   const saveHandler = () => {
-    projectPackagesWriter.saveLocalProjectLayout({
+    const newLayoutEntry: IProjectLayoutEntry = {
       ...sourceLayoutEntry,
       data: emitSavingDesign(),
-    });
-    uiActions.closeSubPage();
+    };
+    projectPackagesWriter.saveLocalProjectLayout(newLayoutEntry);
+    // uiActions.closeSubPage();
+    UiLayouterCore.rebase();
   };
   return (
     <div css={style}>
