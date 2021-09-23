88f2cf22f3001e5098fd61a91a3a3e04b3ed5faf
yahiro <yahiro07@users.noreply.github.com>
2021-09-24 03:45:17 +0900

    デバイス画面の表示を調整

---
+++ software/src/ui/pages/firmware-update-page/sections/Components.tsx
@@ -0,0 +1,13 @@
+import { styled } from 'qx';
+
+export const PartHeader = styled.div`
+  &:before {
+    content: '■';
+    margin-right: 2px;
+  }
+`;
+
+export const PartBody = styled.div`
+  margin-top: 10px;
+  margin-left: 5px;
+`;
