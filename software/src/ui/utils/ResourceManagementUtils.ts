46f2f7462c92f4be082d4bad2f1aa030cd52300d
yahiro <yahiro07@users.noreply.github.com>
2021-10-02 17:04:28 +0900

    リソース名をチェックする関数を移動

--- software/src/ui/utils/ResourceManagementUtils.ts
+++ software/src/ui/utils/ResourceManagementUtils.ts
@@ -1,39 +1,14 @@
+import { validateResourceName } from '~/shared';
 import { modalTextEdit } from '~/ui/components';
 
 export const resourceManagementUtils = {
-  checkValidResourceName(
-    resourceName: string,
-    resourceTypeNameText: string,
-    existingResourceNames?: string[],
-    allowDifferentCasingVariants?: boolean,
-  ): string | undefined {
-    // eslint-disable-next-line no-irregular-whitespace
-    // eslint-disable-next-line no-misleading-character-class
-    if (resourceName.match(/[/./\\:*?"<>| \u3000\u0e49]/)) {
-      return `${resourceName} is not a valid ${resourceTypeNameText}.`;
-    }
-    if (resourceName.length > 32) {
-      return `${resourceTypeNameText} should be no more than 32 characters.`;
-    }
-    if (existingResourceNames) {
-      const existingName = allowDifferentCasingVariants
-        ? existingResourceNames.find((it) => it === resourceName)
-        : existingResourceNames.find(
-            (it) => it.toLowerCase() === resourceName.toLowerCase(),
-          );
-      if (existingName) {
-        return `${existingName} already exists.`;
-      }
-    }
-    return undefined;
-  },
   makeResourceNameValidator(
     resourceTypeNameText: string,
     existingResourceNames?: string[],
     allowDifferentCasingVariants?: boolean,
   ): (text: string) => string | undefined {
     return (resourceName) =>
-      resourceManagementUtils.checkValidResourceName(
+      validateResourceName(
         resourceName,
         resourceTypeNameText,
         existingResourceNames,
