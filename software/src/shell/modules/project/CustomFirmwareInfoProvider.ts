fc798ed6f6c9889e5ff75539511a657961f578be
yahiro <yahiro07@users.noreply.github.com>
2021-10-05 09:36:15 +0900

    カスタムファームウェアの列挙処理を分離

---
+++ software/src/shell/modules/project/CustomFirmwareInfoProvider.ts
@@ -0,0 +1,41 @@
+import { ICustomFirmwareInfo, IFirmwareTargetDevice } from '~/shared';
+import { appConfig } from '~/shell/base';
+import { cacheRemoteResource, fetchJson } from '~/shell/funcs';
+
+type IIndexFirmwaresContent = {
+  firmwares: {
+    firmwareId: string;
+    firmwareProjectPath: string;
+    variationName: string;
+    targetDevice: IFirmwareTargetDevice;
+    buildResult: 'success' | 'failure';
+    firmwareFileName: string;
+    metadataFileName: string;
+    releaseBuildRevision: number;
+    buildTimestamp: string;
+  }[];
+};
+
+function mapIndexFirmwareEntryToCustomFirmwareInfo(
+  entry: IIndexFirmwaresContent['firmwares'][0],
+): ICustomFirmwareInfo {
+  return {
+    firmwareId: entry.firmwareId,
+    firmwareProjectPath: entry.firmwareProjectPath,
+    variationName: entry.variationName,
+    targetDevice: entry.targetDevice,
+    buildRevision: entry.releaseBuildRevision,
+    buildTimestamp: entry.buildTimestamp,
+  };
+}
+
+export const CustomFirmwareInfoProvider = {
+  async getAllCustomFirmwareInfos(): Promise<ICustomFirmwareInfo[]> {
+    const { onlineResourcesBaseUrl } = appConfig;
+    const data = (await cacheRemoteResource(
+      fetchJson,
+      `${onlineResourcesBaseUrl}/index.firmwares.json`,
+    )) as IIndexFirmwaresContent;
+    return data.firmwares.map(mapIndexFirmwareEntryToCustomFirmwareInfo);
+  },
+};
