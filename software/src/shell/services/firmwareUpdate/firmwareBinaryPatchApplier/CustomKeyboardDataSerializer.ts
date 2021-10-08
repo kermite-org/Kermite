e922b2d6392be72eaadc1f002a79631f03681156
yahiro <yahiro07@users.noreply.github.com>
2021-10-09 04:23:37 +0900

    書き込み時のバリデーションでodd splitに対応

--- software/src/shell/services/firmwareUpdate/firmwareBinaryPatchApplier/CustomKeyboardDataSerializer.ts
+++ software/src/shell/services/firmwareUpdate/firmwareBinaryPatchApplier/CustomKeyboardDataSerializer.ts
@@ -16,32 +16,74 @@ function mapPinNameToPinNumber(pinName: string | undefined): number {
   return pinNumber;
 }
 
+const checkMatrixPins = (
+  rowsPins: string[] | undefined,
+  columnsPins: string[] | undefined,
+) => rowsPins && columnsPins && rowsPins.length > 0 && columnsPins.length > 0;
+
+const checkDirectWiredPins = (pins: string[] | undefined) =>
+  pins && pins.length > 0;
+
+const checkEncoderPins = (pins: string[] | undefined, isSplit: boolean) =>
+  pins &&
+  ((!isSplit && [2, 4, 6].includes(pins.length)) ||
+    (isSplit && pins.length === 2));
+
+const checkLightingNumLeds = (num: number | undefined) =>
+  num !== undefined && isNumberInRange(num, 1, 256);
+
+const checkMatrixPins2 = (
+  rowsPins: string[] | undefined,
+  columnsPins: string[] | undefined,
+) =>
+  (rowsPins && columnsPins && rowsPins.length > 0 && columnsPins.length > 0) ||
+  (!rowsPins && !columnsPins);
+
+const checkDirectWiredPins2 = (pins: string[] | undefined) =>
+  pins ? pins.length > 0 : true;
+
+const checkEncoderPins2 = (pins: string[] | undefined, isSplit: boolean) =>
+  (pins &&
+    ((!isSplit && [2, 4, 6].includes(pins.length)) ||
+      (isSplit && pins.length === 2))) ||
+  true;
+
+const checkLightingNumLeds2 = (num: number | undefined) =>
+  num !== undefined ? isNumberInRange(num, 1, 256) : true;
+
 function checkKeyboardSpec(spec: IKermiteStandardKeyboardSpec): boolean {
   const {
-    baseFirmwareType,
+    baseFirmwareType: fw,
     useBoardLedsProMicroAvr,
     useBoardLedsProMicroRp,
     useBoardLedsRpiPico,
     useMatrixKeyScanner,
+    useDirectWiredKeyScanner,
+    useEncoder,
     matrixRowPins,
     matrixColumnPins,
-    useDirectWiredKeyScanner,
     directWiredPins,
-    useEncoder,
     encoderPins,
+    matrixRowPinsR,
+    matrixColumnPinsR,
+    directWiredPinsR,
+    encoderPinsR,
     useLighting,
     lightingPin,
     lightingNumLeds,
+    lightingNumLedsR,
     singleWireSignalPin,
   } = spec;
 
   const isAvr =
-    baseFirmwareType === 'AvrUnified' || baseFirmwareType === 'AvrSplit';
-  const isRp =
-    baseFirmwareType === 'RpUnified' || baseFirmwareType === 'RpSplit';
-
+    fw === 'AvrUnified' || fw === 'AvrSplit' || fw === 'AvrOddSplit';
+  const isRp = fw === 'RpUnified' || fw === 'RpSplit' || fw === 'RpOddSplit';
   const isSplit =
-    baseFirmwareType === 'AvrSplit' || baseFirmwareType === 'RpSplit';
+    fw === 'AvrSplit' ||
+    fw === 'RpSplit' ||
+    fw === 'AvrOddSplit' ||
+    fw === 'RpOddSplit';
+  const isOddSplit = fw === 'AvrOddSplit' || fw === 'RpOddSplit';
 
   if (isAvr) {
     if (useBoardLedsProMicroRp || useBoardLedsRpiPico) {
@@ -59,42 +101,66 @@ function checkKeyboardSpec(spec: IKermiteStandardKeyboardSpec): boolean {
     return false;
   }
 
-  if (
-    useMatrixKeyScanner &&
-    !(
-      matrixRowPins &&
-      matrixColumnPins &&
-      matrixRowPins.length > 0 &&
-      matrixColumnPins.length > 0
-    )
-  ) {
-    return false;
-  }
-  if (
-    useDirectWiredKeyScanner &&
-    !(directWiredPins && directWiredPins.length > 0)
-  ) {
-    return false;
-  }
-  if (useEncoder) {
-    const valid =
-      encoderPins &&
-      ((!isSplit && [2, 4, 6].includes(encoderPins.length)) ||
-        (isSplit && encoderPins.length === 2));
-    if (!valid) {
+  if (!isOddSplit) {
+    if (
+      useMatrixKeyScanner &&
+      !checkMatrixPins(matrixRowPins, matrixColumnPins)
+    ) {
+      return false;
+    }
+    if (useDirectWiredKeyScanner && !checkDirectWiredPins(directWiredPins)) {
+      return false;
+    }
+    if (useEncoder && !checkEncoderPins(encoderPins, isSplit)) {
+      return false;
+    }
+    if (
+      useLighting &&
+      !(lightingPin && checkLightingNumLeds(lightingNumLeds))
+    ) {
+      return false;
+    }
+  } else {
+    if (
+      useMatrixKeyScanner &&
+      !(
+        checkMatrixPins2(matrixRowPins, matrixColumnPins) &&
+        checkMatrixPins2(matrixRowPinsR, matrixColumnPinsR)
+      )
+    ) {
+      return false;
+    }
+    if (
+      useDirectWiredKeyScanner &&
+      !(
+        checkDirectWiredPins2(directWiredPins) &&
+        checkDirectWiredPins2(directWiredPinsR)
+      )
+    ) {
+      return false;
+    }
+    if (useEncoder) {
+      if (
+        !(
+          checkEncoderPins2(encoderPins, isSplit) &&
+          checkEncoderPins2(encoderPinsR, isSplit)
+        )
+      ) {
+        return false;
+      }
+    }
+    if (
+      useLighting &&
+      !(
+        lightingPin &&
+        checkLightingNumLeds2(lightingNumLeds) &&
+        checkLightingNumLeds2(lightingNumLedsR)
+      )
+    ) {
       return false;
     }
   }
-  if (
-    useLighting &&
-    !(
-      lightingPin &&
-      lightingNumLeds !== undefined &&
-      isNumberInRange(lightingNumLeds, 0, 256)
-    )
-  ) {
-    return false;
-  }
+
   return true;
 }
 
