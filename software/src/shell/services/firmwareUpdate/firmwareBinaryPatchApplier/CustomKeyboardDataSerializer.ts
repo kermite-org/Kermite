5636fcb390299b7bd5c461c4802b1359dcd3fa4a
yahiro <yahiro07@users.noreply.github.com>
2021-10-09 04:35:57 +0900

    validatorをファイルに分離

--- software/src/shell/services/firmwareUpdate/firmwareBinaryPatchApplier/CustomKeyboardDataSerializer.ts
+++ software/src/shell/services/firmwareUpdate/firmwareBinaryPatchApplier/CustomKeyboardDataSerializer.ts
@@ -1,4 +1,4 @@
-import { IKermiteStandardKeyboardSpec, isNumberInRange } from '~/shared';
+import { IKermiteStandardKeyboardSpec } from '~/shared';
 import { PinNameToPinNumberMap } from '~/shell/services/firmwareUpdate/firmwareBinaryPatchApplier/DataTables';
 import {
   convertArrayElementsToBytes,
@@ -6,6 +6,7 @@ import {
   padByteArray,
   stringToEmbedBytes,
 } from '~/shell/services/firmwareUpdate/firmwareBinaryPatchApplier/Helpers';
+import { checkStandardKeyboardSpec } from '~/shell/services/firmwareUpdate/firmwareBinaryPatchApplier/StandardKeyboardSpecValidator';
 import { IStandardKeyboardInjectedMetaData } from '~/shell/services/firmwareUpdate/firmwareBinaryPatchApplier/Types';
 
 function mapPinNameToPinNumber(pinName: string | undefined): number {
@@ -14,154 +15,6 @@ function mapPinNameToPinNumber(pinName: string | undefined): number {
     throw new Error(`invalid pinName ${pinName}`);
   }
   return pinNumber;
-}
-
-const checkMatrixPins = (
-  rowsPins: string[] | undefined,
-  columnsPins: string[] | undefined,
-) => rowsPins && columnsPins && rowsPins.length > 0 && columnsPins.length > 0;
-
-const checkDirectWiredPins = (pins: string[] | undefined) =>
-  pins && pins.length > 0;
-
-const checkEncoderPins = (pins: string[] | undefined, isSplit: boolean) =>
-  pins &&
-  ((!isSplit && [2, 4, 6].includes(pins.length)) ||
-    (isSplit && pins.length === 2));
-
-const checkLightingNumLeds = (num: number | undefined) =>
-  num !== undefined && isNumberInRange(num, 1, 256);
-
-const checkMatrixPins2 = (
-  rowsPins: string[] | undefined,
-  columnsPins: string[] | undefined,
-) =>
-  (rowsPins && columnsPins && rowsPins.length > 0 && columnsPins.length > 0) ||
-  (!rowsPins && !columnsPins);
-
-const checkDirectWiredPins2 = (pins: string[] | undefined) =>
-  pins ? pins.length > 0 : true;
-
-const checkEncoderPins2 = (pins: string[] | undefined, isSplit: boolean) =>
-  (pins &&
-    ((!isSplit && [2, 4, 6].includes(pins.length)) ||
-      (isSplit && pins.length === 2))) ||
-  true;
-
-const checkLightingNumLeds2 = (num: number | undefined) =>
-  num !== undefined ? isNumberInRange(num, 1, 256) : true;
-
-function checkKeyboardSpec(spec: IKermiteStandardKeyboardSpec): boolean {
-  const {
-    baseFirmwareType: fw,
-    useBoardLedsProMicroAvr,
-    useBoardLedsProMicroRp,
-    useBoardLedsRpiPico,
-    useMatrixKeyScanner,
-    useDirectWiredKeyScanner,
-    useEncoder,
-    matrixRowPins,
-    matrixColumnPins,
-    directWiredPins,
-    encoderPins,
-    matrixRowPinsR,
-    matrixColumnPinsR,
-    directWiredPinsR,
-    encoderPinsR,
-    useLighting,
-    lightingPin,
-    lightingNumLeds,
-    lightingNumLedsR,
-    singleWireSignalPin,
-  } = spec;
-
-  const isAvr =
-    fw === 'AvrUnified' || fw === 'AvrSplit' || fw === 'AvrOddSplit';
-  const isRp = fw === 'RpUnified' || fw === 'RpSplit' || fw === 'RpOddSplit';
-  const isSplit =
-    fw === 'AvrSplit' ||
-    fw === 'RpSplit' ||
-    fw === 'AvrOddSplit' ||
-    fw === 'RpOddSplit';
-  const isOddSplit = fw === 'AvrOddSplit' || fw === 'RpOddSplit';
-
-  if (isAvr) {
-    if (useBoardLedsProMicroRp || useBoardLedsRpiPico) {
-      return false;
-    }
-  } else if (isRp) {
-    if (useBoardLedsProMicroAvr) {
-      return false;
-    }
-  } else {
-    return false;
-  }
-
-  if (isSplit && !singleWireSignalPin) {
-    return false;
-  }
-
-  if (!isOddSplit) {
-    if (
-      useMatrixKeyScanner &&
-      !checkMatrixPins(matrixRowPins, matrixColumnPins)
-    ) {
-      return false;
-    }
-    if (useDirectWiredKeyScanner && !checkDirectWiredPins(directWiredPins)) {
-      return false;
-    }
-    if (useEncoder && !checkEncoderPins(encoderPins, isSplit)) {
-      return false;
-    }
-    if (
-      useLighting &&
-      !(lightingPin && checkLightingNumLeds(lightingNumLeds))
-    ) {
-      return false;
-    }
-  } else {
-    if (
-      useMatrixKeyScanner &&
-      !(
-        checkMatrixPins2(matrixRowPins, matrixColumnPins) &&
-        checkMatrixPins2(matrixRowPinsR, matrixColumnPinsR)
-      )
-    ) {
-      return false;
-    }
-    if (
-      useDirectWiredKeyScanner &&
-      !(
-        checkDirectWiredPins2(directWiredPins) &&
-        checkDirectWiredPins2(directWiredPinsR)
-      )
-    ) {
-      return false;
-    }
-    if (useEncoder) {
-      if (
-        !(
-          checkEncoderPins2(encoderPins, isSplit) &&
-          checkEncoderPins2(encoderPinsR, isSplit)
-        )
-      ) {
-        return false;
-      }
-    }
-    if (
-      useLighting &&
-      !(
-        lightingPin &&
-        checkLightingNumLeds2(lightingNumLeds) &&
-        checkLightingNumLeds2(lightingNumLedsR)
-      )
-    ) {
-      return false;
-    }
-  }
-
-  return true;
 }
 
 export function serializeCommonKeyboardMetadata(
@@ -187,7 +40,7 @@ export function serializeCommonKeyboardMetadata(
 export function serializeCustomKeyboardSpec_Unified(
   spec: IKermiteStandardKeyboardSpec,
 ): number[] {
-  if (!checkKeyboardSpec(spec)) {
+  if (!checkStandardKeyboardSpec(spec)) {
     throw new Error(`invalid keyboard spec ${JSON.stringify(spec)}`);
   }
   let numMatrixRows = 0;
@@ -247,7 +100,7 @@ export function serializeCustomKeyboardSpec_Unified(
 export function serializeCustomKeyboardSpec_Split(
   spec: IKermiteStandardKeyboardSpec,
 ): number[] {
-  if (!checkKeyboardSpec(spec)) {
+  if (!checkStandardKeyboardSpec(spec)) {
     throw new Error(`invalid keyboard spec ${JSON.stringify(spec)}`);
   }
   let numMatrixRows = 0;
@@ -316,7 +169,7 @@ export function serializeCustomKeyboardSpec_Split(
 export function serializeCustomKeyboardSpec_OddSplit(
   spec: IKermiteStandardKeyboardSpec,
 ): number[] {
-  if (!checkKeyboardSpec(spec)) {
+  if (!checkStandardKeyboardSpec(spec)) {
     throw new Error(`invalid keyboard spec ${JSON.stringify(spec)}`);
   }
   let numMatrixRows = 0;
