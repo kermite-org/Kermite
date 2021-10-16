264f84087ddc71a4caebb4043190ba24889a2cf9
yahiro <yahiro07@users.noreply.github.com>
2021-10-16 22:02:09 +0900

    左右対称の分割キーボードでのキー配置を実装

--- software/src/ui/features/ProjectQuickSetupPart/sections/LayoutConfigurationSection/helpers.ts
+++ software/src/ui/features/ProjectQuickSetupPart/sections/LayoutConfigurationSection/helpers.ts
@@ -9,9 +9,17 @@ import {
 } from '~/shared';
 import { ILayoutGeneratorOptions } from '~/ui/features/ProjectQuickSetupPart/ProjectQuickSetupPartTypes';
 
-const splitKeyboardTypes: IStandardBaseFirmwareType[] = [
+const unifiedKeyboardTypes: IStandardBaseFirmwareType[] = [
+  'AvrUnified',
+  'RpUnified',
+];
+
+const evenSplitKeyboardTypes: IStandardBaseFirmwareType[] = [
   'AvrSplit',
   'RpSplit',
+];
+
+const oddSplitKeyboardTypes: IStandardBaseFirmwareType[] = [
   'AvrOddSplit',
   'RpOddSplit',
 ];
@@ -42,19 +50,48 @@ function makeMatrixKeyEntities(
     };
   });
 }
+
+function makeMatrixKeyEntitiesW(
+  nx: number,
+  ny: number,
+  offsetX: number,
+  offsetY: number,
+  isCentered: boolean,
+  invertX: boolean,
+  invertY: boolean,
+  keyIndexOffset: number,
+) {
+  const xs = generateNumberSequence(nx);
+  const ys = generateNumberSequence(ny);
+  shiftNumbers(xs, offsetX);
+  shiftNumbers(ys, offsetY);
+  if (invertX) {
+    xs.reverse();
+  }
+  if (invertY) {
+    ys.reverse();
+  }
+  return makeMatrixKeyEntities(xs, ys, keyIndexOffset);
+}
+
 export function createLayoutFromFirmwareSpec(
   spec: IKermiteStandardKeyboardSpec,
   layoutOptions: ILayoutGeneratorOptions,
 ): IDisplayKeyboardDesign {
   const design = createFallbackPersistKeyboardDesign();
-  const isSplit = splitKeyboardTypes.includes(spec.baseFirmwareType);
+  const isUnified = unifiedKeyboardTypes.includes(spec.baseFirmwareType);
+  const isEvenSplit = evenSplitKeyboardTypes.includes(spec.baseFirmwareType);
+  const isOddSplit = oddSplitKeyboardTypes.includes(spec.baseFirmwareType);
+
   const { placementOrigin, invertX, invertY } = layoutOptions;
+  const isTopLeft = placementOrigin === 'topLeft';
   const isCentered = placementOrigin === 'center';
   if (isCentered) {
     design.setup.placementAnchor = 'center';
   }
+  const splitXOffset = 1;
 
-  if (!isSplit) {
+  if (isUnified) {
     if (
       spec.useMatrixKeyScanner &&
       spec.matrixRowPins &&
@@ -62,20 +99,65 @@ export function createLayoutFromFirmwareSpec(
     ) {
       const nx = spec.matrixColumnPins.length;
       const ny = spec.matrixRowPins.length;
-      const xs = generateNumberSequence(nx);
-      const ys = generateNumberSequence(ny);
-      if (invertX) {
-        xs.reverse();
-      }
-      if (invertY) {
-        ys.reverse();
-      }
+      let offsetX = 0;
+      let offsetY = 0;
       if (isCentered) {
-        shiftNumbers(xs, -nx / 2 + 0.5);
-        shiftNumbers(ys, -ny / 2 + 0.5);
+        offsetX = -nx / 2 + 0.5;
+        offsetY = -ny / 2 + 0.5;
       }
-      const keys = makeMatrixKeyEntities(xs, ys, 0);
+      const keys = makeMatrixKeyEntitiesW(
+        nx,
+        ny,
+        offsetX,
+        offsetY,
+        isCentered,
+        invertX,
+        invertY,
+        0,
+      );
       design.keyEntities.push(...keys);
+    }
+  } else if (isEvenSplit) {
+    if (
+      spec.useMatrixKeyScanner &&
+      spec.matrixRowPins &&
+      spec.matrixColumnPins
+    ) {
+      const nx = spec.matrixColumnPins.length;
+      const ny = spec.matrixRowPins.length;
+
+      let offsetXL = -nx - splitXOffset;
+      let offsetXR = splitXOffset;
+      let offsetY = 0;
+
+      if (isCentered) {
+        offsetXL = -nx - 0.5;
+        offsetXR = 1.5;
+        offsetY = -ny / 2 + 0.5;
+      }
+
+      const keysLeft = makeMatrixKeyEntitiesW(
+        nx,
+        ny,
+        offsetXL,
+        offsetY,
+        isCentered,
+        !invertX,
+        invertY,
+        0,
+      );
+      const keysRight = makeMatrixKeyEntitiesW(
+        nx,
+        ny,
+        offsetXR,
+        offsetY,
+        isCentered,
+        invertX,
+        invertY,
+        nx * ny,
+      );
+      design.keyEntities.push(...keysLeft);
+      design.keyEntities.push(...keysRight);
     }
   }
   return DisplayKeyboardDesignLoader.loadDisplayKeyboardDesign(design);
