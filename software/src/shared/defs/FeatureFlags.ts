8559a416b7a56a5db53e89c3289069af7e52e300
yahiro <yahiro07@users.noreply.github.com>
2021-10-20 23:45:04 +0900

    AVR DFUブートローダの対応を一旦無効化

--- software/src/shared/defs/FeatureFlags.ts
+++ software/src/shared/defs/FeatureFlags.ts
@@ -1,4 +1,8 @@
-export const featureFlags = {};
+export const featureFlags = {
+  // MacOSでアプリ終了時にnode-usbの実装でSIGABRTが出る問題があるため、
+  // AVRのDFUブートローダの対応を一旦無効化
+  supportFirmwareUpdateSchemeAvrDfu: false,
+};
 // if (process.env.NODE_ENV === 'development') {
 //   console.log('feature flag allowEditLocalProject enabled');
 //   featureFlags.allowEditLocalProject = true;
