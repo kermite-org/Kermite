24f1091bbebb4def57ce10ada4f40a6f9af53fa7
yahiro <yahiro07@users.noreply.github.com>
2021-10-25 06:12:37 +0900

    コンポーネントを統合

--- software/src/ui/features/ProjectQuickSetupPart/ProjectQuickSetupPart_StepFirmwareFlash.tsx
+++ software/src/ui/features/ProjectQuickSetupPart/ProjectQuickSetupPart_StepFirmwareFlash.tsx
@@ -1,6 +1,8 @@
-import { FC, jsx, useMemo } from 'qx';
+import { css, FC, jsx, useMemo } from 'qx';
+import { uiTheme } from '~/ui/base';
+import { DeviceAutoConnectionPart } from '~/ui/fabrics/DeviceAutoConnectionPart/view';
+import { StandardFirmwareFlashPart } from '~/ui/fabrics/StandardFirmwareFlashPart/view';
 import { projectQuickSetupStore } from '~/ui/features/ProjectQuickSetupPart/base/ProjectQuickSetupStore';
-import { ProjectQuickSetupFirmwareFlashStepView } from '~/ui/pageContent/FirmwareFlashPageContent/ProjectQuickSetupFirmwareFlashStepView';
 
 export const ProjectQuickSetupPart_StepFirmwareFlash: FC = () => {
   projectQuickSetupStore.effects.useEditDataPersistence();
@@ -8,10 +10,76 @@ export const ProjectQuickSetupPart_StepFirmwareFlash: FC = () => {
     () => projectQuickSetupStore.readers.emitDraftProjectInfo(),
     [],
   );
+  const firmwareVariationId = '01';
   return (
-    <ProjectQuickSetupFirmwareFlashStepView
-      projectInfo={projectInfo}
-      fixedFirmwareVariationId="01"
-    />
+    <div class={style}>
+      <div class="row first-row">
+        <div class="panel device-connection-panel">
+          <h2>Device Connection Status</h2>
+          <DeviceAutoConnectionPart
+            projectInfo={projectInfo}
+            firmwareVariationId={firmwareVariationId}
+          />
+        </div>
+        <div class="panel keystate-preview-panel">
+          <h2>KeyState Preview</h2>
+        </div>
+      </div>
+      <div class="row second-row">
+        <div class="panel firmware-flash-panel">
+          <h2>Flash Firmware</h2>
+          <StandardFirmwareFlashPart
+            projectInfo={projectInfo}
+            firmwareVariationId={firmwareVariationId}
+          />
+        </div>
+        <div class="panel parameters-panel">
+          <h2>Parameters</h2>
+        </div>
+      </div>
+    </div>
   );
 };
+
+const style = css`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+
+  > .row {
+    display: flex;
+    gap: 5px;
+
+    &.first-row {
+      min-height: 250px;
+    }
+
+    &.second-row {
+      flex-grow: 1;
+    }
+
+    > .panel {
+      background: ${uiTheme.colors.clPanelBox};
+      padding: 7px;
+
+      &.device-connection-panel {
+        width: 50%;
+      }
+
+      &.keystate-preview-panel {
+        width: 50%;
+      }
+
+      &.firmware-flash-panel {
+        width: 55%;
+      }
+
+      &.parameters-panel {
+        width: 45%;
+      }
+    }
+  }
+
+  padding: 5px;
+`;
--- software/src/ui/pages/FirmwareFlashPage.tsx
+++ software/src/ui/pages/FirmwareFlashPage.tsx
@@ -1,15 +1,5 @@
 import { FC, jsx } from 'qx';
-import { ProjectQuickSetupFirmwareFlashStepView } from '~/ui/pageContent/FirmwareFlashPageContent/ProjectQuickSetupFirmwareFlashStepView';
-import { uiReaders } from '~/ui/store';
 
 export const FirmwareFlashPage: FC = () => {
-  const projectInfo = uiReaders.allProjectPackageInfos.find(
-    (it) => it.projectKey === uiReaders.globalProjectKey,
-  )!;
-  return (
-    <ProjectQuickSetupFirmwareFlashStepView
-      projectInfo={projectInfo}
-      fixedFirmwareVariationId=""
-    />
-  );
+  return <div>unimplemented yet</div>;
 };
