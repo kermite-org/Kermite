d4d531642411cc0e2f42c6254c81bf03c05c713d
yahiro <yahiro07@users.noreply.github.com>
2021-10-28 09:43:32 +0900

    context apiの実験コードを追加

--- software/src/ui-mock-view/QxDebugPage4.tsx
+++ software/src/ui-mock-view/QxDebugPage4.tsx
@@ -1,19 +1,69 @@
-import { FC, jsx, useRef, useState } from 'qx';
+import { FC, jsx, useState } from 'qx';
 
-const Foo: FC = () => {
-  const [message] = useState('Hello');
+type IContextProvider<T> = FC<{ value: T; children: any }>;
 
-  const ref = useRef();
-  console.log({ ...ref });
+type IContext<T> = {
+  value: T;
+  Provider: IContextProvider<T>;
+};
 
-  return <div ref={ref}>{message}</div>;
+function createContext<T>(defaultValue: T): IContext<T> {
+  let sourceValue = defaultValue;
+  return {
+    get value() {
+      return sourceValue;
+    },
+    Provider: ({ value, children }) => {
+      sourceValue = value;
+      return children[0];
+    },
+  };
+}
+
+function useContext<T>(context: IContext<T>): T {
+  return context.value;
+}
+
+// ----------
+
+type ICountContextValue = {
+  count: number;
+  increment(): void;
+};
+const CountContext = createContext<ICountContextValue>({} as any);
+const useCountContext = () => useContext(CountContext);
+
+function useCountContextValue(): ICountContextValue {
+  const [count, setCount] = useState(0);
+  return {
+    count,
+    increment: () => setCount((prev) => prev + 1),
+  };
+}
+
+const CountView: FC = () => {
+  const { count, increment } = useCountContext();
+  return <div onClick={increment}>{count}</div>;
+};
+
+const ContextApiDevelopment: FC = () => {
+  const countContextValue = useCountContextValue();
+  const countContextValue2 = useCountContextValue();
+  return (
+    <div>
+      <CountContext.Provider value={countContextValue}>
+        <CountView />
+      </CountContext.Provider>
+      <CountContext.Provider value={countContextValue}>
+        <CountView />
+      </CountContext.Provider>
+      <CountContext.Provider value={countContextValue2}>
+        <CountView />
+      </CountContext.Provider>
+    </div>
+  );
 };
 
 export const QxDebugPage4: FC = () => {
-  console.log(`render`);
-  return (
-    <div onClick={() => {}}>
-      <Foo />
-    </div>
-  );
+  return <ContextApiDevelopment />;
 };
