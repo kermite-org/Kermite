85f9093cf6805d8726ccf88767d486c6b3f747db
yahiro <yahiro07@users.noreply.github.com>
2021-11-01 05:35:26 +0900

    標準ファームウェア設定のデータにボードの種類を追加

--- software/src/shared/defs/DomainTypes.ts
+++ software/src/shared/defs/DomainTypes.ts
@@ -33,11 +33,17 @@ export type IStandardBaseFirmwareType =
   | 'AvrOddSplit'
   | 'RpOddSplit';
 
+export type IStandardFirmwareBoardType =
+  | 'ChipAtMega32U4'
+  | 'ProMicro'
+  | 'ChipRP2040'
+  | 'ProMicroRP2040'
+  | 'RpiPico';
+
 export type IStandardFirmwareConfig = {
   baseFirmwareType: IStandardBaseFirmwareType;
-  useBoardLedsProMicroAvr?: boolean;
-  useBoardLedsProMicroRp?: boolean;
-  useBoardLedsRpiPico?: boolean;
+  boardType?: IStandardFirmwareBoardType;
+  useBoardLeds?: boolean;
   useDebugUart?: boolean;
   useMatrixKeyScanner?: boolean;
   matrixRowPins?: string[];
