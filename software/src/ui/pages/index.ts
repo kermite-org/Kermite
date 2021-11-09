5f6a7c45d34e46675caa1272c49e7333f2c4d1ea
yahiro <yahiro07@users.noreply.github.com>
2021-11-09 23:02:56 +0900

    コンポーネントを移動

--- software/src/ui/pages/index.ts
+++ software/src/ui/pages/index.ts
@@ -1,5 +1,3 @@
-export * from './PresetBrowserPageContent';
-export * from './PresetBrowserPageContent2';
 export * from './ProjectCustomFirmwareCreatePage';
 export * from './ProjectLayoutEditPage';
 export * from './ProjectPresetEditPage';
