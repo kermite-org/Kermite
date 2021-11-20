e41b5ac73951d2edb49ca3df94eb991806e5843c
yahiro <yahiro07@users.noreply.github.com>
2021-11-20 11:58:00 +0900

    fix type of texts

--- software/src/ui/base/UiText.ts
+++ software/src/ui/base/UiText.ts
@@ -1,7 +1,7 @@
 import textSourceEnglish from '../i18n/en.json';
 import textSourceJapanese from '../i18n/ja.json';
 
-type ITextSource = { [key in keyof typeof textSourceEnglish]: string };
+type ITextSource = typeof textSourceEnglish;
 
 export type ILanguageKey = 'english' | 'japanese';
 
