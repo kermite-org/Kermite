de2d2140f50e8c6c3addad75c55fb4e26feabb7f
yahiro <yahiro07@users.noreply.github.com>
2021-12-04 18:32:23 +0900

    move package reenumeration code to helper

--- software/src/shell/modules/project/ProjectPackageModule.ts
+++ software/src/shell/modules/project/ProjectPackageModule.ts
@@ -50,20 +50,21 @@ const projectPackageModuleHelper = {
       keyboardName,
     };
   },
+  async reEnumerateAllProjectPackages() {
+    const allProjectPackageInfos =
+      await projectPackageProvider.getAllProjectPackageInfos();
+    commitCoreState({ allProjectPackageInfos });
+  },
 };
 
 export const projectPackageModule = createCoreModule({
   async project_loadAllProjectPackages() {
     await remoteResourceUpdater_updateRemoteProjectPackages();
-    const allProjectPackageInfos =
-      await projectPackageProvider.getAllProjectPackageInfos();
-    commitCoreState({ allProjectPackageInfos });
+    await projectPackageModuleHelper.reEnumerateAllProjectPackages();
   },
   async project_saveLocalProjectPackageInfo(projectInfo) {
     await projectPackageProvider.saveLocalProjectPackageInfo(projectInfo);
-    const allProjectPackageInfos =
-      await projectPackageProvider.getAllProjectPackageInfos();
-    commitCoreState({ allProjectPackageInfos });
+    await projectPackageModuleHelper.reEnumerateAllProjectPackages();
   },
   async project_loadAllCustomFirmwareInfos() {
     const allCustomFirmwareInfos =
@@ -126,9 +127,7 @@ export const projectPackageModule = createCoreModule({
         packageName: newKeyboardName.toLowerCase(),
       };
       await projectPackageProvider.saveLocalProjectPackageInfo(newProject);
-      const allProjectPackageInfos =
-        await projectPackageProvider.getAllProjectPackageInfos();
-      commitCoreState({ allProjectPackageInfos });
+      await projectPackageModuleHelper.reEnumerateAllProjectPackages();
     }
   },
   async project_openLocalProjectsFolder() {
