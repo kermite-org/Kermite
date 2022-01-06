41e09e60b1ef227f82d715b18907887427f820de
yahiro <yahiro07@users.noreply.github.com>
2022-01-07 02:42:41 +0900

    fix area for loading overlay

--- software/src/ui/components/overlay/LoadingOverlay.tsx
+++ software/src/ui/components/overlay/LoadingOverlay.tsx
@@ -17,11 +17,11 @@ export const LoadingOverlay: FC<Props> = ({ isLoading }) => {
 };
 
 const style = css`
-  position: fixed;
+  position: absolute;
   top: 0;
   left: 0;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   background: rgba(0, 0, 0, 0.5);
   display: flex;
   justify-content: center;
