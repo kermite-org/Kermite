49cd721d33c4145ced078ef3973aa74dafa2cadd
yahiro <yahiro07@users.noreply.github.com>
2022-01-16 21:23:07 +0900

    refactor project selection part store

--- software/src/ui/features/ProjectSelectionPart/ProjectSelectionPart.tsx
+++ software/src/ui/features/ProjectSelectionPart/ProjectSelectionPart.tsx
@@ -1,8 +1,32 @@
-import { FC, jsx } from 'alumina';
+import { FC, jsx, useEffect } from 'alumina';
 import { ProjectSelectionPartComponent } from '~/ui/fabrics';
-import { useProjectSelectionPartModel } from '~/ui/features/ProjectSelectionPart/models/ProjectSelectionPartModel';
+import { projectSelectionPartStore } from '~/ui/store/domains/ProjectSelectionPartStore/ProjectSelectionPartStore';
 
 export const ProjectSelectionPart: FC = () => {
-  const model = useProjectSelectionPartModel();
-  return <ProjectSelectionPartComponent {...model} />;
+  useEffect(projectSelectionPartStore.initializeOnMount, []);
+  const {
+    readers: {
+      sourceProjectItems,
+      projectKey,
+      canSelectResourceOrigin,
+      resourceOriginSelectorSource,
+      isMenuButtonVisible,
+      menuItems,
+      showNoSelectionOption,
+    },
+    actions: { setProjectKey },
+  } = projectSelectionPartStore;
+
+  return (
+    <ProjectSelectionPartComponent
+      sourceProjectItems={sourceProjectItems}
+      projectKey={projectKey}
+      canSelectResourceOrigin={canSelectResourceOrigin}
+      resourceOriginSelectorSource={resourceOriginSelectorSource}
+      isMenuButtonVisible={isMenuButtonVisible}
+      menuItems={menuItems}
+      showNoSelectionOption={showNoSelectionOption}
+      setProjectKey={setProjectKey}
+    />
+  );
 };
