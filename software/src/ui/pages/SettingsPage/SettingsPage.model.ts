25387a5295c48c5d32fded79712c66d0558529fc
yahiro <yahiro07@users.noreply.github.com>
2022-01-16 17:50:03 +0900

    change store format of settings page

--- software/src/ui/pages/SettingsPage/SettingsPage.model.ts
+++ software/src/ui/pages/SettingsPage/SettingsPage.model.ts
@@ -1,4 +1,3 @@
-import { useEffect, useLocal } from 'alumina';
 import { appUi, ipcAgent, ISelectorOption } from '~/ui/base';
 import {
   commitUiSettings,
@@ -7,105 +6,139 @@ import {
   uiState,
 } from '~/ui/store';
 
-export interface ISettingsPageModel {
-  flagDeveloperMode: boolean;
-  setFlagDeveloperMode: (value: boolean) => void;
-  flagUseLocalResources: boolean;
-  setFlagUseLocalResources: (value: boolean) => void;
-  flagAllowCrossKeyboardKeyMappingWrite: boolean;
-  setFlagAllowCrossKeyboardKeyMappingWrite: (value: boolean) => void;
-  canChangeLocalRepositoryFolderPath: boolean;
-  localRepositoryFolderPathDisplayValue: string;
-  handleSelectLocalRepositoryFolder: () => void;
-  isLocalRepositoryFolderPathValid: boolean;
-  uiScalingOptions: ISelectorOption[];
-  uiScalingSelectionValue: string;
-  setUiScalingSelectionValue: (value: string) => void;
-  appVersionInfo: string;
-  flagShowDevelopmentPackages: boolean;
-  setFlagShowDevelopmentPackages: (value: boolean) => void;
+interface ISettingsPageStore {
+  readers: {
+    flagDeveloperMode: boolean;
+    flagUseLocalResources: boolean;
+    flagAllowCrossKeyboardKeyMappingWrite: boolean;
+    canChangeLocalRepositoryFolderPath: boolean;
+    localRepositoryFolderPathDisplayValue: string;
+    isLocalRepositoryFolderPathValid: boolean;
+    uiScalingOptions: ISelectorOption[];
+    uiScalingSelectionValue: string;
+    appVersionInfo: string;
+    flagShowDevelopmentPackages: boolean;
+  };
+  actions: {
+    initialize(): void;
+    setFlagDeveloperMode(value: boolean): void;
+    setFlagUseLocalResources(value: boolean): void;
+    setFlagAllowCrossKeyboardKeyMappingWrite(value: boolean): void;
+    handleSelectLocalRepositoryFolder(): void;
+    setUiScalingSelectionValue(value: string): void;
+    setFlagShowDevelopmentPackages(value: boolean): void;
+  };
 }
 
-const sourceScales = [0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3];
+const helpers = {
+  createUiScalingSelectorOptions(): ISelectorOption[] {
+    const sourceScales = [0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3];
+    if (appUi.processEnv.FE_ADD_SMALL_UI_SCALES) {
+      sourceScales.unshift(...[0.3, 0.4, 0.5, 0.6]);
+    }
+    return sourceScales.map((val) => ({
+      label: `${(val * 100) >> 0}%`,
+      value: val.toString(),
+    }));
+  },
+};
 
-if (appUi.processEnv.FE_ADD_SMALL_UI_SCALES) {
-  sourceScales.unshift(...[0.3, 0.4, 0.5, 0.6]);
-}
+export function createSettingsPageStore(): ISettingsPageStore {
+  type IState = {
+    fixedProjectRootPath: string;
+    temporaryInvalidLocalRepositoryFolderPath: string;
+  };
+  type IReaders = ISettingsPageStore['readers'];
+  type IActions = ISettingsPageStore['actions'];
 
-const uiScaleOptions: ISelectorOption[] = sourceScales.map((val) => ({
-  label: `${(val * 100) >> 0}%`,
-  value: val.toString(),
-}));
-
-export function useSettingsPageModel(): ISettingsPageModel {
-  const local = useLocal({
+  const state: IState = {
     fixedProjectRootPath: '',
     temporaryInvalidLocalRepositoryFolderPath: '',
-  });
-
-  useEffect(() => {
-    (async () => {
-      if (appUi.isDevelopment) {
-        local.fixedProjectRootPath =
-          await ipcAgent.async.config_getProjectRootDirectoryPath();
-      }
-    })();
-  }, []);
-
-  const { globalSettings } = uiReaders;
-
-  const onSelectButton = async () => {
-    const path = await ipcAgent.async.file_getOpenDirectoryWithDialog();
-    if (path) {
-      const valid = await ipcAgent.async.config_checkLocalRepositoryFolderPath(
-        path,
-      );
-      if (!valid) {
-        local.temporaryInvalidLocalRepositoryFolderPath = path;
-      } else {
-        local.temporaryInvalidLocalRepositoryFolderPath = '';
-        globalSettingsWriter.writeValue('localProjectRootFolderPath', path);
-      }
-    }
   };
 
-  const folderPathDisplayValue =
-    local.temporaryInvalidLocalRepositoryFolderPath ||
-    local.fixedProjectRootPath ||
-    globalSettings.localProjectRootFolderPath;
+  const constants = {
+    uiScaleOptions: helpers.createUiScalingSelectorOptions(),
+  };
 
-  const appVersionInfo = uiState.core.applicationVersionInfo.version;
+  const readers: IReaders = {
+    get flagDeveloperMode() {
+      return uiReaders.globalSettings.developerMode;
+    },
+    get flagUseLocalResources() {
+      return uiReaders.globalSettings.useLocalResources;
+    },
+    get flagAllowCrossKeyboardKeyMappingWrite() {
+      return uiReaders.globalSettings.allowCrossKeyboardKeyMappingWrite;
+    },
+    get canChangeLocalRepositoryFolderPath() {
+      const isDeveloperModeOn = uiReaders.globalSettings.developerMode;
+      return !state.fixedProjectRootPath && isDeveloperModeOn;
+    },
+    get localRepositoryFolderPathDisplayValue() {
+      return (
+        state.temporaryInvalidLocalRepositoryFolderPath ||
+        state.fixedProjectRootPath ||
+        uiReaders.globalSettings.localProjectRootFolderPath
+      );
+    },
+    get isLocalRepositoryFolderPathValid() {
+      return !state.temporaryInvalidLocalRepositoryFolderPath;
+    },
+    get uiScalingOptions() {
+      return constants.uiScaleOptions;
+    },
+    get uiScalingSelectionValue() {
+      return uiState.settings.siteDpiScale.toString();
+    },
+    get appVersionInfo() {
+      return uiState.core.applicationVersionInfo.version;
+    },
+    get flagShowDevelopmentPackages() {
+      return uiReaders.globalSettings.showDevelopmentPackages;
+    },
+  };
 
-  const isDeveloperModeOn = globalSettings.developerMode;
-
-  const canChangeFolder = !local.fixedProjectRootPath && isDeveloperModeOn;
-
-  return {
-    flagDeveloperMode: globalSettings.developerMode,
-    setFlagDeveloperMode: (value) =>
-      globalSettingsWriter.writeValue('developerMode', value),
-    flagUseLocalResources: globalSettings.useLocalResources,
-    setFlagUseLocalResources: (value) =>
-      globalSettingsWriter.writeValue('useLocalResources', value),
-    flagAllowCrossKeyboardKeyMappingWrite:
-      globalSettings.allowCrossKeyboardKeyMappingWrite,
-    setFlagAllowCrossKeyboardKeyMappingWrite: (value) =>
+  const actions: IActions = {
+    async initialize() {
+      if (appUi.isDevelopment) {
+        state.fixedProjectRootPath =
+          await ipcAgent.async.config_getProjectRootDirectoryPath();
+      }
+    },
+    setFlagDeveloperMode(value: boolean) {
+      globalSettingsWriter.writeValue('developerMode', value);
+    },
+    setFlagUseLocalResources(value: boolean) {
+      globalSettingsWriter.writeValue('useLocalResources', value);
+    },
+    setFlagAllowCrossKeyboardKeyMappingWrite(value: boolean) {
       globalSettingsWriter.writeValue(
         'allowCrossKeyboardKeyMappingWrite',
         value,
-      ),
-    canChangeLocalRepositoryFolderPath: canChangeFolder,
-    localRepositoryFolderPathDisplayValue: folderPathDisplayValue,
-    handleSelectLocalRepositoryFolder: onSelectButton,
-    isLocalRepositoryFolderPathValid:
-      !local.temporaryInvalidLocalRepositoryFolderPath,
-    uiScalingOptions: uiScaleOptions,
-    uiScalingSelectionValue: uiState.settings.siteDpiScale.toString(),
-    setUiScalingSelectionValue: (strVal) =>
-      commitUiSettings({ siteDpiScale: parseFloat(strVal) }),
-    appVersionInfo,
-    flagShowDevelopmentPackages: globalSettings.showDevelopmentPackages,
-    setFlagShowDevelopmentPackages: (value) =>
-      globalSettingsWriter.writeValue('showDevelopmentPackages', value),
+      );
+    },
+    async handleSelectLocalRepositoryFolder() {
+      const path = await ipcAgent.async.file_getOpenDirectoryWithDialog();
+      if (path) {
+        const valid =
+          await ipcAgent.async.config_checkLocalRepositoryFolderPath(path);
+        if (!valid) {
+          state.temporaryInvalidLocalRepositoryFolderPath = path;
+        } else {
+          state.temporaryInvalidLocalRepositoryFolderPath = '';
+          globalSettingsWriter.writeValue('localProjectRootFolderPath', path);
+        }
+      }
+    },
+    setUiScalingSelectionValue(strVal: string) {
+      commitUiSettings({ siteDpiScale: parseFloat(strVal) });
+    },
+    setFlagShowDevelopmentPackages(value) {
+      globalSettingsWriter.writeValue('showDevelopmentPackages', value);
+    },
   };
+
+  actions.initialize();
+
+  return { readers, actions };
 }
