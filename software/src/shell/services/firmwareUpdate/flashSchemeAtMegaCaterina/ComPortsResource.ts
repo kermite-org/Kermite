20b12a22594fa71ba325116ba4c3be9d9bf24d2c
yahiro <yahiro07@users.noreply.github.com>
2022-06-20 17:41:13 +0900

    ファームウェア書き込み関連のモジュールを削除

--- software/src/shell/services/firmwareUpdate/flashSchemeAtMegaCaterina/ComPortsResource.ts
+++
@@ -1,8 +0,0 @@
-import SerialPort = require('serialport');
-
-export class ComPortsResource {
-  static async getComPortNames() {
-    const ports = await SerialPort.list();
-    return ports.map((port) => port.path);
-  }
-}
