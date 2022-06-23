15897d1f2aee73e4aa390c22a712bc522dd1de5a
yahiro <yahiro07@users.noreply.github.com>
2022-06-23 21:40:17 +0900

    バージョンを更新

--- software/src/shell/base/appConfig.ts
+++ software/src/shell/base/appConfig.ts
@@ -13,7 +13,7 @@ export const appConfig = {
   isDevelopment: processEnv.NODE_ENV === 'development',
   // isDevelopment: location.host === 'localhost',
   // applicationVersion: app.getVersion(),
-  applicationVersion: 'v220623c',
+  applicationVersion: 'v220623d',
   // publicRootPath: `${pathDirname(__dirname)}/ui`,
   // preloadFilePath: `${__dirname}/preload.js`,
   pageTitle: 'Kermite',
