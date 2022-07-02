425abbb68ba4c87de24a1867152a888c58e28620
yahiro <yahiro07@users.noreply.github.com>
2022-07-02 16:38:21 +0900

    devServerにSSLを適用するオプションを追加

--- software/vite.config.ts
+++ software/vite.config.ts
@@ -1,44 +1,58 @@
 import path from 'path';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { defineConfig } from 'vite';
+import * as devcert from 'devcert';
+import { ServerOptions, loadEnv, defineConfig } from 'vite';
 import EnvironmentPlugin from 'vite-plugin-environment';
 
-export default defineConfig({
-  base: './',
-  build: {
-    outDir: './dist',
-    emptyOutDir: true,
-    minify: false,
-    sourcemap: true,
-  },
-  resolve: {
-    alias: {
-      '~': path.join(__dirname, 'src/'),
-      path: 'path-browserify',
+async function getServerSslOptions(): Promise<ServerOptions> {
+  if (process.env.FE_USE_DEV_SERVER_HTTPS) {
+    const { key, cert } = await devcert.certificateFor('localhost');
+    return { https: { key, cert } };
+  }
+  return {};
+}
+
+export default async ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
+
+  return await defineConfig({
+    base: './',
+    build: {
+      outDir: './dist',
+      emptyOutDir: true,
+      minify: false,
+      sourcemap: true,
     },
-  },
-  plugins: [
-    EnvironmentPlugin({
-      FE_USE_KERMITE_SERVER_LOCAL: null,
-      FE_USE_DEBUG_LOCAL_FIRMWARES: null,
-    }),
-  ],
-  // avoid 'Buffer is not defined' exception raised when reading error.stack
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis',
+    resolve: {
+      alias: {
+        '~': path.join(__dirname, 'src/'),
+        path: 'path-browserify',
       },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-        }) as any,
-      ],
     },
-  },
-  envPrefix: 'FE_',
-  clearScreen: false,
-  server: {
-    host: '0.0.0.0',
-  },
-});
+    plugins: [
+      EnvironmentPlugin({
+        FE_USE_KERMITE_SERVER_LOCAL: null,
+        FE_USE_DEBUG_LOCAL_FIRMWARES: null,
+      }),
+    ],
+    // avoid 'Buffer is not defined' exception raised when reading error.stack
+    optimizeDeps: {
+      esbuildOptions: {
+        define: {
+          global: 'globalThis',
+        },
+        plugins: [
+          NodeGlobalsPolyfillPlugin({
+            buffer: true,
+          }) as any,
+        ],
+      },
+    },
+    envPrefix: 'FE_',
+    clearScreen: false,
+    server: {
+      ...(await getServerSslOptions()),
+      host: '0.0.0.0',
+    },
+  });
+};
