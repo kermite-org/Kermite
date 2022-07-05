4b137abcbfe4b32b75c94b629c2ccc3b3ec2b7d9
yahiro <yahiro07@users.noreply.github.com>
2022-07-06 05:29:05 +0900

    試し打ちページを作成

--- software/src/ui/root/SiteRoot.tsx
+++ software/src/ui/root/SiteRoot.tsx
@@ -16,6 +16,7 @@ import {
   uiState,
   uiStateDriverEffect,
 } from '~/ui/store';
+import { EditorDemoPage } from '../pages/EditorDemoPage';
 
 const cssGlobal = css`
   * {
@@ -67,7 +68,7 @@ const AppView: FC = () => {
   const isEditorDemoMode = !!queries.editor_demo;
   const isWidgetMode = uiReaders.pagePath === '/widget';
   if (isEditorDemoMode) {
-    return <WidgetZoneRoot />;
+    return <EditorDemoPage />;
   } else if (isWidgetMode) {
     return <WidgetZoneRoot />;
   } else {
