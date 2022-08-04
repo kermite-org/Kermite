fd357d0d5c0b1506b5951436f64559c422ce268f
yahiro <yahiro07@users.noreply.github.com>
2022-08-04 22:57:18 +0900

    バージョンを修正

--- software/src/shell/base/appConfig.ts
+++ software/src/shell/base/appConfig.ts
@@ -13,7 +13,7 @@ export const processEnv = (import.meta as any).env as IProcessEnv;
 // console.log({ processEnv });
 
 export const appConfig = {
-  applicationVersion: 'v220805',
+  applicationVersion: 'v220804b',
   isDevelopment: processEnv.DEV,
   // isDevelopment: location.host === 'localhost',
   // applicationVersion: app.getVersion(),
