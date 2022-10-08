82798794f95e8739046a1b2d64172324c3b73362
yahiro <yahiro07@users.noreply.github.com>
2022-10-08 10:28:21 +0900

    必要な要素のみを取り込むフィルタ機能を実装

---
+++ software_v2/src/entityInitializer.ts
@@ -0,0 +1,11 @@
+export function createFallbackProjectPackage(): ILocalProject {
+  return {
+    formatRevision: "PKG1",
+    projectId: "",
+    projectName: "",
+    variationName: "",
+    profiles: [],
+    layouts: [],
+    firmwares: [],
+  };
+}
---
+++ software_v2/src/features/importResourceSelector/ImportResourceSelector.tsx
@@ -0,0 +1,112 @@
+import { FC, jsx } from "alumina";
+import { reflectChecked } from "~/funcs";
+
+type InputProps = {
+  localProject: ILocalProject;
+  remoteProject: ILocalProject;
+};
+
+type IItemType = "profile" | "layout" | "firmware";
+type IResourceListItem = {
+  itemKey: string;
+  label: string;
+  itemType: IItemType;
+  itemName: string;
+  selected: boolean;
+  canSelect: boolean;
+};
+
+type IOutputProps = {
+  importItems: {
+    itemType: IItemType;
+    itemName: string;
+  }[];
+};
+
+export function createImportResourceSelectorStore() {
+  const state = {
+    resourceListItems: undefined! as IResourceListItem[],
+  };
+
+  const readers = {
+    getListItemEntities() {},
+  };
+
+  const actions = {
+    setItemChecked(itemKey: string, checked: boolean) {
+      const item = state.resourceListItems.find((it) => it.itemKey === itemKey);
+      if (item) {
+        item.selected = checked;
+      }
+    },
+  };
+
+  function setInputProps(props: InputProps) {
+    const { profiles, layouts, firmwares } = props.remoteProject;
+    state.resourceListItems = [
+      ...profiles.map((it) => ({
+        itemType: "profile" as const,
+        itemName: it.name,
+        itemKey: `profile__${it.name}`,
+        label: it.name,
+        selected: false,
+        canSelect: true,
+      })),
+      ...layouts.map((it) => ({
+        itemType: "layout" as const,
+        itemName: it.name,
+        itemKey: `layout__${it.name}`,
+        label: it.name,
+        selected: false,
+        canSelect: true,
+      })),
+      ...firmwares.map((it) => ({
+        itemType: "firmware" as const,
+        itemName: it.name,
+        itemKey: `firmware__${it.name}`,
+        label: it.name,
+        selected: false,
+        canSelect: true,
+      })),
+    ];
+  }
+
+  function getOutputProps(): IOutputProps {
+    const importItems = state.resourceListItems
+      .filter((it) => it.selected)
+      .map((it) => ({ itemType: it.itemType, itemName: it.itemName }));
+    return { importItems };
+  }
+
+  return { state, readers, actions, setInputProps, getOutputProps };
+}
+
+type IImportResourceSelectorStore = ReturnType<
+  typeof createImportResourceSelectorStore
+>;
+
+export const ImportResourceSelector: FC<{
+  store: IImportResourceSelectorStore;
+}> = ({ store }) => {
+  const {
+    state: { resourceListItems },
+    actions: { setItemChecked },
+  } = store;
+  return (
+    <div>
+      <ul>
+        {resourceListItems.map((item) => (
+          <li key={item.itemKey}>
+            <input
+              type="checkbox"
+              onChange={reflectChecked((checked) =>
+                setItemChecked(item.itemKey, checked)
+              )}
+            ></input>
+            ({item.itemType}) {item.label}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
