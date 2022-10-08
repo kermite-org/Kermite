b8fa140134f11b6743ae37a9fe9118a33b5fe056
yahiro <yahiro07@users.noreply.github.com>
2022-10-08 10:38:44 +0900

    ファイルを分割

---
+++ software_v2/src/globalStyle.ts
@@ -0,0 +1,35 @@
+import { css } from "alumina";
+
+export const globalStyle = css`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  html,
+  body {
+    height: 100%;
+  }
+
+  #app {
+    height: 100%;
+    font-family: "Noto Sans JP", sans-serif;
+    font-size: 16px;
+    color: #222;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5 {
+    font-size: 16px;
+    font-weight: normal;
+  }
+
+  ul,
+  li {
+    list-style: none;
+  }
+`;
