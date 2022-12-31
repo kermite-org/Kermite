640b4138aa4337e9782574c60184f911b9267580
yahiro <yahiro07@users.noreply.github.com>
2022-12-31 18:22:50 +0900

    エディタ領域のスクロールを調整

--- software_v2/src/platform/PageRoot.tsx
+++ software_v2/src/platform/PageRoot.tsx
@@ -5,62 +5,88 @@ import { ProfileEditorView } from '~/feature-profile-editor';
 import { appStore } from './appStore';
 import { globalStyle } from './globalStyle';
 
-const MenuBar: FC = () => {
-  const { actions } = appStore;
-  const handleCreateProjectFromOnline = () =>
-    actions.openModel('onlineProjectImporter');
-  const handleCreateBlankProject = actions.loadBlankProject;
+export const PageRoot: FC = () => {
+  applyGlobalStyle(globalStyle);
 
+  const { modalType } = appStore.state;
   return domStyled(
     <div>
-      <ul>
-        <li>プロジェクト</li>
-        <li onClick={handleCreateProjectFromOnline}>
-          -新規作成(オンラインから)
-        </li>
-        <li onClick={handleCreateBlankProject}>-新規作成(空のプロジェクト)</li>
-        <li onClick={undefined}>-プロジェクト設定</li>
-        <li onClick={undefined}>-エクスポート</li>
-        <li>--------</li>
-        <li onClick={undefined}>-キーマップ作成</li>
-        <li onClick={undefined}>-レイアウト作成</li>
-        <li onClick={undefined}>-ファームウェア作成</li>
-      </ul>
-      <ul>
-        <li>編集</li>
-        <li onClick={undefined}>-元に戻す</li>
-        <li onClick={undefined}>-やり直し</li>
-      </ul>
+      <div class="app-title-bar">Kermite</div>
+      <div class="top-bar">
+        <MenuBar />
+      </div>
+      <div class="main-row">
+        <div class="side-bar">
+          <ProjectResourcePanel />
+          <div>デバイス</div>
+        </div>
+        <div class="main-column">
+          <EditorAreaContent />
+        </div>
+      </div>
+      <OnlineProjectImporterView if={modalType === 'onlineProjectImporter'} />
     </div>,
     css`
+      height: 100%;
       display: flex;
-      gap: 20px;
+      flex-direction: column;
 
-      > ul {
-        > li:not(:first-child) {
-          cursor: pointer;
-          &:hover {
-            text-decoration: underline;
+      > .app-title-bar {
+        flex-shrink: 0;
+        background: #08f;
+        padding: 2px 5px;
+        color: #fff;
+      }
+
+      > .top-bar {
+        flex-shrink: 0;
+        background: #ddd;
+        padding: 2px 5px;
+      }
+      > .main-row {
+        flex-grow: 1;
+        overflow-y: hidden;
+
+        display: flex;
+        > .side-bar {
+          width: 180px;
+          flex-shrink: 0;
+          border: solid 1px #aaa;
+          border-right: none;
+          padding: 5px;
+
+          display: flex;
+          flex-direction: column;
+
+          > div:nth-child(2) {
+            margin-top: auto;
           }
+        }
+        > .main-column {
+          flex-grow: 1;
+          border: solid 1px #aaa;
+          padding: 5px;
+
+          overflow-y: scroll;
         }
       }
     `,
   );
 };
 
-const ProjectItemIcon: FC<{ text: string }> = ({ text }) => {
-  return domStyled(
-    <div>{text}</div>,
-    css`
-      display: inline-flex;
-      width: 26px;
-      height: 26px;
-      flex-shrink: 0;
-      justify-content: center;
-      align-items: center;
-      border: solid 1px #222;
-    `,
-  );
+const EditorAreaContent: FC = () => {
+  const { editorTargetPath } = appStore.state;
+
+  if (editorTargetPath) {
+    const editorType = editorTargetPath.split('/')[1];
+    if (editorType === 'profile') {
+      return <ProfileEditorView itemPath={editorTargetPath} />;
+    }
+    if (editorType === 'layout') {
+      return <LayoutEditorView itemPath={editorTargetPath} />;
+    }
+  }
+  return <div>no content</div>;
 };
 
 const ProjectResourcePanel: FC = () => {
@@ -116,81 +142,58 @@ const ProjectResourcePanel: FC = () => {
   );
 };
 
-const EditorAreaContent: FC = () => {
-  const { editorTargetPath } = appStore.state;
-
-  if (editorTargetPath) {
-    const editorType = editorTargetPath.split('/')[1];
-    if (editorType === 'profile') {
-      return <ProfileEditorView itemPath={editorTargetPath} />;
-    }
-    if (editorType === 'layout') {
-      return <LayoutEditorView itemPath={editorTargetPath} />;
-    }
-  }
-  return <div>no content</div>;
+const ProjectItemIcon: FC<{ text: string }> = ({ text }) => {
+  return domStyled(
+    <div>{text}</div>,
+    css`
+      display: inline-flex;
+      width: 26px;
+      height: 26px;
+      flex-shrink: 0;
+      justify-content: center;
+      align-items: center;
+      border: solid 1px #222;
+    `,
+  );
 };
 
-export const PageRoot: FC = () => {
-  applyGlobalStyle(globalStyle);
+const MenuBar: FC = () => {
+  const { actions } = appStore;
+  const handleCreateProjectFromOnline = () =>
+    actions.openModel('onlineProjectImporter');
+  const handleCreateBlankProject = actions.loadBlankProject;
 
-  const { modalType } = appStore.state;
   return domStyled(
     <div>
-      <div class="app-title-bar">Kermite</div>
-      <div class="top-bar">
-        <MenuBar />
-      </div>
-      <div class="main-row">
-        <div class="side-bar">
-          <ProjectResourcePanel />
-          <div>デバイス</div>
-        </div>
-        <div class="main-column">
-          <EditorAreaContent />
-        </div>
-      </div>
-      <OnlineProjectImporterView if={modalType === 'onlineProjectImporter'} />
+      <ul>
+        <li>プロジェクト</li>
+        <li onClick={handleCreateProjectFromOnline}>
+          -新規作成(オンラインから)
+        </li>
+        <li onClick={handleCreateBlankProject}>-新規作成(空のプロジェクト)</li>
+        <li onClick={undefined}>-プロジェクト設定</li>
+        <li onClick={undefined}>-エクスポート</li>
+        <li>--------</li>
+        <li onClick={undefined}>-キーマップ作成</li>
+        <li onClick={undefined}>-レイアウト作成</li>
+        <li onClick={undefined}>-ファームウェア作成</li>
+      </ul>
+      <ul>
+        <li>編集</li>
+        <li onClick={undefined}>-元に戻す</li>
+        <li onClick={undefined}>-やり直し</li>
+      </ul>
     </div>,
     css`
-      height: 100%;
       display: flex;
-      flex-direction: column;
+      gap: 20px;
 
-      > .app-title-bar {
-        flex-shrink: 0;
-        background: #08f;
-        padding: 2px 5px;
-        color: #fff;
-      }
-
-      > .top-bar {
-        flex-shrink: 0;
-        background: #ddd;
-        padding: 2px 5px;
-      }
-      > .main-row {
-        flex-grow: 1;
-
-        display: flex;
-        > .side-bar {
-          width: 180px;
-          flex-shrink: 0;
-          border: solid 1px #aaa;
-          border-right: none;
-          padding: 5px;
-
-          display: flex;
-          flex-direction: column;
-
-          > div:nth-child(2) {
-            margin-top: auto;
+      > ul {
+        > li:not(:first-child) {
+          cursor: pointer;
+          &:hover {
+            text-decoration: underline;
           }
-        }
-        > .main-column {
-          flex-grow: 1;
-          border: solid 1px #aaa;
-          padding: 5px;
         }
       }
     `,
