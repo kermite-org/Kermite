84fe8a81f8a543249ca10f41c9f3915a030aafb7
yahiro <yahiro07@users.noreply.github.com>
2023-01-01 22:39:37 +0900

    リソースの上書き保存を実装

--- software_v2/src/feature-layout-editor/view.tsx
+++ software_v2/src/feature-layout-editor/view.tsx
@@ -1,15 +1,54 @@
-import { FC, jsx, useMemo } from 'alumina';
+import { css, domStyled, FC, jsx, useMemo } from 'alumina';
+import { RouteHeaderBar } from '~/fe-shared';
 import { diLayoutEditor } from './di';
-import { LayoutEditorGeneralComponent } from './layoutEditor';
+import {
+  LayoutEditorGeneralComponent,
+  LayoutEditorGeneralComponent_OutputPropsSupplier,
+} from './layoutEditor';
 
 function createStore(itemPath: string) {
-  const layout = diLayoutEditor.loadLayout(itemPath);
+  const layoutName = itemPath.split('/')[2];
+  const state = {
+    layout: diLayoutEditor.loadLayout(itemPath),
+  };
+
   return {
-    layout,
+    layoutName,
+    get layout() {
+      return state.layout;
+    },
+    get isModified() {
+      return LayoutEditorGeneralComponent_OutputPropsSupplier.isModified;
+    },
+    saveLayout() {
+      const newLayout =
+        LayoutEditorGeneralComponent_OutputPropsSupplier.emitSavingDesign();
+      diLayoutEditor.saveLayout(itemPath, newLayout);
+      state.layout = newLayout;
+    },
   };
 }
 
 export const LayoutEditorView: FC<{ itemPath: string }> = ({ itemPath }) => {
-  const { layout } = useMemo(() => createStore(itemPath), [itemPath]);
-  return <LayoutEditorGeneralComponent layout={layout} />;
+  const { layoutName, layout, isModified, saveLayout } = useMemo(
+    () => createStore(itemPath),
+    [itemPath],
+  );
+
+  return domStyled(
+    <div>
+      <RouteHeaderBar
+        title={`edit layout: ${layoutName}`}
+        // backHandler={() => {}}
+        canSave={isModified}
+        saveHandler={saveLayout}
+      />
+      <LayoutEditorGeneralComponent layout={layout} />
+    </div>,
+    css`
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+    `,
+  );
 };
--- software_v2/src/feature-profile-editor/view.tsx
+++ software_v2/src/feature-profile-editor/view.tsx
@@ -1,26 +1,60 @@
-import { FC, jsx, useMemo } from 'alumina';
+import { css, domStyled, FC, jsx, useMemo } from 'alumina';
+import { RouteHeaderBar } from '~/fe-shared';
 import { diProfileEditor } from './di';
-import { AssignerGeneralComponent } from './profileEditor';
+import {
+  AssignerGeneralComponent,
+  AssignerGeneralComponent_OutputPropsSupplier,
+} from './profileEditor';
 
 function createStore(itemPath: string) {
-  const profile = diProfileEditor.loadProfile(itemPath);
-  const [projectId, ,] = itemPath.split('/');
-  const layoutName = profile.referredLayoutName;
+  const [projectId, _, profileName] = itemPath.split('/');
+  const state = {
+    profile: diProfileEditor.loadProfile(itemPath),
+  };
+  const layoutName = state.profile.referredLayoutName;
   const layoutItemPath = `${projectId}/layout/${layoutName}`;
   const layout = diProfileEditor.loadLayout(layoutItemPath);
 
   return {
-    profile,
+    profileName,
+    get profile() {
+      return state.profile;
+    },
+    get isModified() {
+      return AssignerGeneralComponent_OutputPropsSupplier.isModified;
+    },
+    saveProfile() {
+      const newProfile =
+        AssignerGeneralComponent_OutputPropsSupplier.emitSavingDesign();
+      diProfileEditor.saveProfile(itemPath, newProfile);
+      state.profile = newProfile;
+    },
     layout,
   };
 }
 
 export const ProfileEditorView: FC<{ itemPath: string }> = ({ itemPath }) => {
-  const { profile, layout } = useMemo(() => createStore(itemPath), [itemPath]);
-  return (
-    <AssignerGeneralComponent
-      originalProfile={profile}
-      keyboardLayout={layout}
-    />
+  const { profileName, profile, layout, isModified, saveProfile } = useMemo(
+    () => createStore(itemPath),
+    [itemPath],
+  );
+  return domStyled(
+    <div>
+      <RouteHeaderBar
+        title={`edit profile: ${profileName}`}
+        // backHandler={() => {}}
+        canSave={isModified}
+        saveHandler={saveProfile}
+      />
+      <AssignerGeneralComponent
+        originalProfile={profile}
+        keyboardLayout={layout}
+      />
+    </div>,
+    css`
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+    `,
   );
 };
