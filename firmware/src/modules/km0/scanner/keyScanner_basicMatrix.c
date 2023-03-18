8ceda22efc2954f05a8c7d1f29d14f1a4ab46226
yahiro <yahiro07@users.noreply.github.com>
2023-03-18 18:13:31 +0900

    マトリクスの行駆動方式を変更

--- firmware/src/modules/km0/scanner/keyScanner_basicMatrix.c
+++ firmware/src/modules/km0/scanner/keyScanner_basicMatrix.c
@@ -15,13 +15,6 @@ void keyScanner_basicMatrix_initialize(
     uint8_t _numColumns,
     const uint8_t *_rowPins,
     const uint8_t *_columnPins, uint8_t _scanIndexBase) {
-  for (uint8_t i = 0; i < numRows; i++) {
-    digitalIo_setInputPullup(rowPins[i]);
-  }
-  for (uint8_t i = 0; i < numColumns; i++) {
-    digitalIo_setInputPullup(columnPins[i]);
-  }
-
   numRows = _numRows;
   numColumns = _numColumns;
   rowPins = _rowPins;
@@ -29,13 +22,10 @@ void keyScanner_basicMatrix_initialize(
   scanIndexBase = _scanIndexBase;
 
   for (uint8_t i = 0; i < numRows; i++) {
-    uint8_t rowPin = rowPins[i];
-    digitalIo_setOutput(rowPin);
-    digitalIo_setHigh(rowPin);
+    digitalIo_setInputPullup(rowPins[i]);
   }
   for (uint8_t i = 0; i < numColumns; i++) {
-    uint8_t columnPin = columnPins[i];
-    digitalIo_setInputPullup(columnPin);
+    digitalIo_setInputPullup(columnPins[i]);
   }
 }
 
@@ -43,6 +33,7 @@ void keyScanner_basicMatrix_update(uint8_t *keyStateBitFlags) {
   uint8_t keySlotIndex = 0;
   for (uint8_t i = 0; i < numRows; i++) {
     uint8_t rowPin = rowPins[i];
+    digitalIo_setOutput(rowPin);
     digitalIo_setLow(rowPin);
     delayUs(5); //RP2040の場合僅かに待たないとキーの状態を正しく読み出せない
     for (uint8_t j = 0; j < numColumns; j++) {
@@ -51,6 +42,6 @@ void keyScanner_basicMatrix_update(uint8_t *keyStateBitFlags) {
       utils_writeArrayedBitFlagsBit(keyStateBitFlags, scanIndexBase + keySlotIndex, isDown);
       keySlotIndex++;
     }
-    digitalIo_setHigh(rowPin);
+    digitalIo_setInputPullup(rowPin);
   }
 }
