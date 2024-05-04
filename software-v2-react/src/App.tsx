37febc4361e033663b608260616d1d5778f7556a
yahiro <yahiro07@users.noreply.github.com>
2024-05-04 15:12:20 +0900

    KermiteServerへの疎通を確認

--- software-v2-react/src/App.tsx
+++ software-v2-react/src/App.tsx
@@ -1,6 +1,22 @@
 import { Box } from "@mui/system";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(
+        `https://server.kermite.org/api/packages/catalog`
+      );
+      const obj = await res.json();
+      console.log({ res, obj });
+
+      const res2 = await fetch(
+        `https://server.kermite.org/api/packages/projects/79xv6Z`
+      );
+      const obj2 = await res2.json();
+      console.log({ res2, obj2 });
+    })();
+  }, []);
   return (
     <>
       <Box color={"red"}>hello mui system</Box>
