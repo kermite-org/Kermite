53ef4a944ad871d10dcd8e1e5beae13dc6ed6a28
yahiro <yahiro07@users.noreply.github.com>
2024-05-04 15:00:05 +0900

    mui systemを導入

--- software-v2-react/src/App.tsx
+++ software-v2-react/src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box } from "@mui/system";
+import { useState } from "react";
+import "./App.css";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,8 +29,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Box color={"red"}>hello mui system</Box>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- software-v2-react/src/main.tsx
+++ software-v2-react/src/main.tsx
@@ -1,2 +1,2 @@
-// import "./app-entry";
+import "./app-entry";
 // import "./dev0-idb-bucket";
