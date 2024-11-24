bebd5abf60749b9fcc9b1262b54380a0df3cef4d
yahiro <yahiro07@users.noreply.github.com>
2024-11-24 21:48:22 +0900

    分割キーボードの左右のconfigを修正

--- firmware/src/projects/dev/testbed/encoder_rp_split/main.c
+++ firmware/src/projects/dev/testbed/encoder_rp_split/main.c
@@ -7,23 +7,30 @@
 #include "km0/scanner/keyScanner_encoders.h"
 #include "km0/wrapper/splitKeyboard.h"
 
-#define NumEncoders 1
-
-static EncoderConfig encoderConfigs[NumEncoders] = {
-  { .pinA = GP4, .pinB = GP5, .scanIndexBase = 0 },
-};
-
 static uint8_t pins_directWired[] = { GP9 };
 
 static void setupBoard(int8_t side) {
-  keyScanner_directWired_initialize(1, pins_directWired, 2);
-  keyboardMain_useKeyScanner(keyScanner_directWired_update);
-  keyScanner_encoders_initialize(NumEncoders, encoderConfigs);
-  keyboardMain_useKeyScanner(keyScanner_encoders_update);
+  if (side == 0) {
+    static EncoderConfig encoderConfigs[1] = {
+      { .pinA = GP4, .pinB = GP5, .scanIndexBase = 0 },
+    };
+    keyScanner_directWired_initialize(1, pins_directWired, 2);
+    keyboardMain_useKeyScanner(keyScanner_directWired_update);
+    keyScanner_encoders_initialize(1, encoderConfigs);
+    keyboardMain_useKeyScanner(keyScanner_encoders_update);
+  } else {
+    static EncoderConfig encoderConfigs[1] = {
+      { .pinA = GP4, .pinB = GP5, .scanIndexBase = 3 },
+    };
+    keyScanner_directWired_initialize(1, pins_directWired, 5);
+    keyboardMain_useKeyScanner(keyScanner_directWired_update);
+    keyScanner_encoders_initialize(1, encoderConfigs);
+    keyboardMain_useKeyScanner(keyScanner_encoders_update);
+  }
 }
 
 int main() {
-  debugUart_initialize(115200);
+  // debugUart_initialize(115200);
   boardIoImpl_setupLeds_kb2040();
   splitKeyboard_setBoardConfigCallback(setupBoard);
   splitKeyboard_start();
